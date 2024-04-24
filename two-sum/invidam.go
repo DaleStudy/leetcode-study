f3f5d84ad3d8e4cebfe9d4e18dc24f7071b047d0
Invidam <hansupark0208@gmail.com>
2024-04-24 17:28:00 +0900

    two-sum: map version

--- two-sum/invidam.go
+++ two-sum/invidam.go
@@ -1,31 +1,10 @@
-type NumAndIdx struct {
-	Num int
-	Idx int
-}
-
 func twoSum(nums []int, target int) []int {
-	numAndIdxs := make([]NumAndIdx, 0, len(nums))
+	need := make(map[int]int, len(nums))
 	for i, n := range nums {
-		numAndIdxs = append(numAndIdxs, NumAndIdx{Num: n, Idx: i})
-	}
-
-	sort.Slice(numAndIdxs, func(i, j int) bool {
-		return numAndIdxs[i].Num < numAndIdxs[j].Num
-	})
-
-	ldx, rdx := 0, len(numAndIdxs)-1
-
-	for ldx < rdx {
-		l := numAndIdxs[ldx]
-		r := numAndIdxs[rdx]
-		if sum := l.Num + r.Num; sum > target {
-			rdx--
-		} else if sum < target {
-			ldx++
-		} else {
-			return []int{l.Idx, r.Idx}
+		if j, ok := need[n]; ok {
+			return []int{i, j}
 		}
+		need[target-n] = i
 	}
-
 	return nil
 }
