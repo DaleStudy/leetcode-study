ea57aafb698eb2733e1435861eb8b8c79fcb9b62
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-04-25 02:36:12 +0900

    valid anagram solution

--- valid-anagram/Bumsu-Yi.py
+++ valid-anagram/Bumsu-Yi.py
@@ -1,3 +1,15 @@
 """
 https://leetcode.com/problems/valid-anagram/
 """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        my_dict1 = {}
+        my_dict2 = {}
+
+        for char in s:
+            my_dict1[char] = my_dict1.get(char, 0) + 1
+        
+        for char in t:
+            my_dict2[char] = my_dict2.get(char, 0) + 1
+        
+        return my_dict1 == my_dict2
