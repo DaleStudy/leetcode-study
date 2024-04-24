f644e1f77c1f85c8684bd365d428ab443162dd7c
LEEWONCHEOL <wclee2265@naver.com>
2024-04-24 16:36:24 +0900

    solved Valid Anagram

---
+++ valid-anagram/wclee2265.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int cnt1[26]={0,}, cnt2[26] = {0,};
+        for(char c : s) cnt1[c-'a']++;
+        for(char c : t) cnt2[c-'a']++;
+
+        for(int i=0; i<26; i++) {
+            if(cnt1[i] != cnt2[i]) return false;
+        }
+
+        return true;
+    }
+};
