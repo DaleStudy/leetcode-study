e6206a0e8936861313da7503606e5d0fb7c8c0cc
LEEWONCHEOL <wclee2265@naver.com>
2024-04-24 16:24:03 +0900

    solved Two Sum

---
+++ two-sum/wclee2265.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int, int> > numsIndices;
+
+        for(int i=0; i<n; i++){
+            numsIndices.push_back({nums[i], i});
+        }
+
+        sort(numsIndices.begin(), numsIndices.end());
+        int i, j = n-1;
+        for(i=0; i<n; i++) {
+            while(numsIndices[i].first + numsIndices[j].first > target) j--;
+            if(numsIndices[i].first + numsIndices[j].first == target) break;
+        }
+
+        vector<int> ans;
+        ans.push_back(numsIndices[i].second);
+        ans.push_back(numsIndices[j].second);
+        return ans;
+    }
+};
