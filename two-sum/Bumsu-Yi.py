a999846dfb9af6c62c2e4aa558274569830dd1e1
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-04-25 00:36:15 +0900

    initial commit

--- two-sum/Bumsu-Yi.py
+++ two-sum/Bumsu-Yi.py
@@ -1,5 +1,14 @@
 """
 https://leetcode.com/problems/two-sum/
 """
-#
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+         numMap = {}
+         
+         for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            
+            numMap[nums[i]] = i
 
