a8669dae31ce3d187738e9867e634d12f8614d43
Invidam <hansupark0208@gmail.com>
2024-04-24 15:12:30 +0900

    contains-duplicate: refactor - init map with size. - check exists only in if statement.

--- contains-duplicate/invidam.go
+++ contains-duplicate/invidam.go
@@ -1,10 +1,12 @@
 func containsDuplicate(nums []int) bool {
-	appeared := make(map[int]bool)
+	appeared := make(map[int]bool, len(nums))
 
 	for _, num := range nums {
-		if appeared[num] {
+		if _, found := appeared[num]; found {
 			return true
 		}
 		appeared[num] = true
 	}
+
+	return false
 }
