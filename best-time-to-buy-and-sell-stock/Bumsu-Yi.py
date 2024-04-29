714ab37bef6cb0ead465fca2bb8de4f23a3a9b64
Leo Kim <81009412+leokim0922@users.noreply.github.com>
2024-04-29 01:10:20 -0700

    Revert "Sam's week 1 solutions"

--- best-time-to-buy-and-sell-stock/Bumsu-Yi.py
+++ best-time-to-buy-and-sell-stock/Bumsu-Yi.py
@@ -1,16 +1,3 @@
 """
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 """
-
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        min_price = prices[0]
-
-        for price in prices:
-            profit = price - min_price
-            min_price = min(min_price, price)
-            max_profit = max(profit, max_profit)
-
-        return max_profit
--- valid-palindrome/Bumsu-Yi.py
+++ valid-palindrome/Bumsu-Yi.py
@@ -1,14 +1,3 @@
 """
 https://leetcode.com/problems/valid-palindrome/
 """
-
-
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        alphanumeric_chars = []
-        for char in s:
-            if char.isalnum():
-                lowercase_letter = char.lower()
-                alphanumeric_chars.append(lowercase_letter)
-
-        return alphanumeric_chars == alphanumeric_chars[::-1]
