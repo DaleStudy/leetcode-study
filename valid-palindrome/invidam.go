a19cf61baa1ed5be1e1a99d9c5d43f9a3fbdf1d6
Invidam <hansupark0208@gmail.com>
2024-04-30 18:02:18 +0900

    valid-palindrome: string concat version

--- valid-palindrome/invidam.go
+++ valid-palindrome/invidam.go
@@ -1,13 +1,12 @@
 func isPalindrome(s string) bool {
-	var sb strings.Builder
-	sb.Grow(len(s))
+	filtered := ""
 	for _, ch := range s {
 		if unicode.IsLetter(ch) || unicode.IsNumber(ch) {
-			sb.WriteRune(unicode.ToLower(ch))
+			filtered += string(unicode.ToLower(ch))
 		}
 	}
-	filtered := sb.String()
-	for ldx, rdx := 0, len(filtered)-1; ldx < rdx; ldx, rdx = ldx+1, rdx-1 {
+
+	for ldx, rdx := 0, len(filtered) - 1; ldx < rdx; ldx, rdx = ldx + 1, rdx - 1 {
 		if filtered[ldx] != filtered[rdx] {
 			return false
 		}
