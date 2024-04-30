555ea0581d3a3934e6283df64215be6460ec5059
Invidam <hansupark0208@gmail.com>
2024-04-30 18:22:48 +0900

    valid-palindrome: map function version

--- valid-palindrome/invidam.go
+++ valid-palindrome/invidam.go
@@ -1,10 +1,10 @@
 func isPalindrome(s string) bool {
-	filtered := ""
-	for _, ch := range s {
-		if unicode.IsLetter(ch) || unicode.IsNumber(ch) {
-			filtered += string(unicode.ToLower(ch))
+	filtered := strings.Map(func(r rune) rune {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+			return -1
 		}
-	}
+		return unicode.ToLower(r)
+	}, s)
 
 	for ldx, rdx := 0, len(filtered) - 1; ldx < rdx; ldx, rdx = ldx + 1, rdx - 1 {
 		if filtered[ldx] != filtered[rdx] {
