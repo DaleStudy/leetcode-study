8da6d78505dde45334f50f5a496e08998112dd62
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-05-03 23:29:42 +0900

    changed the readme and solved valid-parentheses

---
+++ valid-parentheses/SamTheKorean.py
@@ -0,0 +1,22 @@
+# Time complexity : O(n)
+# Space complexity : O(n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses = {"[": "]", "{": "}", "(": ")"}
+        stack = []
+
+        # if ch is opening bracket
+        for ch in s:
+            if ch in parentheses:
+                stack.append(ch)
+            # if ch is closing bracket
+            else:
+                # if closing bracket comes without opening bracket previously
+                if not stack:
+                    return False
+                # if closing bracket doesnt match with the latest type of opening bracket
+                elif ch != parentheses[stack.pop()]:
+                    return False
+
+        # True if stack is empty after going through the process above
+        return not stack
