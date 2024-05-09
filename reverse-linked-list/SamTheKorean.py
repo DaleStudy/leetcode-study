955d3d23c190203bd7a6aa6dcdc8b49da58c4458
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-05-09 22:02:53 +0900

    re-solved reverse linked list

--- reverse-linked-list/SamTheKorean.py
+++ reverse-linked-list/SamTheKorean.py
@@ -2,16 +2,26 @@
 # Space complexity : O(n)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Return None when head is None
+        if not head:
+            return None
+
         stack = []
         node = head
 
         while node:
-            stack.append(node.val)
+            stack.append(node)
             node = node.next
 
+        head = stack.pop()
         node = head
-        while node:
-            node.val = stack.pop()
+
+        # End the loop when stack is empty
+        while stack:
+            node.next = stack.pop()
             node = node.next
+
+        # Set the last node's next to None to indicate the end of the reversed list
+        node.next = None
 
         return head
