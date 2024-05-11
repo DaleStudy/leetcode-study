91082d47fb8b0e40ba5662c697c9fc5785368030
khw0828 <97077535+gusdn0828@users.noreply.github.com>
2024-05-12 04:33:56 +0900

    feat: reverse-linked-list, valid-parentheses 풀이

---
+++ valid-parentheses/saysimple.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        arr = []
+
+        for c in s:
+            if c in ["(", "[", "{"]:
+                arr.append(c)
+                continue
+            else:
+                if not arr:
+                    return False
+                b = arr.pop()
+                if b == "(" and c == ")":
+                    continue
+                if b == "[" and c == "]":
+                    continue
+                if b == "{" and c == "}":
+                    continue
+                return False
+        if arr:
+            return False
+        else:
+            return True
