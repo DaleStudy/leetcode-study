31ddda82bb8cca811af1219cf7dedaf5a387c610
SeungHyun Hong <whi7ehyun@gmail.com>
2024-05-13 14:25:38 +0900

    feat: Optimize space complexity in climbStairs function

--- climbing-stairs/WhiteHyun.swift
+++ climbing-stairs/WhiteHyun.swift
@@ -8,12 +8,11 @@
 
 final class Solution {
   func climbStairs(_ n: Int) -> Int {
-    var dp: [Int] = .init(repeating: 0, count: 45)
-    dp[0] = 1
-    dp[1] = 2
-    for i in 2 ..< 45 {
-      dp[i] = dp[i - 1] + dp[i - 2]
+    var prevWays = 1
+    var prevPrevWays = 1
+    for _ in stride(from: 2, through: n, by: 1) {
+      (prevWays, prevPrevWays) = (prevPrevWays, prevWays + prevPrevWays)
     }
-    return dp[n - 1]
+    return prevPrevWays
   }
 }
