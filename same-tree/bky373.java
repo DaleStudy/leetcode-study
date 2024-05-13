f636cb162922be86bb998a061426031faf71c9b6
bky373 <bo373@naver.com>
2024-05-13 20:56:55 +0900

    [week3] solve 100. Same Tree

---
+++ same-tree/bky373.java
@@ -0,0 +1,11 @@
+/**
+ * TC: O(N)
+ * SC: O(N)
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
