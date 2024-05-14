9e8fd5f0ed158e9e80a98013d039f26501accb01
bky373 <bo373@naver.com>
2024-05-15 08:27:37 +0900

    [week3] improvement for 70. Climbing Stairs

--- climbing-stairs/bky373.java
+++ climbing-stairs/bky373.java
@@ -1,20 +1,20 @@
 class Solution {
 
     public int climbStairs(int n) {
-        int[] ans = new int[n + 1];
-        if (n <= 2) {
+        if (n == 1) {
             return n;
         }
-        ans[1] = 1;
-        ans[2] = 2;
-
+        int a1 = 1;
+        int a2 = 2;
         for (int i = 3; i < n + 1; i++) {
-            ans[i] = ans[i - 1] + ans[i - 2];
+            int a3 = a1 + a2;
+            a1 = a2;
+            a2 = a3;
         }
-        return ans[n];
+        return a2;
     }
 }
 /**
  * TC: O(N)
- * SC: O(N)
+ * SC: O(1)
  */
