62dbc8d5ce8e1e4892d0a6b5b3259413363f43d2
khw0828 <97077535+gusdn0828@users.noreply.github.com>
2024-05-19 02:52:31 +0900

    feat: 미팅룸 문제 풀이

---
+++ meeting-rooms/saysimple.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key=lambda x: x.start)
+
+        e = intervals[0].end
+
+        for i in range(1, len(intervals)):
+            if intervals[i].start < e:
+                return False
+            e = intervals[i].end
+
+        return True
