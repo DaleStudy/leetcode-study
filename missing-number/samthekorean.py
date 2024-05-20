cb1b3842fabb4c41bc373dd37620c95f029e6b7b
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-05-20 15:52:39 +0900

    solved missing number

---
+++ missing-number/samthekorean.py
@@ -0,0 +1,14 @@
+# TC : O(n)
+# SC : O(n)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # The sum of the numbers in the range(0,n)
+        sumOfNums = (1 + n) * n // 2
+
+        # Subtract every element of nums to leave only missing number
+        for num in nums:
+            sumOfNums -= num
+
+        return sumOfNums
