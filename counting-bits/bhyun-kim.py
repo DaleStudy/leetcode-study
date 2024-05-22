c540da72781bf122b86d61534f0b97809fb79848
bhyun-kim <kimbh.mail@gmail.com>
2024-05-22 15:38:25 -0500

    [bhyun-kim, 김병현] Week 4 solutions

---
+++ counting-bits/bhyun-kim.py
@@ -0,0 +1,31 @@
+"""
+338. Counting Bits
+https://leetcode.com/problems/counting-bits/description/
+
+Solution:
+    - Convert the number to binary string
+    - Sum the number of 1s in the binary string
+    - Append the sum to the output list
+    - Return the output list
+
+Time complexity: O(nlogn)
+    - The for loop runs n times
+    - The sum function runs O(logn) times
+
+Space complexity: O(n)
+    - The output list has n elements
+"""
+
+
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        output = []
+        for i in range(n+1):
+            _str = str(bin(i))[2:]
+            _sum = sum(map(int, _str.strip()))
+            output.append(_sum)
+
+        return output
---
+++ number-of-1-bits/bhyun-kim.py
@@ -0,0 +1,31 @@
+"""
+191. Number of 1 Bits
+https://leetcode.com/problems/number-of-1-bits/
+
+Solution:
+    - Create a counter
+    - For each bit in the input number:
+        - If the bit is 1:
+            - Increment the counter
+    - Return the counter
+
+Time complexity: O(1)
+    - The while loop runs 32 times
+
+Space complexity: O(1)
+    - No extra space is used
+"""
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        output = 0
+
+        i = 1 << 31
+        while(i > 0) :
+        
+            if((n & i) != 0) :
+                output += 1
+                
+            i = i // 2
+
+        return output
