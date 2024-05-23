94adc0763703701146fe805a683a320de03ee307
SamTheKorean <bumsuyi@gmail.com>
2024-05-23 13:07:05 +0900

    solve counting bit with O(n) complexity

--- counting-bits/samthekorean.py
+++ counting-bits/samthekorean.py
@@ -1,15 +1,10 @@
-# TC : O(nlog(n))
-# SC : O(n)
+# TC: O(n)
+# SC: O(n)
+# For each number from 1 to n, update the count of set bits for i based on the count for i divided by two
+# and the least significant bit (LSB) of i.
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        count = 0
-        ans = []
-
-        for i in range(n + 1):
-            while i > 0:
-                count += i % 2
-                i = i // 2
-            ans.append(count)
-            count = 0
-
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
         return ans
