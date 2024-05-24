9f0880ccef80c4ef3d952cbd01fa24916dba552c
SeungHyun Hong <whi7ehyun@gmail.com>
2024-05-24 14:54:30 +0900

    feat: Add solution for LeetCode problem 191

---
+++ number-of-1-bits/WhiteHyun.swift
@@ -0,0 +1,13 @@
+//  
+//  191. Number of 1 Bits
+//  https://leetcode.com/problems/number-of-1-bits/description/
+//  Dale-Study
+//  
+//  Created by WhiteHyun on 2024/05/19.
+//  
+
+final class Solution {
+  func hammingWeight(_ n: Int) -> Int {
+    n.nonzeroBitCount
+  }
+}
