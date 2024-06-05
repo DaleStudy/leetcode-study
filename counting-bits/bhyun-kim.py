7eb6656c40f2982409b8dc5240ec7ce3f5301db0
bhyun-kim <kimbh.mail@gmail.com>
2024-06-05 14:52:39 -0500

    reformatting

--- counting-bits/bhyun-kim.py
+++ counting-bits/bhyun-kim.py
@@ -55,11 +55,12 @@ Space complexity: O(n)
 
 from typing import List
 
+
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        output = [0] * (n+1)
+        output = [0] * (n + 1)
 
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             output[i] = output[i >> 1] + (i & 1)
 
         return output
