dde3d5d35a6afdd24cf5659955c94ef348ea54dc
leokim0922 <leo.k0922@gmail.com>
2024-06-22 13:30:25 -0700

    [Leo] 8th Week solutions (last 2 Qs)

---
+++ implement-trie-prefix-tree/Leo.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isEnd = True
+
+        ## TC: O(len(word)), SC: O(len(word))
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isEnd
+
+        ## TC: O(len(word)), SC: O(1)
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+
+        ## TC: O(len(prefix)). SC: O(1)
