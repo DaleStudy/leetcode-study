5e112c38f08ac3b5331463fc46b07b69233fe424
SeungHyun Hong <whi7ehyun@gmail.com>
2024-06-28 11:44:36 +0900

    feat: Add solution for LeetCode problem 39

--- combination-sum/WhiteHyun.swift
+++ combination-sum/WhiteHyun.swift
@@ -7,7 +7,30 @@
 //  
 
 class Solution {
-    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
-        
+  func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+    let sortedCandidates = candidates.sorted()
+    var answer: [[Int]] = []
+
+    func backtrack(_ sum: Int, _ start: Int, _ current: [Int]) {
+      if sum == target {
+        answer.append(current)
+        return
+      }
+
+      if sum > target {
+        return
+      }
+
+      for i in start ..< sortedCandidates.count {
+        let newSum = sum + sortedCandidates[i]
+        if newSum > target {
+          break
+        }
+        backtrack(newSum, i, current + [sortedCandidates[i]])
+      }
     }
+
+    backtrack(0, 0, [])
+    return answer
+  }
 }
