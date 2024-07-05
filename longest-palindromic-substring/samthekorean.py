18947f7d44e4305cdabed47dde6616a34afa6222
SamTheKorean <bumsuyi@gmail.com>
2024-07-05 18:25:10 +0900

    solve : longest palindromic substring

---
+++ longest-palindromic-substring/samthekorean.py
@@ -0,0 +1,25 @@
+# O(n^2)
+# O(1)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1 : right]
+
+        max_str = s[0]
+
+        for i in range(len(s) - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
