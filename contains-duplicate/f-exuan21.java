1c880abd15194b94223591df042453165521f3b0
f-exuan21 <f.exuan21@gmail.com>
2024-08-12 21:12:35 +0900

    contains-duplicate solution

---
+++ contains-duplicate/f-exuan21.java
@@ -0,0 +1,15 @@
+// time : O(n)
+// space : O(n)
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            if(set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
