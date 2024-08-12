87bb8ead91b6f7306993c0b9ca6a2ad38cba05ba
crispy <dltmdcks702@cau.ac.kr>
2024-08-12 20:57:25 +0900

    solve: contains-duplicate

---
+++ contains-duplicate/crispy.cpp
@@ -0,0 +1,18 @@
+
+#include <set>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> count;
+
+        for(int num : nums) {
+            if (count.find(num) != count.end()) {
+                return true;
+            }
+            count.insert(num);
+        }
+
+        return false;
+    }
+};
