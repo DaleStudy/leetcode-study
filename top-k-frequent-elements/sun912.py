4841b41a2b2b92193015bc21abd8de8adcfae8ca
sun912 <gslee100@gmail.com>
2024-08-13 22:09:05 +0900

    top-k-frequent-elements solution

---
+++ top-k-frequent-elements/sun912.py
@@ -0,0 +1,24 @@
+"""
+    TC : O(klogn)
+
+    used max heap
+    index -> count number
+    list value -> frequent nums
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for key, val in count.items():
+            freq[val].append(key)
+
+        result = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
