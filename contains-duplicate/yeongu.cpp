a457daec48243fda0b795bb4c6413effe41cf350
YeonguChoe <yeongu.choe@icloud.com>
2024-08-13 03:09:11 -0400

    Update yeongu.cpp

--- contains-duplicate/yeongu.cpp
+++ contains-duplicate/yeongu.cpp
@@ -1,3 +1,6 @@
+// Time complexity: o(n log n) 왜냐하면, for 문이 n이고 find 연산이 log n이기 때문.
+// Space complexity: O(n) 왜냐하면, 다 다른경우 n
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
