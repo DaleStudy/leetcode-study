5b4f4292349490d001273427bbf2106434a8f18f
HC-kang <weston0713@gmail.com>
2024-08-13 09:06:03 +0900

    Feat: add an awesome code from leetcode

--- kth-smallest-element-in-a-bst/HC-kang.ts
+++ kth-smallest-element-in-a-bst/HC-kang.ts
@@ -48,3 +48,60 @@ function kthSmallest(root: TreeNode | null, k: number): number {
   inorder(root, arr);
   return arr[k - 1];
 }
+
+/**
+ * Awesome solution from leetcode
+ */
+function kthSmallest(root: TreeNode | null, k: number): number {
+  /*
+  define. return the kth smallest value in a BST.
+  assess. smallest value can be 0. At least k nodes, which can be at its smallest, 1.
+  approach. DFS with backtracking. Traverse down the left edges until we hit null. if k is 1, return that value. Else, backtrack, go right, then try to go left again. Use a stack.
+  */
+
+  // let currentRank = 0;
+
+  // let stack = [];
+
+  // let currentNode = root;
+
+  // while (currentNode || stack.length > 0) {
+  //     while (currentNode) {
+  //         stack.push(currentNode);
+
+  //         currentNode = currentNode.left;
+  //     }
+
+  //     currentNode = stack.pop();
+
+  //     currentRank++;
+
+  //     if (currentRank === k) return currentNode.val;
+
+  //     currentNode = currentNode.right;
+  // }
+
+  const stack = [];
+
+  let currentRank = 0;
+
+  let currentNode = root;
+
+  while (currentNode || stack.length > 0) {
+
+      while (currentNode) {
+          stack.push(currentNode);
+
+          currentNode = currentNode.left;
+      }
+
+      currentNode = stack.pop();
+
+      currentRank++;
+
+      if (currentRank === k) return currentNode.val;
+
+      currentNode = currentNode.right;
+  }
+
+};
