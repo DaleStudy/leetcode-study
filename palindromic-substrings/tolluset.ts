381f802f24abdf73cc8bfdf1c9c2aca51db98579
tolluset <dlqud19@gmail.com>
2024-08-16 02:11:57 +0900

    refacotr: rename variables more descriptive

--- palindromic-substrings/tolluset.ts
+++ palindromic-substrings/tolluset.ts
@@ -3,36 +3,40 @@
  * SC: O(n)
  * */
 function countSubstrings(s: string): number {
-  const str = "#" + s.split("").join("#") + "#";
-  const len = str.length;
-  const pal = new Array(len).fill(0);
-  let center = 0,
-    radius = 0,
-    total = 0;
+  const transformedString = "#" + s.split("").join("#") + "#";
+  const transformedStringLength = transformedString.length;
+  const palindromeLengths = new Array(transformedStringLength).fill(0);
+  let currentCenter = 0,
+    rightBoundary = 0,
+    totalPalindromeCount = 0;
 
-  for (let i = 0; i < len; i++) {
+  for (let i = 0; i < transformedStringLength; i++) {
     // If i is within the rightmost center, copy the palindromes value from the mirror
-    if (i < radius) {
-      pal[i] = Math.min(radius - i, pal[center * 2 - i]);
+    if (i < rightBoundary) {
+      palindromeLengths[i] = Math.min(
+        rightBoundary - i,
+        palindromeLengths[currentCenter * 2 - i],
+      );
     }
 
     // Expand around i until it's not a palindrome and not over left or right
     while (
-      i + pal[i] + 1 < len &&
-      i - pal[i] - 1 >= 0 &&
-      str[i + pal[i] + 1] === str[i - pal[i] - 1]
+      i + palindromeLengths[i] + 1 < transformedStringLength &&
+      i - palindromeLengths[i] - 1 >= 0 &&
+      transformedString[i + palindromeLengths[i] + 1] ===
+        transformedString[i - palindromeLengths[i] - 1]
     ) {
-      pal[i]++;
+      palindromeLengths[i]++;
     }
 
     // If palindromes value is the new rightmost center, update center and right
-    if (i + pal[i] > radius) {
-      center = i;
-      radius = i + pal[i];
+    if (i + palindromeLengths[i] > radius) {
+      currentCenter = i;
+      rightBoundary = i + palindromeLengths[i];
     }
 
     // Add the number of palindromes with center i to the result
-    total += Math.floor((pal[i] + 1) / 2);
+    total += Math.floor((palindromeLengths[i] + 1) / 2);
   }
 
   return total;
