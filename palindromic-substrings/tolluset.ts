2ad5c0efa0ff36f40325c4aa728d947b37863280
tolluset <dlqud19@gmail.com>
2024-08-16 01:58:54 +0900

    refacotr: rename variables more clearly

--- palindromic-substrings/tolluset.ts
+++ palindromic-substrings/tolluset.ts
@@ -5,35 +5,35 @@
 function countSubstrings(s: string): number {
   const str = "#" + s.split("").join("#") + "#";
   const len = str.length;
-  const pit = new Array(len).fill(0);
+  const pal = new Array(len).fill(0);
   let center = 0,
-    right = 0,
-    result = 0;
+    radius = 0,
+    total = 0;
 
   for (let i = 0; i < len; i++) {
-    // If i is within the rightmost center, copy the pit value from the mirror
-    if (i < right) {
-      pit[i] = Math.min(right - i, pit[center * 2 - i]);
+    // If i is within the rightmost center, copy the palindromes value from the mirror
+    if (i < radius) {
+      pal[i] = Math.min(radius - i, pal[center * 2 - i]);
     }
 
     // Expand around i until it's not a palindrome and not over left or right
     while (
-      i + pit[i] + 1 < len &&
-      i - pit[i] - 1 >= 0 &&
-      str[i + pit[i] + 1] === str[i - pit[i] - 1]
+      i + pal[i] + 1 < len &&
+      i - pal[i] - 1 >= 0 &&
+      str[i + pal[i] + 1] === str[i - pal[i] - 1]
     ) {
-      pit[i]++;
+      pal[i]++;
     }
 
-    // If pit value is the new rightmost center, update center and right
-    if (i + pit[i] > right) {
+    // If palindromes value is the new rightmost center, update center and right
+    if (i + pal[i] > radius) {
       center = i;
-      right = i + pit[i];
+      radius = i + pal[i];
     }
 
     // Add the number of palindromes with center i to the result
-    result += Math.floor((pit[i] + 1) / 2);
+    total += Math.floor((pal[i] + 1) / 2);
   }
 
-  return result;
+  return total;
 }
