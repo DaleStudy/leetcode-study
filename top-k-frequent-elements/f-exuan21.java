a069025b892e7c7cc8345588d5408507b93979cc
A-Hyeon <71107963+f-exuan21@users.noreply.github.com>
2024-08-16 22:06:38 +0900

    Update f-exuan21.java
    시간복잡도 공간복잡도 수정

--- top-k-frequent-elements/f-exuan21.java
+++ top-k-frequent-elements/f-exuan21.java
@@ -24,12 +24,16 @@ class Solution {
     }
 }
 
-// time : O(n) + O(m log m) + O(k log m) = O(n + m*logm + k*logm)
-// 
-// 최악의 경우 n log n 이 될 수 있음
-// space : O(m) + O(m) + O(k) = O(m + k)
-// 최악의 경우 n + k 가 될 수 있음
-
 // n : nums의 길이
 // m : nums에서 서로 다른 숫자의 개수
+
+// time : O(n) + O(m*logm) + O(k*logm) = O(n + m*logm + k*logm)
+// 최악의 경우, nums 가 다 unique 하기 때문에 n == m == k 가 됨
+// 따라서, O(n*logn)
+
+// space : O(m) + O(m) + O(k) = O(m + k)
+// 최악의 경우 n == m == k 가 됨
+// 따라서, O(n)
+
+
 
