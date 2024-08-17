0faaeae8395851c8935561fa54b5dd5b56b5b895
jinbeom <kst20026@gamil.com>
2024-08-17 16:19:46 +0900

    Refactor Contains Duplicate

--- contains-duplicate/kayden.py
+++ contains-duplicate/kayden.py
@@ -2,5 +2,4 @@
 # 공간복잡도: O(N)
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        unique_nums = set(nums)
-        return len(unique_nums) != len(nums)
+        return len(set(nums)) != len(nums)
