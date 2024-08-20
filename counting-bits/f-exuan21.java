af285fb2532b5f993903628052cd6d0736405cb3
A-Hyeon <71107963+f-exuan21@users.noreply.github.com>
2024-08-20 20:20:27 +0900

    Create f-exuan21.java
    Counting bits solutions

---
+++ counting-bits/f-exuan21.java
@@ -0,0 +1,15 @@
+//time : O(n)
+//space : O(n)
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] answer = new int[n + 1];
+
+        answer[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            answer[i] = answer[i >> 1] + (i & 1);
+        }
+
+        return answer;
+    }
+}
