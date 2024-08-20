3f815c0825cd73b439beb8d1c933e0db85d7ee2d
devtkwon <devtkwon@gmail.com>
2024-08-20 22:55:25 +0900

    valid-anagram

---
+++ valid-anagram/taekwon-dev.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+}
