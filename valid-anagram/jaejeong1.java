5991bb9d7aeabcdb90fd50622ecfea30d4e9e949
wad-jangjaejeong <jaejeong@catchtable.co.kr>
2024-08-22 22:21:24 +0900

    피드백 반영

--- valid-anagram/jaejeong1.java
+++ valid-anagram/jaejeong1.java
@@ -6,7 +6,7 @@ class SolutionValidAnagram {
     // 풀이: 해시맵을 사용해 s와 t의 문자 별 빈도수를 저장한다
     // 두 빈도수의 모든 키와 값이 같고, 크기가 같은지 비교한다.
     // 다르다면 false를 반환, 모두 같다면 true를 반환한다.
-    // 시간복잡도: O(N), 공간복잡도: O(N)
+    // 시간복잡도: O(N), 공간복잡도: O(1)
 
     Map<Character, Integer> sAnagram = createAnagramMap(s);
     Map<Character, Integer> tAnagram = createAnagramMap(t);
