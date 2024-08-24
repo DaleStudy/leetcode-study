605c596b31782c5528f9be994ef63fb713848cbf
유하준 <hajuny129@gmail.com>
2024-08-25 01:37:52 +0900

    [LC] feat: 240824 encode-and-decode-string 2

--- encode-and-decode-strings/hajunyoo.py
+++ encode-and-decode-strings/hajunyoo.py
@@ -16,3 +16,32 @@ class Solution1:
     # space complexity: O(1)
     def decode(self, str):
         return str.split(":;")
+
+class Solution2:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    # time complexity: O(n)
+    # space complexity: O(1)
+    def encode(self, strs):
+        txt = ""
+        for s in strs:
+            txt += str(len(s)) + ":" + s
+        return txt
+
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    # time complexity: O(n)
+    # space complexity: O(1)
+    def decode(self, str):
+        res = []
+        i = 0
+        while i < len(str):
+            colon = str.find(":", i)
+            length = int(str[i:colon])
+            res.append(str[colon + 1:colon + 1 + length])
+            i = colon + 1 + length
+        return res
