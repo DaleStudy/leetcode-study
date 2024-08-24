3cfbbfee27a73817adf4f455c756a9966923a897
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-08-25 00:00:58 +0900

    Construct Binary Tree from Preorder and Inorder Traversal Solution

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal/naringst.js
@@ -0,0 +1,28 @@
+/**
+ * preorder는 val -> left -> right
+ * inorder는 left -> val -> right
+ * 따라서 preorder의 val을 기준으로 inorder 배열을 왼, 오로 나누면 val의 왼쪽 트리, 오른쪽 트리 구조가 된다.
+ *
+ * node.val = preorder[i]
+ * const leftAndRight = inorder.split(preorder[0])
+ * node.left = leftAndRight[0]
+ * node.right = leftAndRight[1]
+ *
+ * 구현이 안되어 코드 참고 ...
+ *
+ */
+
+if (preorder.length === 0 || inorder.length === 0) {
+  return null;
+}
+const root = new TreeNode(preorder[0]);
+const rootIndex = inorder.indexOf(root.val);
+root.left = buildTree(
+  preorder.slice(1, rootIndex + 1),
+  inorder.slice(0, rootIndex)
+);
+root.right = buildTree(
+  preorder.slice(rootIndex + 1, preorder.length),
+  inorder.slice(rootIndex + 1, inorder.length)
+);
+return root;
