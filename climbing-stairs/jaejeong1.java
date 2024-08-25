236e5ea69c116a2c8ff90416197a34b2fd72113e
wad-jangjaejeong <jaejeong@catchtable.co.kr>
2024-08-25 22:58:40 +0900

    climbing stairs solution

---
+++ climbing-stairs/jaejeong1.java
@@ -0,0 +1,27 @@
+class SolutionClimbStairs {
+
+  public static void main(String[] args) {
+    SolutionClimbStairs s = new SolutionClimbStairs();
+    System.out.println(s.climbStairs(3));
+  }
+
+  public int climbStairs(int n) {
+    if (n == 1) {
+      return 1;
+    }
+
+    if (n == 2) {
+      return 2;
+    }
+
+    int[] stairs = new int[n+1];
+    stairs[1] = 1;
+    stairs[2] = 2;
+
+    for (int i=3; i<=n; i++) {
+      stairs[i] = stairs[i-1] + stairs[i-2];
+    }
+
+    return stairs[n];
+  }
+}
