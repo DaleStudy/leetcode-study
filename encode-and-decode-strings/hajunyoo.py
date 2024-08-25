5dd8db8dad324985350f7d63363ad4cac5d7785b
유하준 <hajuny129@gmail.com>
2024-08-25 18:20:19 +0900

    [LC] feat: 240825 construct binary tree from preorder and inorder traversal

--- encode-and-decode-strings/hajunyoo.py
+++ encode-and-decode-strings/hajunyoo.py
@@ -1,27 +1,15 @@
 class Solution1:
-    """
-    @param: strs: a list of strings
-    @return: encodes a list of strings to a single string.
-    """
     # time complexity: O(n)
     # space complexity: O(1)
     def encode(self, strs):
         return ":;".join(strs)
 
-    """
-    @param: str: A string
-    @return: decodes a single string to a list of strings
-    """
     # time complexity: O(n)
     # space complexity: O(1)
     def decode(self, str):
         return str.split(":;")
 
 class Solution2:
-    """
-    @param: strs: a list of strings
-    @return: encodes a list of strings to a single string.
-    """
     # time complexity: O(n)
     # space complexity: O(1)
     def encode(self, strs):
@@ -30,10 +18,6 @@ class Solution2:
             txt += str(len(s)) + ":" + s
         return txt
 
-    """
-    @param: str: A string
-    @return: decodes a single string to a list of strings
-    """
     # time complexity: O(n)
     # space complexity: O(1)
     def decode(self, str):
