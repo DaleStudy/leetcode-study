7e66c5416fdb98205ee126bdc5de874a1851cc7b
HC-kang <weston0713@gmail.com>
2024-08-27 08:29:55 +0900

    simplify: 70. Climbing Stairs

--- climbing-stairs/HC-kang.ts
+++ climbing-stairs/HC-kang.ts
@@ -3,11 +3,9 @@
 function climbStairs(n: number): number {
   let p = 0;
   let q = 1;
-  let t: number;
   for (let i = 0; i < n; i++) {
-    t = p + q;
-    p = q;
-    q = t;
+    q = q + p;
+    p = q - p;
   }
   return q;
 }
