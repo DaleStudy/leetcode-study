0363257a6f4b9a2c1d9a8fb5810b280879bcc1c2
Young Kim <younghyun87@gmail.com>
2024-08-26 23:21:39 -0400

    Climbing Stairs solution

---
+++ climbing-stairs/kimyoung.js
@@ -0,0 +1,11 @@
+// found a pattern where the result is the addition of two previous elements,
+// but not sure if this is the expected answer
+var climbStairs = function (n) { 
+    let arr = new Array(n).fill(1);
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return n === 1 ? 1 : arr[n];
+};
+
+// time - O(n) iterate up to n times
