1090e3214fb17ecb12f4d2f4bf423d4c2c3d6299
mykoo <mykoo@eluocnc.com>
2024-08-30 14:36:11 +0900

    two-sum  qhrwkqeh cnrk

--- two-sum/GUMUNYEONG.js
+++ two-sum/GUMUNYEONG.js
@@ -37,3 +37,12 @@ var twoSum = function (nums, target) {
     }
 
 };
+
+// TC : O(n)
+// 1. num 을 순회하며 numPair 객체를 만듦(num 길이 n)
+// 2. numPair 를 순회하며 키-값이 대칭되는 첫번째 쌍을 찾음 (numPair 길이 n)
+// 3. num을 순회하며 인덱스를 찾음 (num 길이 n)
+// O(3n) 따라서 시간복잡도는 O(n)
+
+// SC : O(n)
+// 크기가 n만큼인 객체(numPair)를 생성하므로 공간 복잡도도 O(n)
