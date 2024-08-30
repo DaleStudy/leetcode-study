3013ae5ccc64ad517292404029f50ec997b10fae
장재정 [캐테서비스] <jaejeong@catchtable.co.kr>
2024-08-31 00:11:33 +0900

    line break + 설명 추가

--- climbing-stairs/jaejeong1.java
+++ climbing-stairs/jaejeong1.java
@@ -1,6 +1,12 @@
 class SolutionClimbStairs {
 
   public int climbStairs(int n) {
+    // n번째 계단까지 오르는 방법의 수
+    // 첫 번째 계단까지 오르는 방법은 1가지
+    // 두 번째 계단까지 오르는 방법은 2가지
+    // 세 번째 계단부터는 이전 두 계단의 방법을 더하여 계산
+    // stairs[i]는 i번째 계단까지 오르는 방법의 수
+    // 시간복잡도: O(N), 공간복잡도: O(N)
     if (n == 1) {
       return 1;
     }
@@ -12,6 +18,7 @@ class SolutionClimbStairs {
     int[] stairs = new int[n+1];
     stairs[1] = 1;
     stairs[2] = 2;
+
 
     for (int i=3; i<=n; i++) {
       stairs[i] = stairs[i-1] + stairs[i-2];
