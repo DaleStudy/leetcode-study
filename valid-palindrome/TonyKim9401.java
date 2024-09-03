aaf34871480657907815ba7b41e319271e51fdb0
ktony <k.tony9412@gmail.com>
2024-09-03 02:20:43 -0400

    Valid Palindrome   Code refactoring

--- valid-palindrome/TonyKim9401.java
+++ valid-palindrome/TonyKim9401.java
@@ -2,35 +2,12 @@
 // SC: O(n)
 class Solution {
     public boolean isPalindrome(String s) {
-        String target = checkString(s);
-        return checkPalindrome(target);
-    }
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-    private String checkString(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c >= 'a' && c <= 'z') {
-                sb.append(c);
-            }
-            if (c >= 'A' && c <= 'Z') {
-                c = (char)(c - 'A' + 'a');
-                sb.append(c);
-            }
-            if (c >= '0' && c <= '9') {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
+        if (s.length() == 1) return true;
 
-    private Boolean checkPalindrome(String target) {
-        int start = 0;
-        int end = target.length() - 1;
-
-        while (start < end) {
-            if (target.charAt(start) != target.charAt(end)) return false;
-            start += 1;
-            end -= 1;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
         }
         return true;
     }
