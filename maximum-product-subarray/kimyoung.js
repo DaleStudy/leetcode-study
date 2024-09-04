0f4e7cd98f35e29ae30521222d279c7dca5f6bf3
Young Kim <younghyun87@gmail.com>
2024-09-03 23:58:13 -0400

    Optimized brute force approach

--- maximum-product-subarray/kimyoung.js
+++ maximum-product-subarray/kimyoung.js
@@ -1,23 +1,16 @@
-var maxProduct = function (nums) { // brute force approach
-    let subarrays = [];
+var maxProduct = function (nums) { // brute force approach - doesn't pass leetcode (Time Limit Exceeded)
+    let maxProduct = -Infinity;
     for (let i = 0; i < nums.length; i++) { // get subarrays
         for (let j = i; j < nums.length; j++) {
-            subarrays.push(nums.slice(i, j + 1));
+            let prod = nums.slice(i, j + 1).reduce((acc, el) => acc *= el, 1);
+            maxProduct = Math.max(prod, maxProduct);
         }
     }
-
-    let maxProduct = 0;
-    subarrays.forEach(arr => { // find product of each subarray and compare to maxProduct
-        let prod = arr.reduce((acc, el) => acc *= el, 1);
-        max = Math.max(prod, max);
-    })
-
     return maxProduct;
 };
 
 // time - O(n^2) double for loop
 // space - O(n^2) total count of subarrays = n*(n+1)/2
-
 
 
 // TODO - different approach
