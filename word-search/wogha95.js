649b32c731978a2d39238598972894770bca8804
wogha95 <wogha95@naver.com>
2024-09-06 21:31:43 +0900

    solve: word search

--- word-search/wogha95.js
+++ word-search/wogha95.js
@@ -1,3 +1,65 @@
+// 2차
+// result flag 변수를 활용 => boolean 반환으로 개선
+// TC: O(M * N * 4^W)
+// SC: O(MIN)
+// W: word.length, MIN: min(M * N, W)
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[0].length; c++) {
+      if (dfs(r, c, 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+
+  function dfs(row, column, wordIndex) {
+    if (!isValid(row, column)) {
+      return false;
+    }
+    if (board[row][column] !== word[wordIndex]) {
+      return false;
+    }
+    if (wordIndex === word.length - 1) {
+      return true;
+    }
+
+    const temp = board[row][column];
+    board[row][column] = "#";
+
+    if (
+      dfs(row + 1, column, wordIndex + 1) ||
+      dfs(row - 1, column, wordIndex + 1) ||
+      dfs(row, column + 1, wordIndex + 1) ||
+      dfs(row, column - 1, wordIndex + 1)
+    ) {
+      return true;
+    }
+
+    board[row][column] = temp;
+
+    return false;
+  }
+
+  function isValid(row, column) {
+    if (row < 0 || board.length <= row) {
+      return false;
+    }
+    if (column < 0 || board[0].length <= column) {
+      return false;
+    }
+    return true;
+  }
+};
+
+// 1차
 // TC: O(M * N * 4^W)
 // SC: O(MIN)
 // W: word.length, MIN: min(M * N, W)
