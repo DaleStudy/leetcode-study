c7fb410f5c747b0b7f7a49f43607cd1c83246b18
tolluset <dlqud19@gmail.com>
2024-09-09 00:18:23 +0900

    solve: best time to by and sell stock 2

--- best-time-to-buy-and-sell-stock/tolluset.ts
+++ best-time-to-buy-and-sell-stock/tolluset.ts
@@ -1,4 +1,36 @@
 /*
+ * TC: O(n)
+ * SC: O(1)
+ * */
+function maxProfitV2(prices: number[]): number {
+  const n = prices.length;
+
+  let min = Infinity,
+    max = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+      continue;
+    }
+
+    if (prices[i] - min > max) {
+      max = prices[i] - min;
+      continue;
+    }
+  }
+
+  return max;
+}
+
+const tc1V2 = maxProfitV2([7, 1, 5, 3, 6, 4]);
+console.info("🚀 : tolluset.ts:27: tc1V2=", tc1V2); // 5
+
+const tc2V2 = maxProfitV2([7, 6, 4, 3, 1]);
+console.info("🚀 : tolluset.ts:30: tc2V2=", tc2V2); // 0
+
+/*
+ * @FAILED: Time Limit Exceeded
  * TC: O(n^2)
  * SC: O(1)
  * */
