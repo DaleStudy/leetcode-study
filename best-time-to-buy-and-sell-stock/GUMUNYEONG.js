06ae7fdfd0d9fe1e8f6cc611042707997e3d46c1
mykoo <mykoo@eluocnc.com>
2024-09-15 14:31:27 +0900

    best-time-to-buy-and-sell-stock

--- best-time-to-buy-and-sell-stock/GUMUNYEONG.js
+++ best-time-to-buy-and-sell-stock/GUMUNYEONG.js
@@ -5,7 +5,7 @@
 var maxProfit = function (prices) {
     let max = 0;
 
-    for (let i = 1; i <= prices.length; i++) {
+    for (let i = 0; i <= prices.length; i++) {
         for (let j = i + 1; j <= prices.length; j++) {
             const profit = prices[j] - prices[i];
             max = profit > max ? profit : max;
