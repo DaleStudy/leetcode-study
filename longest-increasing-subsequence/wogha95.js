e1c705d41e8d402ce04c4545d74377b523395a7a
wogha95 <wogha95@naver.com>
2024-09-18 14:07:03 +0900

    solve: longest increasing subsequence

---
+++ longest-increasing-subsequence/wogha95.js
@@ -0,0 +1,28 @@
+/**
+ * TC: O(N^2)
+ * SC: O(N)
+ * N: nums.length
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  // 각자 스스로는 최소 1의 lengthOfLIS를 가짐
+  const longestLength = new Array(nums.length).fill(1);
+
+  // nums배열의 right까지 원소들 중 lengthOfLIS를 저장
+  for (let right = 1; right < nums.length; right++) {
+    for (let left = 0; left < right; left++) {
+      if (nums[left] < nums[right]) {
+        longestLength[right] = Math.max(
+          longestLength[right],
+          longestLength[left] + 1
+        );
+      }
+    }
+  }
+
+  return Math.max(...longestLength);
+};
