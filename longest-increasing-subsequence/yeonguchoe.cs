afecf62f1def06636512ff738714bdf496deb31f
YeonguChoe <yeongu.choe@icloud.com>
2024-09-19 19:59:16 -0400

    Create yeonguchoe.cs

---
+++ longest-increasing-subsequence/yeonguchoe.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        if (nums.Length == 0) return 0;
+
+        int[] dp = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < nums.Length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxLength = 1;
+        foreach (int length in dp) {
+            maxLength = Math.Max(maxLength, length);
+        }
+        
+        return maxLength;
+    }
+}
