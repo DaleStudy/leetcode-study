3d78960e74d410b6b15f30db976b5caa78bac26f
hyejjun <hjlee9725@gmail.com>
2024-09-20 14:00:33 +0900

    Valid Parentheses

---
+++ valid-parentheses/hyejjun.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
