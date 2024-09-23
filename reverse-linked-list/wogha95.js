4b1c111ef572bb243fad9fd7c7946e65d20c02c9
wogha95 <wogha95@naver.com>
2024-09-23 20:57:43 +0900

    solve: reverse linked list

--- reverse-linked-list/wogha95.js
+++ reverse-linked-list/wogha95.js
@@ -1,6 +1,47 @@
 /**
+ * 2차
+ * Tony님 풀이 참고해서 SC 개선
+ *
  * TC: O(N)
  * SC: O(1)
+ * 순회동안 노드 생성하지 않으므로 공간복잡도가 상수다.
+ *
+ * N: linked-list length
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let pointer = null;
+
+  while (head !== null) {
+    let temp = head.next;
+    head.next = pointer;
+    pointer = head;
+    head = temp;
+  }
+
+  return pointer;
+};
+
+/**
+ * 1차
+ * TC: O(N)
+ * linked-list 길이 만큼 순회
+ *
+ * SC: O(N)
+ * linked-list 길이만큼 생성
+ *
+ * N: linked-list length
  */
 
 /**
