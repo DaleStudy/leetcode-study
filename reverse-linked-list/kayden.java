9a4fe5e84142b23565e4e56e630deda519309a91
Jinbeom <kst20026@gamil.com>
2024-09-23 15:38:07 +0900

    Refactor Reverse Linked List

--- reverse-linked-list/kayden.java
+++ reverse-linked-list/kayden.java
@@ -12,13 +12,13 @@
 // 공간복잡도: O(1)
 class Solution {
     public ListNode reverseList(ListNode head) {
-
         ListNode prev = null;
 
         while (head != null){
-            ListNode curr = new ListNode(head.val, prev);
-            prev = curr;
-            head = head.next;
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
 
         return prev;
