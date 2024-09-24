6c88f0e5ffc53963132502d39658c5ab374a81f5
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-09-25 00:31:23 +0900

    Longest Substring Without Repeating Characters

---
+++ longest-substring-without-repeating-characters/naringst.py
@@ -0,0 +1,22 @@
+
+#  Runtime: 51ms, Memory: 16.83MB
+#  Time complexity: O(len(s))
+#  Space complexity: O(len(s))
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        stringArr = []
+        maxLength = 0
+
+        for sub in s :
+            if sub in stringArr :    
+                maxLength = max(maxLength, len(stringArr))
+                repeatIdx = stringArr.index(sub)
+                stringArr = stringArr[repeatIdx+1 :]
+
+            stringArr.append(sub)
+        
+        maxLength = max(maxLength, len(stringArr))
+
+
+        return maxLength
