5c0a937532d71c8d6f5e8b890612e7e105b1eb6e
HC-kang <weston0713@gmail.com>
2024-09-30 08:07:16 +0900

    Feat: 21. Merge Two Sorted Lists

---
+++ merge-two-sorted-lists/HC-kang.ts
@@ -0,0 +1,31 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  let head = new ListNode();
+  let current = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return head.next;
+}
