78bec4d793cb6ab45eacef50e3f9819047f79b39
ktony <k.tony9412@gmail.com>
2024-10-06 18:08:47 -0400

    Linked List Cycle

---
+++ linked-list-cycle/TonyKim9401.java
@@ -0,0 +1,15 @@
+// TC: O(n)
+// SC: O(1)
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
