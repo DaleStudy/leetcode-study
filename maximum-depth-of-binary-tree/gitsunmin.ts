b0dbcb23d6f536bdee4d044dd88c91d76212612b
김선민 <tjsals116@gmail.com>
2024-10-26 22:30:04 +0900

    Add week 11 solutions: maximum-depth-of-binary-tree

---
+++ maximum-depth-of-binary-tree/gitsunmin.ts
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * time complexity : O(n)
+ * space complexity : O(n)
+ */
+
+export class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+export function maxDepth(root: TreeNode | null): number {
+    if (!root) return 0;
+
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
