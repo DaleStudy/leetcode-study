cdeeb80862b99b5fa0b57af32bdee8500745d341
JEONGHWANMIN <mimi0j0j@naver.com>
2024-11-02 12:33:46 +0900

    feat: 문제풀이 추가

---
+++ same-tree/hwanmini.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if ((p && !q) || (!p && q)) return false;
+    if (!p || !q) return true
+    if (p.val === q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    return false
+};
+
