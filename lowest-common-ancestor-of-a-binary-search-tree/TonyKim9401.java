68c9451a7c93a3fd980e66be9c1a2f54800479c8
ktony <k.tony9412@gmail.com>
2024-11-08 22:07:14 -0500

    Lowest Common Ancestor Of A Binary Search Tree

--- lowest-common-ancestor-of-a-binary-search-tree/TonyKim9401.java
+++ lowest-common-ancestor-of-a-binary-search-tree/TonyKim9401.java
@@ -1,31 +1,13 @@
-// TC: O(n)
-// need to check all nodes
-// SC: O(h)
-// h can be O(n) in the worst case, O(log n) in the best case
+// TC: O(h)
+// h = the high of binary search tree
+// SC: O(1)
+// Doesn't require additional space
 class Solution {
-    public TreeNode lcaDeepestLeaves(TreeNode root) {
-        return dfs(root, 0).node;
-    }
-
-    private ResultNode dfs(TreeNode node, int depth) {
-        if (node == null) return new ResultNode(null, depth);
-
-        depth += 1;
-        ResultNode leftNode = dfs(node.left, depth);
-        ResultNode rightNode = dfs(node.right, depth);
-
-        if (leftNode.depth == rightNode.depth) return new ResultNode(node, leftNode.depth);
-        else if (leftNode.depth > rightNode.depth) return leftNode;
-        else return rightNode;
-    }
-
-    private class ResultNode {
-        private TreeNode node;
-        private int depth;
-
-        ResultNode(TreeNode node, int depth) {
-            this.node = node;
-            this.depth = depth;
-        }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        if (p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        return root;
     }
 }
