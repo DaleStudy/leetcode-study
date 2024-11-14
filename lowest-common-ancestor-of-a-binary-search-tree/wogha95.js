e354f1d0614fc5f609579ccb4d104e022f75eb3e
wogha95 <wogha95@naver.com>
2024-11-14 18:30:26 +0900

    solve: lowest common ancestor of a binary search tree

---
+++ lowest-common-ancestor-of-a-binary-search-tree/wogha95.js
@@ -0,0 +1,32 @@
+/**
+ * TC: O(logN)
+ * left와 right 중 한 곳으로만 탐색
+ *
+ * SC: O(1)
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  while (root) {
+    if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else {
+      return root;
+    }
+  }
+};
