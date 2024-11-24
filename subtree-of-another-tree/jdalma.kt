3803716931abbf6b6c44784b66fb7786171663ea
Hyunjun Jeong <jeongdalma@gmail.com>
2024-11-24 09:55:39 +0900

    Update subtree-of-another-tree/jdalma.kt
    Co-authored-by: Dale Seo <5466341+DaleSeo@users.noreply.github.com>

--- subtree-of-another-tree/jdalma.kt
+++ subtree-of-another-tree/jdalma.kt
@@ -14,8 +14,8 @@ class `subtree-of-another-tree` {
 
     private fun isSame(root: TreeNode?, subRoot: TreeNode?): Boolean {
         if (root == null && subRoot == null) return true
-        else if (root == null || subRoot == null) return false
-        else if (root.`val` != subRoot.`val`) return false
+        if (root == null || subRoot == null) return false
+        if (root.`val` != subRoot.`val`) return false
 
         return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right)
     }
