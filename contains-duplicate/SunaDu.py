f4ac4335b61246204fdbc0e2929249cab8ba028b
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-12-04 15:26:45 +0900

    add solution: contains duplicate

---
+++ contains-duplicate/SunaDu.py
@@ -0,0 +1,22 @@
+'''
+# Leetcode 217. Contains Duplicate
+
+use set to store distinct elements 🗂️
+
+## Time and Space Complexity
+
+```
+TC: O(n)
+SC: O(n)
+```
+
+### TC is O(n):
+- iterating through the list just once to convert it to a set.
+
+### SC is O(n):
+- creating a set to store the distinct elements of the list.
+'''
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
