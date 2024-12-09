4b99ac73029188d010910753ea56d1c08511e322
sungjinwi <0202wsj@gmail.com>
2024-12-10 00:34:35 +0900

    Docs : complexity calc for containDuplicate

--- contains-duplicate/sungjinwi.c
+++ contains-duplicate/sungjinwi.c
@@ -6,7 +6,7 @@ int compare(void const *a, void const *b)
     return (*(int *)a - *(int *)b);
 }
 
-bool containsDuplicate(int* nums, int numsSize) {\
+bool containsDuplicate(int* nums, int numsSize) {
 	int	i;
 
 	i = 0;
@@ -19,3 +19,19 @@ bool containsDuplicate(int* nums, int numsSize) {\
 	}
 	return (0);
 }
+
+/*
+	시간 복잡도
+
+	qsort(퀵소트)를 통해 O(n log n)을 한 번 수행 + while문을 한 번 돌아서 O(n)만큼의 복잡도를 가짐
+
+	최종 시간 복잡도 : O(n log n)
+	
+	============================
+
+	공간 복잡도
+
+	추가적으로 할당하는 메모리가 없으므로 O(1)의 복잡도를 가진다
+
+	최종 공간 복잡도 : O(1)
+*/
