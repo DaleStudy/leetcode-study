fbe1ac8ba8534b308beae57106cc8eba540765b8
Changhyun Lee <heypaprika@naver.com>
2024-12-09 11:43:16 +0900

    ADD : #220 #237 #240 #264 by heypaprika

---
+++ longest-consecutive-sequence/heypaprika.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = sorted(list(set(nums)))
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return 1
+        cur_long = 1
+        longest = 1
+        for i, num in enumerate(nums):
+            if i == 0:
+                continue
+            else:
+                if nums[i-1] + 1 == nums[i]:
+                    cur_long += 1
+                    if longest < cur_long:
+                        longest = cur_long
+                else:
+                    cur_long = 1
+        return longest
+
