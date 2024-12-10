912848ae31eda3836ee5325d24aba319b3b28b9f
gmlwls96 <gmlwls961209@naver.com>
2024-12-11 08:35:23 +0900

    줄바꿈 누락 처리 수정

--- top-k-frequent-elements/gmlwls96.kt
+++ top-k-frequent-elements/gmlwls96.kt
@@ -2,12 +2,12 @@ class Solution {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
         val answer = IntArray(k)
         val set = nums.toSet()
-        val mutableList = mutableListOf<Pair<Int,Int>>()
+        val mutableList = mutableListOf<Pair<Int, Int>>()
         set.forEach { num ->
-            mutableList.add(num to nums.count { it == num})
+            mutableList.add(num to nums.count { it == num })
         }
         mutableList.sortByDescending { it.second }
-        for (i in 0 until k){
+        for (i in 0 until k) {
             answer[i] = mutableList[i].first
         }
         return answer
