d1fbcfb2dc0b6651b1f19b6ea173a5a0da456bae
suhacs <parksuha94@gmail.com>
2024-12-11 16:25:33 -0500

    contains duplicate

--- contains-duplicate/suhacs.js
+++ contains-duplicate/suhacs.js
@@ -1,24 +1,7 @@
-/**
- * @description
- * time complexity: O(n)
- * space complexity: O(n)
- * approach/strategy:
- * 1. brute force + hash table
- */
-
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-
 function containsDuplicate(nums) {
   const setLength = [...new Set(nums)].length;
   const numLength = nums.length;
-
-  if (numLength === setLength) return false;
-  else return true;
+  return numLength === setLength ? false : true;
 }
-
 console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
-
 //
