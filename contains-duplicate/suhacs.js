7fe96f43f5b1dff21c6ed46cc32bb7d1ead2fb46
Suha <parksuha94@gmail.com>
2024-12-10 23:28:13 -0400

    contains duplicate solution

---
+++ contains-duplicate/suhacs.js
@@ -0,0 +1,9 @@
+function containsDuplicate(nums) {
+  const setLength = [...new Set(nums)].length;
+  const numLength = nums.length;
+
+  if (numLength === setLength) return false;
+  else return true;
+}
+
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
