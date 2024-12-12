c12452bc1a1ee77253eaccebb1eeabed14bbb02a
unknown <jaym991776@gmail.com>
2024-12-11 22:33:34 -0500

    contains-duplicate

--- contains-duplicate/Jay-Mo-99.py
+++ contains-duplicate/Jay-Mo-99.py
@@ -4,8 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        #??
         #sets don't allow duplicate elements.
         #If the length of the set created from nums is different from the original list(nums), It means there are duplicates. 
+        
+        #Big O
+        #N: ??? ?? nums? ??(Length of the input list nums)
+        #
+        #Time Complexity: O(N)
+        #- set? nums? ?? n? ???? ????(Creating a set from nums): O(N)
+        #- ??? list? ?? nums?? ??? ??(Comparing the lengths between created list and original list) : O(1)
+        #
+        #Space Complexity: O(N)
+        #-set? nums? ??? ?? ????? n? ????(The set requires extra space depends on the size of nums) : O(N) 
         if len(list(set(nums))) == len(nums): 
             return False
         else:
