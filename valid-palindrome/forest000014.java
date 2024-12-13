19d1243d0ac6fc350e7a1c34413b121771fbfba5
forest000014 <forest000014@gmail.com>
2024-12-13 23:18:41 +0900

    Longest Consecutive Sequence - Character 메소드 사용

--- valid-palindrome/forest000014.java
+++ valid-palindrome/forest000014.java
@@ -1,36 +1,34 @@
 /*
-Runtime: 1 ms(Beats: 100.00 %)
+Runtime: 2 ms(Beats: 99.10 %)
 Time Complexity: O(n)
 
-Memory: 43.00 MB(Beats: 64.54 %)
+Memory: 42.75 MB(Beats: 85.31 %)
 Space Complexity: O(1)
 ... 문제에서 주어진 String s는 space complexity 계산에서 제외하고, 제가 추가한 변수에 대해서만 계산하면 될까요?
+
+ps. 처음 풀이에서는 alphanumeric 여부와 대소문자 관련 로직을 일일이 구현했다가, isLetterOrDigit(), toLowerCase()로 변경했습니다.
 */
 
 class Solution {
     public boolean isPalindrome(String s) {
         for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
             char a = s.charAt(i);
-            while (a < '0' || (a > '9' && a < 'A') || (a > 'Z' && a < 'a') || a > 'z') {
+            while (!Character.isLetterOrDigit(a)) {
                 a = s.charAt(++i);
                 if (i >= j) {
                     return true;
                 }
             }
-            if (a <= 'Z') {
-                a += ('a' - 'A');
-            }
+            a = Character.toLowerCase(a);
 
             char b = s.charAt(j);
-            while (b < '0' || (b > '9' && b < 'A') || (b > 'Z' && b < 'a') || b > 'z') {
+            while (!Character.isLetterOrDigit(b)) {
                 b = s.charAt(--j);
                 if (i >= j) {
                     return true;
                 }
             }
-            if (b <= 'Z') {
-                b += ('a' - 'A');
-            }
+            b = Character.toLowerCase(b);
 
             if (a != b) {
                 return false;
