dd6ca48be4da2b1398b0b79b5c9363355acc67eb
Nayeon <nayeonkn0330@gmail.com>
2024-12-13 16:06:21 +0900

    refactor: Improve time complexity using heap for topKFrequent

--- top-k-frequent-elements/KwonNayeon.py
+++ top-k-frequent-elements/KwonNayeon.py
@@ -18,16 +18,43 @@ Space Complexity:
     - O(n)
 """
 
+# Original Solution
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         frequency = {}
         result = []
+        
         for num in nums:
             if num in frequency:
                 frequency[num] += 1
             else:
                 frequency[num] = 1
+                
         sorted_frequency = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+        
         for i in range(k):
             result.append(sorted_frequency[i][0])
+            
+        return result
+
+# Improved Solution using Heap
+# Time Complexity: O(n log k)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequency = {}
+        result = []
+        heap = []
+        
+        for num in nums:
+            if num in frequency:
+                frequency[num] += 1
+            else:
+                frequency[num] = 1
+                
+        for num, freq in frequency.items():
+            heapq.heappush(heap, (-freq, num))
+            
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+            
         return result
