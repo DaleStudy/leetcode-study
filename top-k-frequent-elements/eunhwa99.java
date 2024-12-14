32ed2baa8e51b39625f99ea72a780c6edca6e9ee
eunhwa99 <ekdusp89@gmail.com>
2024-12-14 15:41:53 +0900

    top k frequent

---
+++ top-k-frequent-elements/eunhwa99.java
@@ -0,0 +1,17 @@
+
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> myMap = new HashMap<>();
+        for (int num : nums) {
+            myMap.put(num, myMap.getOrDefault(num, 0) + 1);
+        }
+        return myMap.entrySet()
+                .stream()
+                .sorted((v1, v2) -> Integer.compare(v2.getValue(),v1.getValue()))
+                .map(Map.Entry::getKey)
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
