1bd28b2b384955abf00d609cfbaa81e899346790
o-ddong <odh0112@naver.com>
2024-12-14 16:28:07 +0900

    Feat: contains-duplicate Solution

--- contains-duplicate/oddong.py
+++ contains-duplicate/oddong.py
@@ -1,19 +1,3 @@
-a = [1, 2, 3, 5, 4]
-
-a.sort()
-
-print(a)
-print(set(a))
-print(type(set(a)))
-
-convert_list_to_set = list(set(a))
-
-print()
-print(a)
-print(convert_list_to_set)
-print()
-
-if a == convert_list_to_set:
-    print(True)
-else:
-    print(False)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
