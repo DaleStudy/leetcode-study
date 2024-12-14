389d6b8fb85d66997fc30d95c07eeb38efc0958b
Esther <181283957+EstherKim97@users.noreply.github.com>
2024-12-13 23:17:14 -0800

    top k frequent element

---
+++ top-k-frequent-elements/EstherKim97.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        unique_no = set(nums)
+        data = []
+
+        for i in unique_no:
+            count = 0
+            for j in nums:
+                if i == j:
+                    count += 1
+            data.append((count, i))
+        
+        data = sorted(data, reverse=True, key=lambda x:[1])
+
+        return [x[1] for x in data[:k]]
+        
+        #nums.count(i)
