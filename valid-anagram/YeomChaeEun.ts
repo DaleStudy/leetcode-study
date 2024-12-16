91742b0c3f1b55d21778c01e5a1d898f1307b5f6
chae <yce0222@gmail.com>
2024-12-16 18:01:41 +0900

    feat: valid-anagram solution

---
+++ valid-anagram/YeomChaeEun.ts
@@ -0,0 +1,17 @@
+function isAnagram(s: string, t: string): boolean {
+    if(s.length !== t.length) return false
+
+    let counter = {}
+    for(let sValue of s) {
+        if(!counter[sValue])  { counter[sValue] = 1 }
+        else { counter[sValue]++ }
+
+    }
+
+    for(let tValue of t) {
+        if(!counter[tValue]) return false
+        else counter[tValue]--
+    }
+
+    return Object.values(counter).findIndex(value => value !== 0) < 0;
+};
