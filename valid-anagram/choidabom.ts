0269cec0975abb6d372769a582c7f42ddb9f66d2
choidabom <dabomi0305@naver.com>
2024-12-16 10:07:24 +0900

    fix: runtime

--- valid-anagram/choidabom.ts
+++ valid-anagram/choidabom.ts
@@ -16,7 +16,7 @@ function sortedString(s: string): string {
 }
 
 /**
- * Runtime: 29ms, Memory: 55.02MB
+ * Runtime: 19ms, Memory: 55.02MB
  *
  * 접근: 문자들끼리 anagram 관계인 경우, 각 문자들의 count가 동일할 것이기에 Map 자료구조를 이용해서 합/차 계산
  * Time Complexity: O(N)
