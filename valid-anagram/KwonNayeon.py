a636a6764c6041a13e130a8d2814f8ee0047e9bf
Nayeon <nayeonkn0330@gmail.com>
2024-12-16 14:24:40 +0900

    Solved 218. Valid Anagram using Python code

--- valid-anagram/KwonNayeon.py
+++ valid-anagram/KwonNayeon.py
@@ -1,2 +1,21 @@
+"""
+Constraints:
+    - 1 <= len(s), len(t) <= 50_000
+    - s and t consist of lowercase English letters (a-z) only
 
+Time Complexity:
+    - O(n log n)
+Space Complexity:
+    - O(n)
+"""
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        s = s.replace(' ', '').lower()
+        t = t.replace(' ', '').lower()
+
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
