9871f0b238bb29476ee6f4b4f2fcb3aa575c1b61
oyeong011 <oyeong011@gmail.com>
2024-12-17 15:54:40 +0900

    valid-anagram sol

---
+++ valid-anagram/oyeong011.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> ss, tt;
+        if(s.length() != t.length())return false;
+        for(auto i : s) ss[i]++;
+        for(auto i : t) tt[i]++;
+        return ss == tt;
+    }
+};
