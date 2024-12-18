594b023427ff5987cb6620d9e4910913b43699f1
Esther <181283957+EstherKim97@users.noreply.github.com>
2024-12-18 10:33:33 -0800

    update

--- climbing-stairs/EstherKim97.py
+++ climbing-stairs/EstherKim97.py
@@ -3,6 +3,18 @@ class Solution(object):
         
         import math
 
+        # First attempt to this question. 
+        # Get dividend and remainder of 2 steps & get all combinations & add one additional method for all 1 step combination
+        # But how to get all combinations? => permutation
+        # def climbStairs(self, n):
+        #     # 2 steps
+        #     two_steps = n // 2
+        #     two_steps_remainder = n % 2
+        #     total_steps = two_steps + two_steps_remainder
+        #     total_permu = math.factorial(total_steps) // (math.factorial(two_steps) * math.factorial(two_steps_remainder))
+        #     total_permu += 1
+        #     return total_permu
+
         # Second method - n = 2x+y
         if n % 2 == 0:
             max = n //2
@@ -19,20 +31,3 @@ class Solution(object):
             total_methods += total_permu
 
         return total_methods
-    
-
-    # First attempt to this question. 
-    # Get dividend and remainder of 2 steps & get all combinations & add one additional method for all 1 step combination
-    # But how to get all combinations? => permutation
-    # def climbStairs(self, n):
-    #     # 2 steps
-    #     two_steps = n // 2
-    #     two_steps_remainder = n % 2
-
-    #     total_steps = two_steps + two_steps_remainder
-        
-    #     total_permu = math.factorial(total_steps) // (math.factorial(two_steps) * math.factorial(two_steps_remainder))
-    #     total_permu += 1
-
-    #     return total_permu
-    
