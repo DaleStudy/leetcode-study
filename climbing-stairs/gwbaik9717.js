1eb02cbf21a8b301243e040d9ec5ad0a419bf46c
Paik <gwbaik9717@icloud.com>
2024-12-18 09:20:35 +0900

    refactor: 70. Climbing Stairs to reduce Space complexity to O(1)

--- climbing-stairs/gwbaik9717.js
+++ climbing-stairs/gwbaik9717.js
@@ -1,18 +1,16 @@
 // Time complexity: O(n)
-// Space complexity: O(n)
+// Space complexity: O(1)
 
 /**
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function (n) {
-  const dp = Array.from({ length: n + 1 }, () => 0);
-  dp[0] = 1;
-  dp[1] = 1;
+  const dp = [1, 1];
 
   for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
+    dp[i % 2] = dp[0] + dp[1];
   }
 
-  return dp[n];
+  return dp[n % 2];
 };
