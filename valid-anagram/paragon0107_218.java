c7e4d2d03080900e1a14b38ca0d17e78e741ba33
paragon0107 <smg0725@naver.com>
2024-12-19 17:11:57 +0900

    Paragon0107 218 solution

---
+++ valid-anagram/paragon0107_218.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public static boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        return Arrays.equals(s1, s2);
+    }
+}
