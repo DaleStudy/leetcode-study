a9de6a6a498be57d81467bf132cc07be50e62600
minji-go <kohminji10@gmail.com>
2024-12-20 03:07:14 +0900

    refactor: valid anagram - map.values()

--- valid-anagram/minji-go.java
+++ valid-anagram/minji-go.java
@@ -3,7 +3,7 @@
     Description: return true if one string is an anagram of the other, one formed by rearranging the letters of the other
     Concept:String, Hash Table, Sorting, Array, Counting, String Matching, Ordered Map, Ordered Set, Hash Function ...
     Time Complexity: O(n), Runtime: 27ms
-    Space Complexity: O(n), Memory: 43.11MB
+    Space Complexity: O(n), Memory: 43.05MB
 */
 import java.util.HashMap;
 import java.util.Map;
@@ -12,13 +12,13 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
 
-        Map<Character, Integer> count = new HashMap<>();
+        Map<Character, Integer> charCount = new HashMap<>();
         for(int i=0; i<s.length(); i++){
-            count.put(s.charAt(i), count.getOrDefault(s.charAt(i), 0)+1);
-            count.put(t.charAt(i), count.getOrDefault(t.charAt(i), 0)-1);
+            charCount.put(s.charAt(i), charCount.getOrDefault(s.charAt(i), 0)+1);
+            charCount.put(t.charAt(i), charCount.getOrDefault(t.charAt(i), 0)-1);
         }
-        for(Character key : count.keySet()){
-            if(count.get(key)!=0) return false;
+        for(Integer count : charCount.values()){
+            if(count !=0) return false;
         }
         return true;
     }
