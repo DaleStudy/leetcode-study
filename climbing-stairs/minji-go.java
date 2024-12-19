895e6b2b96864a1af9c8ff828a840a64fecb98e2
minji-go <kohminji10@gmail.com>
2024-12-20 03:02:47 +0900

    refactor: climbing stairs - early return

--- climbing-stairs/minji-go.java
+++ climbing-stairs/minji-go.java
@@ -3,16 +3,19 @@
     Description: how many distinct ways can you climb to the top, if you can either climb 1 or 2 steps
     Concept: Dynamic Programming, Memoization, Recursion, Math, Array, Iterator, Combinatorics ...
     Time Complexity: O(n), Runtime: 0ms
-    Space Complexity: O(1), Memory: 40.51MB
+    Space Complexity: O(1), Memory: 40.63MB
 */
 class Solution {
     public int climbStairs(int n) {
-        int step1=1, step2=2;
-        for(int i=3; i<n; i++){
-            int step3=step1+step2;
-            step1=step2;
-            step2=step3;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        int prev=1, cur=2;
+        for(int i=3; i<=n; i++){
+            int next=prev+cur;
+            prev=cur;
+            cur=next;
         }
-        return n==1?step1:step2;
+        return cur;
     }
 }
