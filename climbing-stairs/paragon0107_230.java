e101f27c31aa2c83e389728f840b76f6c4930115
paragon0107 <smg0725@naver.com>
2024-12-19 18:00:07 +0900

    Paragon0107 230 solution

---
+++ climbing-stairs/paragon0107_230.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}
