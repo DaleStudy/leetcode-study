b366ad758fec5c72b658309692483a864edaf97c
paragon0107 <smg0725@naver.com>
2024-12-19 18:11:41 +0900

    Paragon0107 230 solution

--- climbing-stairs/paragon0107_230.java
+++ climbing-stairs/paragon0107_230.java
@@ -1,3 +1,11 @@
+/*
+*
+* 시간 복잡도:
+*   바텀업 형식으로 배열을 훑으며 올라가기 때문에 O(N)
+* 공간 복잡도:
+*   자연수 마다 해당하는 방법의 갯수를 저장하기 때문에 O(N)
+*
+* */
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
