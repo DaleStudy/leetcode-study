0bc32ec2261e065b1bd7218f65d31a5f471c428b
jinah92 <jinah0818@gmail.com>
2024-12-20 14:07:33 +0900

    feat: climbing-stairs 풀이

---
+++ climbing-stairs/jinah92.py
@@ -0,0 +1,12 @@
+# 공간복잡도: O(1), 시간복잡도: O(n)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        
+        prev, curr  = 1, 2
+        for num in range(3, n+1):
+            prev, curr = curr, prev + curr
+        
+        return curr
+    
