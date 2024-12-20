b72ba0d28be22b988f79e5461f25cd5b750e4e2b
HiGeuni <khyogeun_@naver.com>
2024-12-20 13:44:51 +0900

    Add solutions : Week 2(1,2)

---
+++ climbing-stairs/higeuni.js
@@ -0,0 +1,17 @@
+// complexity
+// time: O(n)
+// space: O(1)
+
+var climbStairs = function(n) {
+  let num1 = 1;
+  let num2 = 1;
+  let temp = 0;
+
+  for(let i = 2; i<=n; ++i ) {
+      temp = num2;
+      num2 = num1 + num2;
+      num1 = temp;
+  }
+  
+  return num2;
+};
---
+++ valid-anagram/higeuni.js
@@ -0,0 +1,7 @@
+// complexity
+// time: O(n log n)
+// space: O(n)
+
+var isAnagram = function(s, t) {
+  return s.split('').sort().join('') === t.split('').sort().join('')
+};
