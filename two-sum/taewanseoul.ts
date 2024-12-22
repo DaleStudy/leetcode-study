5d7ed3f96fc8cbafcae9c1aea5a2ac16536345af
Taewan Lim <taewan.seoul@gmail.com>
2024-12-23 00:12:44 +0900

    two-sum

---
+++ two-sum/taewanseoul.ts
@@ -0,0 +1,21 @@
+/**
+ * 1. Two Sum
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ */
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [i, map.get(target - nums[i])!];
+    }
+    map.set(nums[i], i);
+  }
+}
+
+// O(n) time
+// O(n) space
