e61d2e6518df1e45c6ede7d2d8aa3e6f5eb1731f
MrKeeplearning <jaeseok1115@gmail.com>
2024-12-22 10:29:29 +0900

    lint error 수정

--- contains-duplicate/MrKeeplearning.java
+++ contains-duplicate/MrKeeplearning.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MrKeeplearning {
+class MrKeeplearningContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hashSet = new HashSet<>();
         for (int num : nums) {
