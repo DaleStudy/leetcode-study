549c2d8a23a3402799a67d67c69b5a3d3ffc12c1
sungjinwi <0202wsj@gmail.com>
2024-12-27 20:57:58 +0900

    reverse-bit complexity

--- reverse-bits/sungjinwi.py
+++ reverse-bits/sungjinwi.py
@@ -1,3 +1,10 @@
+"""
+	TC : n의 크기에 상관없이 32번 반복하므로
+		O(1)
+	SC : 추가적인 메모리 쓰지 않으므로
+		O(1)
+"""
+
 class Solution:
     def reverseBits(self, n: int) -> int:
         ret = 0
@@ -5,5 +12,5 @@ class Solution:
             ret |= n & 1
             ret <<= 1
             n >>= 1
-        ret += n & 1
+        ret |= n & 1
         return ret
