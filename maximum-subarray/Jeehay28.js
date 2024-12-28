54d1bcffb4eeb67b0332686e95048fd721ee4762
jeehay <sophiepark528@gmail.com>
2024-12-28 22:06:04 +0900

    Add maximum-subarray solution

---
+++ maximum-subarray/Jeehay28.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Dynamic programming
+
+
+// Optimized Solution:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+var maxSubArray = function (nums) {
+
+    let currentMax = nums[0];
+    let globalMax = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+
+        currentMax = Math.max(currentMax + nums[i], nums[i]);
+        globalMax = Math.max(currentMax, globalMax);
+
+    }
+
+    return globalMax;
+
+};
+
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+// var maxSubArray = function (nums) {
+
+//     let dp = [nums[0]];
+
+//     for (let i = 1; i < nums.length; i++) {
+
+//         dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
+
+//     }
+
+//     return Math.max(...dp)
+
+// };
