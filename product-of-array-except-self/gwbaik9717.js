0cd8146c232f10a757b5da395a7164575c80d308
Paik <gwbaik9717@icloud.com>
2024-12-28 21:32:26 +0900

    refactor: feat: 238. Product of Array Except Self

--- product-of-array-except-self/gwbaik9717.js
+++ product-of-array-except-self/gwbaik9717.js
@@ -6,28 +6,19 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  let count0 = 0;
+  const n = nums.length;
+  const fromLeft = Array.from({ length: n + 1 }, () => 1);
+  const fromRight = Array.from({ length: n + 1 }, () => 1);
 
-  const totalProduct = nums.reduce((acc, num) => {
-    if (num === 0) {
-      count0++;
-    }
+  for (let i = 1; i <= n; i++) {
+    fromLeft[i] = fromLeft[i - 1] * nums[i - 1];
+  }
 
-    return acc * num;
-  }, 1);
+  for (let i = n - 1; i >= 0; i--) {
+    fromRight[i] = fromRight[i + 1] * nums[i];
+  }
 
-  return nums.map((num) => {
-    if (num === 0) {
-      // 0이 2 개 이상 존재할 때
-      if (count0 > 1) {
-        return 0;
-      }
-
-      // 0이 1 개 존재할 때
-      // 0을 제외한 나머지 product 계산 (이 과정은 단 한번만 존재하기 때문에 시간 복잡도는 여전히 O(n))
-      return nums.filter((num) => num !== 0).reduce((a, c) => a * c, 1);
-    }
-
-    return totalProduct / num;
+  return nums.map((num, i) => {
+    return fromLeft[i] * fromRight[i + 1];
   });
 };
