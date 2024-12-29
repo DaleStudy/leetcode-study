4213391fc439ce9f4790e30d096f03e74939b636
Paik <gwbaik9717@icloud.com>
2024-12-29 14:04:02 +0900

    feat: 21. Merge Two Sorted Lists

---
+++ merge-two-sorted-lists/gwbaik9717.js
@@ -0,0 +1,46 @@
+// m: list1, n: list2
+// Time complexity: O(m+n)
+// Space complexity: O(m+n)
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  const answer = new ListNode();
+  let current = answer;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = new ListNode(list1.val);
+      list1 = list1.next;
+    } else {
+      current.next = new ListNode(list2.val);
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  while (list1) {
+    current.next = new ListNode(list1.val);
+    list1 = list1.next;
+    current = current.next;
+  }
+
+  while (list2) {
+    current.next = new ListNode(list2.val);
+    list2 = list2.next;
+    current = current.next;
+  }
+
+  return answer.next;
+};
