9f7dd2f7778850b87d1737f1776d08f3539e0c65
Heejin Ham <gmlwls961209@naver.com>
2024-12-29 11:50:56 +0900

    [Week4](gmlwls96) Merge two sorted lists.

---
+++ merge-two-sorted-lists/gmlwls96.kt
@@ -0,0 +1,14 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+
+    }
+}
