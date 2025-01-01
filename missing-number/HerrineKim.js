3ce15270f09a8dfdb541da7591458d0d60fcecbc
hyerin <herrine.kim@gmail.com>
2025-01-01 20:58:16 +0900

    fix: 시간 복잡도 개선

--- missing-number/HerrineKim.js
+++ missing-number/HerrineKim.js
@@ -1,15 +1,17 @@
-// 시간 복잡도: O(n^2)
-// 공간 복잡도: O(1)
+// 시간 복잡도: O(n)
+// 공간 복잡도: O(n)
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var missingNumber = function(nums) {
-  for(let i = 0; i <= nums.length; i++) {
-      if (!(nums.includes(i))) {
-          return i
-      }
+  const numSet = new Set(nums);
+
+  for (let i = 0; i <= nums.length; i++) {
+    if (!numSet.has(i)) {
+      return i;
+    }
   }
 };
 
