6c8cc958e6ffa4ad5d7addc4d3f79be0fc460965
hyerin <herrine.kim@gmail.com>
2025-01-01 20:40:34 +0900

    add: missing-number

---
+++ missing-number/HerrineKim.js
@@ -0,0 +1,14 @@
+// 시간 복잡도: O(n)
+// 공간 복잡도: O(1)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  for(let i = 0; i <= nums.length; i++) {
+      if (!(nums.includes(i))) {
+          return i
+      }
+  }
+};
