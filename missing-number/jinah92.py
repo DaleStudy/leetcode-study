c981bfbfa14e00cb56447df3ee6380de41e70f33
jinah92 <jinah0818@gmail.com>
2025-01-01 10:29:34 +0900

    feat: missing-number 알고리즘 리팩토링
    - set 자료구조를 활용
    - for문을 모두 순회한경우 리스트의 길이를 리턴

--- missing-number/jinah92.py
+++ missing-number/jinah92.py
@@ -1,12 +1,10 @@
 # O(n) times, O(n) spaces
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums_keys = dict.fromkeys(nums,0)
-        last = 0
+        nums_keys = set(nums)
         
         for i in range(len(nums)):
             if not i in nums_keys:
                 return i
-            last += 1
         
-        return last
+        return len(nums)
