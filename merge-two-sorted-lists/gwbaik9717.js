0cdbba8b7bcaf5c35b57da4d7a8ab2df517c4508
Paik <gwbaik9717@icloud.com>
2025-01-02 07:58:14 +0900

    refactor: feat: 21. Merge Two Sorted Lists

--- merge-two-sorted-lists/gwbaik9717.js
+++ merge-two-sorted-lists/gwbaik9717.js
@@ -30,16 +30,12 @@ var mergeTwoLists = function (list1, list2) {
     current = current.next;
   }
 
-  while (list1) {
-    current.next = new ListNode(list1.val);
-    list1 = list1.next;
-    current = current.next;
+  if (list1) {
+    current.next = list1;
   }
 
-  while (list2) {
-    current.next = new ListNode(list2.val);
-    list2 = list2.next;
-    current = current.next;
+  if (list2) {
+    current.next = list2;
   }
 
   return answer.next;
