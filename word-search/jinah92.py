137592b248de9cf1154a03c4a6b8030d55e3aff8
jinah92 <jinah0818@gmail.com>
2025-01-03 11:08:50 +0900

    feat: word-search 알고리즘 리팩토링 - 공간복잡도 개선

--- word-search/jinah92.py
+++ word-search/jinah92.py
@@ -1,9 +1,8 @@
-# O(M*N*4^W) times, O(M*N+W) spaces
+# O(M*N*4^W) times, O(W) spaces
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
-        visited = set()
-
+    
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
         def dfs(row, col, idx):
@@ -13,13 +12,13 @@ class Solution:
                 return False
             if board[row][col] != word[idx]:
                 return False
-            if (row, col) in visited:
-                return False
             
-            visited.add((row, col))
-            result = any(dfs(row+r, col+c, idx+1) for (r, c) in directions)
-            visited.remove((row, col))
+            temp_val = board[row][col]
+            board[row][col] = ""
 
+            result = any(dfs(row+r, col+c, idx+1) for (r, c) in directions)
+
+            board[row][col] = temp_val
             return result
     
         return any(dfs(r, c, 0) for r in range(rows) for c in range(cols))
