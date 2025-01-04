6ccf94cdb123fc9bb108388e468dc5a973be92d4
hyerin <herrine.kim@gmail.com>
2025-01-04 12:32:13 +0900

    add: 가우스 공식 사용해 공간 복잡도 개선

--- missing-number/HerrineKim.js
+++ missing-number/HerrineKim.js
@@ -1,17 +1,17 @@
 // 시간 복잡도: O(n)
-// 공간 복잡도: O(n)
+// 공간 복잡도: O(1)
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var missingNumber = function(nums) {
-  const numSet = new Set(nums);
-
-  for (let i = 0; i <= nums.length; i++) {
-    if (!numSet.has(i)) {
-      return i;
-    }
-  }
+  const n = nums.length;
+  // 0부터 n까지의 합 공식: n * (n + 1) / 2
+  const expectedSum = (n * (n + 1)) / 2;
+  // 실제 배열의 합
+  const actualSum = nums.reduce((sum, num) => sum + num, 0);
+  
+  return expectedSum - actualSum;
 };
 
