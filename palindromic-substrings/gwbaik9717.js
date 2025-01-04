e7c0e56efc28f14d7271d0488a25bcd05038da43
Paik <gwbaik9717@icloud.com>
2025-01-04 09:28:45 +0900

    refactor: 647. Palindromic Substrings

--- palindromic-substrings/gwbaik9717.js
+++ palindromic-substrings/gwbaik9717.js
@@ -10,34 +10,28 @@ var countSubstrings = function (s) {
   const dp = Array.from({ length: n }, () =>
     Array.from({ length: n }, () => false)
   );
+  let answer = 0;
 
   for (let end = 0; end < n; end++) {
     for (let start = end; start >= 0; start--) {
       if (start === end) {
         dp[start][end] = true;
+        answer++;
         continue;
       }
 
       if (start + 1 === end) {
         if (s[start] === s[end]) {
           dp[start][end] = true;
+          answer++;
         }
         continue;
       }
 
       if (s[start] === s[end] && dp[start + 1][end - 1]) {
         dp[start][end] = true;
-        continue;
-      }
-    }
-  }
-
-  let answer = 0;
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (dp[i][j]) {
         answer++;
+        continue;
       }
     }
   }
