c2689a9643f716b40927d390428eb4f7b779aba4
sungjinwi <0202wsj@gmail.com>
2025-01-04 16:18:16 +0900

    Refactor : use list1&& list2 instead make new instance

--- merge-two-sorted-lists/sungjinwi.py
+++ merge-two-sorted-lists/sungjinwi.py
@@ -23,10 +23,10 @@ class Solution:
         
         while list1 and list2 :
             if list1.val < list2.val :
-                node.next = ListNode(list1.val)
+                node.next = list1
                 list1 = list1.next
             else :
-                node.next = ListNode(list2.val)
+                node.next = list2
                 list2 = list2.next
             node = node.next
         node.next = list1 or list2
