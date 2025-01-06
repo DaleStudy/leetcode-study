98eaeb2ff2012b9f83aa8e102fd3a460d52b5be0
Taewan Lim <taewan.seoul@gmail.com>
2025-01-06 22:32:31 +0900

    best-time-to-buy-and-sell-stock

---
+++ best-time-to-buy-and-sell-stock/taewanseoul.ts
@@ -0,0 +1,29 @@
+/**
+ * 121. Best Time to Buy and Sell Stock
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ */
+
+// O(n) time
+// O(n) space
+function maxProfit(prices: number[]): number {
+  let highestProfit = 0;
+  let left = 0;
+  let right = 1;
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left];
+      highestProfit = Math.max(highestProfit, profit);
+    } else {
+      left = right;
+    }
+
+    right++;
+  }
+
+  return highestProfit;
+}
