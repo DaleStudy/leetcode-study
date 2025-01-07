af7f0942ba907d85f995dbf136363829b6d33e5d
vmop899 <vmop899@gmail.com>
2025-01-07 18:19:22 +0900

    CHORE : added comment

--- best-time-to-buy-and-sell-stock/aa601.py
+++ best-time-to-buy-and-sell-stock/aa601.py
@@ -1,15 +1,16 @@
+# 시간복잡도 O(n)
+# 공간복잡도 O(1)
 class Solution:
 	def maxProfit(self, prices: list[int]) -> int:
-		min_p = prices[0]
+		min_p = prices[0] # 최소 가격 설정  : 배열의 첫 번째 가격
 		cur = 0
 		max_p = 0
 		for n in prices:
-			if n < min_p:
+			if n < min_p: # 현재 가격이 최소 가격보다 작다면 최소가격 갱신
 				min_p = n
-			cur = n - min_p
-			if max_p < cur:
-				max_p = cur
+			cur = n - min_p # 현재 이익 계산
+			if max_p < cur: # 현재 이익과 최대로 얻을 수 있는 이익 비교
+				max_p = cur # 최대 이익 갱신신
 			
 		return max_p
-	
 	
--- group-anagrams/aa601.py
+++ group-anagrams/aa601.py
@@ -1,16 +1,15 @@
+# 시간복잡도 O(n)
+# 공간복잡도 O(1)
 class Solution:
-	def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-		ans = {}
-
-		for word in strs:
-			# 단어의 문자를 정렬 후 키로 사용
-			sortedWord = ''.join(sorted(word))			
-			# 초기 리스트 생성
-			if sortedWord not in ans:
-				ans[sortedWord] = []
-			ans[sortedWord].append(word)
-		
-		# 딕셔너리의 value를 list로 변환
-		ansLst = list(ans.values())
-		return ansLst
-	
+	def maxProfit(self, prices: list[int]) -> int:
+		min_p = prices[0] # 최소 가격 설정  : 배열의 첫 번째 가격
+		cur = 0
+		max_p = 0
+		for n in prices:
+			if n < min_p: # 현재 가격이 최소 가격보다 작다면 최소가격 갱신
+				min_p = n
+			cur = n - min_p # 현재 이익 계산
+			if max_p < cur: # 현재 이익과 최대로 얻을 수 있는 이익 비교
+				max_p = cur # 최대 이익 갱신신
+			
+		return max_p
