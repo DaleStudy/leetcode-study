0a4c99a459d746268d237dea8d74933c754adf35
eunhwa99 <ekdusp89@gmail.com>
2025-01-10 22:51:27 +0900

    container with most water

---
+++ container-with-most-water/eunhwa99.java
@@ -0,0 +1,22 @@
+// two pointer
+
+// 시간 복잡도: O(nlogn) - 투 포인터 
+// 공간 복잡도: O(n) - height 배열 크기
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right = height.length-1;
+
+        int maxContainer = 0;
+        while(left<right){
+            int container = (Math.min(height[left], height[right])) * (right-left);
+    
+            maxContainer = Math.max(maxContainer, container);
+            if(height[left]< height[right]) left++;
+            else right--;
+        }
+
+        return maxContainer;
+    }
+}
