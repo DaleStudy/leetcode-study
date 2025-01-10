02f4602039c2260a415b6a16077585b7b4348ff2
eunhwa99 <ekdusp89@gmail.com>
2025-01-10 22:04:58 +0900

    Add comment

--- valid-parentheses/eunhwa99.java
+++ valid-parentheses/eunhwa99.java
@@ -2,6 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+// 열린 괄호일 경우. 스택에 푸쉬
+// 닫힌 괄호일 경우, 스택의 top 부분이 같은 종류의 열린 괄호이면 pop, 다른 종류 열린 괄호이면 invalid한 문자열
+
+// 시간 복잡도: 스택의 크기 = 문자열의 크기 O(N)
+// 공간 복잡도: 스택의 크기 = O(N)
 class Solution {
     public boolean isValid(String s) {
         char[] array = s.toCharArray();
