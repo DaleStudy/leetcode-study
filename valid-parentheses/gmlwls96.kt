ceda89cbca3e8b03a9c277fea2778fcf39593dcd
Heejin Ham <gmlwls961209@naver.com>
2025-01-13 21:00:53 +0900

    [WEEK6](gmlwls96) valid parentheses

--- valid-parentheses/gmlwls96.kt
+++ valid-parentheses/gmlwls96.kt
@@ -2,6 +2,25 @@ package leetcode_study
 
 class Solution {
     fun isValid(s: String): Boolean {
-
+        val stack = Stack<Char>()
+        val openParentheses = "([{"
+        s.forEach {
+            if (openParentheses.contains(it)) {
+                stack.push(it)
+            } else {
+                if (stack.isEmpty()) {
+                    return false
+                }
+                val top = stack.pop()
+                if (
+                    top == openParentheses[0] && it != ')' ||
+                    top == openParentheses[1] && it != ']' ||
+                    top == openParentheses[2] && it != '}'
+                ) {
+                    return false
+                }
+            }
+        }
+        return stack.isEmpty()
     }
 }
