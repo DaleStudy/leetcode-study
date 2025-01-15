fa625bd132b73b4e2a36ac6071e663547ffb6e0a
eunhwa99 <ekdusp89@gmail.com>
2025-01-15 22:28:57 +0900

    reverse linked list

---
+++ reverse-linked-list/eunhwa99.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ // 시간 복잡도: O(N)
+ // 공간복잡도: O(N)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null) return head;
+
+        ListNode pointer = new ListNode(head.val);
+        
+        ListNode tempPointer;
+        while(head.next!=null){
+            tempPointer = new ListNode(head.next.val, pointer);
+            pointer = tempPointer;
+            head = head.next;
+        }
+    }
+    return pointer;
+}
+
