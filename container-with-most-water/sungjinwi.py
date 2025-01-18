c934be0900bcf76e78427193628cd629eb31b039
sungjinwi <0202wsj@gmail.com>
2025-01-18 15:27:56 +0900

    #242 valid-parentheses calculate complexity

--- container-with-most-water/sungjinwi.py
+++ container-with-most-water/sungjinwi.py
@@ -1,3 +1,16 @@
+"""
+	풀이 :
+		left, right 커서를 양 끝에 놓고 조건에 따라 반대편으로 이동시키며 물 양 비교
+		둘 중 더 높은 높이를 이동시키면 무조건 물의 양이 줄어들기 때문에 더 낮거나 같은 커서를 이동시키며 업데이트
+		둘이 만나면 비교 종료
+
+
+	len(height) : N
+	TC : O(N)
+		l, r의 이동이 전체 height 개수만큼 일어나므로
+	SC : O(1)
+"""
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l = 0
