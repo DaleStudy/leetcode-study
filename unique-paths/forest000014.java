282433f1e56f84c7db381eda8dd49136cc5e223b
forest000014 <forest000014@gmail.com>
2025-01-18 17:48:32 +0900

    Unique Paths

---
+++ unique-paths/forest000014.java
@@ -0,0 +1,21 @@
+/*
+Time Complexity: O(m * n)
+Space Complexity: O(m * n)
+*/
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
