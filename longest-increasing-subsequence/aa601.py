9dac8d1f588431c67a3d23fb5a5d5fc95432bc79
aa601 <152935684+aa601@users.noreply.github.com>
2025-01-19 01:41:14 +0900

    CHORE : edit comment

--- longest-increasing-subsequence/aa601.py
+++ longest-increasing-subsequence/aa601.py
@@ -1,3 +1,11 @@
+"""
+    prv [-1, -1, -1,  2, 2, 3, 5,    5]
+    dp  [ 1,  1,  1,  2, 2, 3, 4,    4]
+    nums[10,  9,  2,  5, 3, 7, 101, 18]
+    nums[2] = 2
+    nums[4] = 3
+    nums[5] = 7
+"""
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1 for _ in range(len(nums))]
@@ -7,16 +15,4 @@ class Solution:
                 if nums[i] < nums[j]:
                     if dp[i] + 1 > dp[j]: 
                         dp[j] = dp[i] + 1
-        """
-        prv [-1, -1, -1,  2, 2, 3, 5,    5]
-        dp  [ 1,  1,  1,  2, 2, 3, 4,    4]
-        nums[10,  9,  2,  5, 3, 7, 101, 18]
-        nums[2] = 2
-        nums[4] = 3
-        nums[5] = 7
-
-                #dp[j] = max(dp[i] + 1, dp[j]) 
-                # dp[i] + 1 <= dp[j]일 때 dp[j] 갱신X
-                # LIS길이가 더 적거나 같은 상황. => LIS배열이 업데이트X
-        """
         return max(dp)
