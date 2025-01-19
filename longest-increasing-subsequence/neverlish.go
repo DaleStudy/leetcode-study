d511a4f66cd998ca9ea22b65daa91a24bbfcba94
Jinho Hyeon <neverlish@gmail.com>
2025-01-19 09:55:22 +0900

    week 06

---
+++ longest-increasing-subsequence/neverlish.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+
+func TestLengthOfLIS(t *testing.T) {
+	result1 := lengthOfLIS([]int{10, 9, 2, 5, 3, 7, 101, 18})
+
+	if result1 != 4 {
+		t.Errorf("Expected 4, but got %v", result1)
+	}
+
+	result2 := lengthOfLIS([]int{0, 1, 0, 3, 2, 3})
+
+	if result2 != 4 {
+		t.Errorf("Expected 4, but got %v", result2)
+	}
+
+	result3 := lengthOfLIS([]int{7, 7, 7, 7, 7, 7, 7})
+
+	if result3 != 1 {
+		t.Errorf("Expected 1, but got %v", result3)
+	}
+}
+
+func lengthOfLIS(nums []int) int {
+	result := 0
+
+	dp := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+
+		result = max(result, dp[i])
+	}
+
+	return result
+
+}
---
+++ valid-parentheses/neverlish.go
@@ -0,0 +1,48 @@
+package main
+
+import "testing"
+
+func TestIsValid(t *testing.T) {
+	result1 := isValid("()")
+
+	if result1 != true {
+		t.Errorf("Expected true, but got %v", result1)
+	}
+
+	result2 := isValid("()[]{}")
+	if result2 != true {
+		t.Errorf("Expected true, but got %v", result2)
+	}
+
+	result3 := isValid("(]")
+	if result3 != false {
+		t.Errorf("Expected false, but got %v", result3)
+	}
+
+	result4 := isValid("([)]")
+	if result4 != false {
+		t.Errorf("Expected false, but got %v", result4)
+	}
+}
+
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+
+	for _, c := range s {
+		switch c {
+		case '(':
+			stack = append(stack, ')')
+		case '{':
+			stack = append(stack, '}')
+		case '[':
+			stack = append(stack, ']')
+		default:
+			if len(stack) == 0 || stack[len(stack)-1] != c {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
