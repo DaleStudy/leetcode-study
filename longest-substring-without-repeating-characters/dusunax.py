e2941b125d7b55499413bef6c3e24f43027c2d9f
Dusuna <94776135+dusunax@users.noreply.github.com>
2025-01-24 01:41:09 +0900

    update solution: longest-substring-without-repeating-characters

--- longest-substring-without-repeating-characters/dusunax.py
+++ longest-substring-without-repeating-characters/dusunax.py
@@ -6,21 +6,36 @@ use a set to store the characters in the current substring.
 
 ## Time and Space Complexity
 
+### Solution 1: using set
+
 ```
 TC: O(n)
 SC: O(n)
 ```
 
-#### TC is O(n):
+####  TC is O(n):
 - iterating with end pointer through the string once. = O(n)
 - inner while loop runs at most once for each character in the string. = Amortized O(1)
 
 #### SC is O(n):
 - using a set to store the characters in the current substring. = O(n)
 
+### Solution 2: using ASCII array
+
+```
+TC: O(n)
+SC: O(128)
+```
+
+#### TC is O(n):
+- iterating with end pointer through the string once. = O(n)
+- checking if the character is in the current substring.
+
+#### SC is O(1):
+- using an array to store the characters in the current substring. = constant space O(128)
 '''
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def lengthOfLongestSubstringWithSet(self, s: str) -> int:
         max_count = 0
         start = 0
         substrings = set() # SC: O(n)
@@ -32,4 +47,17 @@ class Solution:
             substrings.add(s[end])
             max_count = max(max_count, end - start + 1)
         
+        return max_count
+    
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_count = 0
+        start = 0
+        char_index = [-1] * 128 # SC: O(128)
+
+        for end in range(len(s)): # TC: O(n)
+            if char_index[ord(s[end])] >= start:
+                start = char_index[ord(s[end])] + 1
+            char_index[ord(s[end])] = end
+            max_count = max(max_count, end - start + 1)
+            
         return max_count
