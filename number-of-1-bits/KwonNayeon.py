04b2d9d92a75d90fefe282dc23295915089338a4
Nayeon <nayeonkn0330@gmail.com>
2025-01-27 14:44:31 +0900

    solve: Number of 1 Bits

--- number-of-1-bits/KwonNayeon.py
+++ number-of-1-bits/KwonNayeon.py
@@ -1,17 +1,20 @@
 """
 Constraints:
-- 
+- 1 <= n <= 2^31 - 1
 
-Time Complexity: 
-- 
+Time Complexity: O(k)
+- 여기서 k는 입력의 비트 수
+- 이 경우 32비트 정수이므로 실질적으로 O(1) (항상 최대 32번 반복하기 때문)
 
-Space Complexity: 
-- 
-
-풀이 방법:
-- 
-
-Further Consideration:
-- 
+Space Complexity: O(1)
+- count 변수만 사용하므로 상수 공간 복잡도
 """
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
+
 
