4bdaa7da4c8b86237cd665ae308c06f3f816428d
dongha kim <kimdongha15@naver.com>
2025-01-30 22:21:53 +0900

    feat : week 8

---
+++ longest-common-subsequence/imsosleepy.java
@@ -0,0 +1,20 @@
+// 공통 부분 수열 문제는 되게 유명한 문제임
+// 일반적인 DP는 O(N)인데, 이건 O(N^2)이라 어려움
+// GPT의 도움을 받았음. 따로 정리가 필요할 듯
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
