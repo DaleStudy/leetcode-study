97f8fdcf674e00a4f80e3eef8f170696d0876fb2
limlim <skyblue.777@daum.net>
2025-02-08 14:02:07 +0900

    linked list cycle solution

---
+++ linked-list-cycle/limlimjo.js
@@ -0,0 +1,29 @@
+// 시간 복잡도: O(n)
+// 공간 복잡도: O(1)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
