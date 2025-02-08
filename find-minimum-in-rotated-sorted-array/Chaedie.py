a33f733f4c519ad181ddb58aafb4ed2ae5b790a6
ChaedongIm <chaedong.im.dev@gmail.com>
2025-02-08 11:06:10 +0900

    fix: find-minimum-in-rotated-sorted-array 시간 복잡도 개선

--- find-minimum-in-rotated-sorted-array/Chaedie.py
+++ find-minimum-in-rotated-sorted-array/Chaedie.py
@@ -14,3 +14,27 @@ class Solution:
                 return nums[i]
 
         return nums[0]
+
+    """
+    Solution: 
+        시간 복잡도 O(log n)으로 풀기 위해 binary search 사용
+    Time: O(log n)
+    Space: O(1)
+    """
+
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 1, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            # prev 값보다 mid 가 작으면 찾던 값
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            # mid 까지 정상 순서면 우측 탐색
+            if nums[0] < nums[mid]:
+                l = mid + 1
+            # mid 까지 비 정상 순서면 좌측 탐색
+            else:
+                r = mid - 1
+        # 못찾을 경우 전체 정상 순서 케이스
+        return nums[0]
