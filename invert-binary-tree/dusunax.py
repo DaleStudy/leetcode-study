2ccbfe3ade2b36bfec32611c970290c3829a88ba
Dusuna <94776135+dusunax@users.noreply.github.com>
2025-02-10 16:50:54 +0900

    update solution: invert-binary-tree

--- invert-binary-tree/dusunax.py
+++ invert-binary-tree/dusunax.py
@@ -15,7 +15,10 @@ h is height of tree
 - worst case: O(N), skewed tree
 '''
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    '''
+    DFS
+    '''
+    def invertTreeRecursive(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
 
@@ -23,5 +26,40 @@ class Solution:
 
         self.invertTree(root.left)
         self.invertTree(root.right)
+
+        return root
+
+    '''
+    BFS
+    - 직관적인 stack 풀이
+    '''
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            
+            node.left, node.right = node.right, node.left
+            stack.append(node.left)
+            stack.append(node.right)
+
+        return root
+
+    '''
+    - 참고용 deque 풀이
+    '''
+    def invertTreeDeque(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        dq = deque([root])
+
+        while dq:
+            node = dq.popleft()
+            if not node:
+                continue
+            
+            node.left, node.right = node.right, node.left
+            dq.append(node.left)
+            dq.append(node.right)
 
         return root
