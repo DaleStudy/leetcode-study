bf498a944b1d0d92838e7ed91b5759e8d6c8ca23
Dusuna <94776135+dusunax@users.noreply.github.com>
2025-02-17 16:15:38 +0900

    update solution: maximum-depth-of-binary-tree

--- maximum-depth-of-binary-tree/dusunax.py
+++ maximum-depth-of-binary-tree/dusunax.py
@@ -3,6 +3,7 @@
 
 use DFS to find the maximum depth of the binary tree.
 
+## A. if we use a helper function (not a good idea)
 ``` 
 helper function explanation:
 - store the max_depth in the nonlocal variable.(outside of the helper function)
@@ -11,6 +12,11 @@ helper function explanation:
   - update the count for the depth of the tree.
 - update the max_depth when the node is a leaf node's children.
 ```
+
+## B. return the max_depth directly
+👉 why helper function is not necessary?
+recursion function can return the max_depth directly.  
+remove side effect & non-local variable.
 
 ## TC is O(n)
 
@@ -21,7 +27,10 @@ visit each node once for checking if it is a leaf node's children.
 h for height of the tree
 '''
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int: 
+    '''
+    A. first approach with side effect
+    '''
+    def maxDepthWithHelper(self, root: Optional[TreeNode]) -> int: 
         max_depth = 0
 
         def helper(node, count):
@@ -36,3 +45,17 @@ class Solution:
         helper(root, max_depth)
 
         return max_depth
+    
+    '''
+    B. return the max_depth directly.
+    - more concise & readable
+    - no side effect & non-local variable
+    '''
+    def maxDepth(self, root: Optional[TreeNode]) -> int: 
+        if root is None:
+            return 0
+        
+        left_count = self.maxDepth(root.left)
+        right_count = self.maxDepth(root.right)
+
+        return max(left_count, right_count) + 1
