d57d3349dbbc0846202c00ca6fd0fd04b71d167a
forest000014 <forest000014@gmail.com>
2025-03-06 00:14:34 +0900

    DaleStudy#231 Meeting Rooms

---
+++ meeting-rooms/forest000014.java
@@ -0,0 +1,15 @@
+/*
+# Time Complexity: O(nlogn)
+# Space Complexity: O(1)
+*/
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) return false;
+        }
+
+        return true;
+    }
+}
