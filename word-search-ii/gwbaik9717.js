100154c7ae4cb1cc9ae6d261548882a25619a04f
ganu <gwbaik9717@icloud.com>
2025-03-14 09:37:45 +0900

    refactor: word-search-ii

--- word-search-ii/gwbaik9717.js
+++ word-search-ii/gwbaik9717.js
@@ -1,6 +1,33 @@
-// n: length of words, k: length of word, h: height of board, w: width of board
-// Time complexity: O(4^k * h * w * n)
+// k: length of word, h: height of board, w: width of board
+// Time complexity: O(4^k * h * w)
 // Space complexity: O(4^k)
+
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+    this.isEnd = false;
+  }
+}
+class Trie {
+  constructor() {
+    this.head = new Node();
+  }
+
+  add(str) {
+    let current = this.head;
+
+    for (const chr of str) {
+      if (!current.children.has(chr)) {
+        current.children.set(chr, new Node(current.value + chr));
+      }
+
+      current = current.children.get(chr);
+    }
+
+    current.isEnd = true;
+  }
+}
 
 /**
  * @param {character[][]} board
@@ -8,6 +35,8 @@
  * @return {string[]}
  */
 var findWords = function (board, words) {
+  const answer = new Set();
+
   const h = board.length;
   const w = board[0].length;
 
@@ -17,63 +46,42 @@ var findWords = function (board, words) {
     Array.from({ length: w }, () => false)
   );
 
-  const findWord = (current, i, word) => {
+  const dfs = (current, children) => {
     const [cy, cx] = current;
 
-    if (i === word.length - 1) {
-      return true;
+    if (!children.has(board[cy][cx])) {
+      return;
     }
 
-    let found = false;
+    if (children.get(board[cy][cx]).isEnd) {
+      answer.add(children.get(board[cy][cx]).value);
+    }
 
     for (let j = 0; j < dx.length; j++) {
       const nx = cx + dx[j];
       const ny = cy + dy[j];
 
       if (nx >= 0 && nx < w && ny >= 0 && ny < h && !checked[ny][nx]) {
-        if (board[ny][nx] === word[i + 1]) {
-          checked[ny][nx] = true;
-
-          if (findWord([ny, nx], i + 1, word)) {
-            found = true;
-          }
-
-          checked[ny][nx] = false;
-        }
+        checked[ny][nx] = true;
+        dfs([ny, nx], children.get(board[cy][cx]).children);
+        checked[ny][nx] = false;
       }
     }
-
-    return found;
   };
 
-  const answer = [];
+  const trie = new Trie();
 
   for (const word of words) {
-    let found = false;
+    trie.add(word);
+  }
 
-    for (let i = 0; i < h; i++) {
-      if (found) {
-        break;
-      }
-
-      for (let j = 0; j < w; j++) {
-        if (found) {
-          break;
-        }
-
-        if (board[i][j] === word[0]) {
-          checked[i][j] = true;
-
-          if (findWord([i, j], 0, word)) {
-            answer.push(word);
-            found = true;
-          }
-
-          checked[i][j] = false;
-        }
-      }
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      checked[i][j] = true;
+      dfs([i, j], trie.head.children);
+      checked[i][j] = false;
     }
   }
 
-  return answer;
+  return [...answer];
 };
