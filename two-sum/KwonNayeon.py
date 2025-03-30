303dddb543b4474ed47dc3b0d71aeb54aca8bcc6
Nayeon <nayeonkn0330@gmail.com>
2025-03-30 11:35:13 +0900

    Plan to add another solution for the 'Two Sum' problem

--- two-sum/KwonNayeon.py
+++ two-sum/KwonNayeon.py
@@ -15,6 +15,7 @@ Space Complexity: O(1)
     - 추가 공간을 사용하지 않음
     - result 리스트는 항상 크기가 2로 고정
 """
+# Solution 1
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = []
@@ -23,3 +24,6 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[j] == target - nums[i]:
                     return [i, j]
+
+# Solution 2
+
