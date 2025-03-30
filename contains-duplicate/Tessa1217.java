8377a45d5d616fa4c55441caffd452c8d52b2c5c
Tessa1217 <kwonyuj1996@gmail.com>
2025-03-30 15:56:33 +0900

    update: add comments

--- contains-duplicate/Tessa1217.java
+++ contains-duplicate/Tessa1217.java
@@ -3,6 +3,10 @@ import java.util.Set;
 
 class Solution {
 
+    /** 217. 중복된 수
+      * 정수 배열 nums가 주어졌을 때 배열 요소 중 한 개 이상이 두 번 이상 중복되어
+      * 나타는 경우 true를, 모든 배열의 요소가 고유한 경우 false를 반환
+      */
     public boolean containsDuplicate(int[] nums) {
 
         Set<Integer> distincts = new HashSet<>();
@@ -14,3 +18,4 @@ class Solution {
         return distincts.size() != nums.length;
     }
 }
+
