25ad7dfa0f89f4bbbbc41ca039450e3801068ef0
kut7728 <kut7728@hanyang.ac.kr>
2025-03-31 14:33:52 +0900

    contains-duplicate solved

---
+++ contains-duplicate/kut7728.swift
@@ -0,0 +1,14 @@
+///  Time, Space Complexity O(N)
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var seen = Set<Int>()
+        for num in nums {
+            if seen.contains(num) {
+                return true
+            }
+            seen.insert(num)
+        }
+        return false
+    }
+}
