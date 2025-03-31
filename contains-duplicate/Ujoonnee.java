4e942db3148e8a5168d584451f95ec9ee15a7fd3
Ujoonnee <dnwns0933@naver.com>
2025-03-31 18:15:41 +0900

    add shortened solution of contains-duplicate

--- contains-duplicate/Ujoonnee.java
+++ contains-duplicate/Ujoonnee.java
@@ -14,3 +14,11 @@ class Solution {
         return false;
     }
 }
+
+/*
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count() != nums.length;
+    }
+}
+*/
