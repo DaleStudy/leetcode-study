cbd7d6ec5866b7277e7b91215f2392c5623d4321
jangseyeong <jang.se.yeong000@gmail.com>
2025-04-02 00:30:42 +0900

    contains-duplicate, two-sum 설명 추가

--- contains-duplicate/JANGSEYEONG.js
+++ contains-duplicate/JANGSEYEONG.js
@@ -1,6 +1,12 @@
-// 시간복잡도: O(n) - Set 생성 시 모든 요소를 한 번씩 처리하기 때문
-// - new Set(nums)는 배열의 모든 요소를 순회하며 Set에 추가: O(n)
-// - 길이 비교 자체는 O(1)이므로 전체 시간복잡도는 O(n)
+/*
+  시간복잡도: O(n) - new Set(nums)에서 배열 요소 순회하며 Set 생성 O(n) + 길이 비교 O(1)
+  - Set 자료구조는 중복된 값을 자동으로 제거
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var containsDuplicate = function (nums) {
   // Set으로 만들었을 때, 기존 배열과 사이즈가 다르면 중복이 제거된거임
   const numsSet = new Set(nums);
--- two-sum/JANGSEYEONG.js
+++ two-sum/JANGSEYEONG.js
@@ -1,20 +1,29 @@
-// 시간복잡도 O(n²)
-// - nums.indexOf()는 배열 전체를 순회하는 O(n) 작업
-// - 이 작업이 for 루프(O(n)) 내부에서 실행되므로 전체 시간복잡도는 O(n) * O(n) = O(n²)
-/*var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    let x = nums.indexOf(target - nums[i]);
-    if (x > -1 && x !== i) {
-      return [i, x];
-    }
-  }
-  return [];
-};*/
+/*
+  시간복잡도:  O(n²)
+  - nums.indexOf()는 배열 전체를 순회하는 O(n) 작업
+  - 이 작업이 for 루프(O(n)) 내부에서 실행되므로 전체 시간복잡도는 O(n) * O(n) = O(n²)
 
-// 시간복잡도 O(n)
-// 전체 배열을 한 번만 순회 O(n) + 키-값 쌍 저장에 O(1)
-// Map 대신 객체(Object)를 사용해도 될듯 함
-// => 일반 객체는 {}, 접근은 obj[key]로 가능, has() 대신 (key in obj) 또는 obj[key] !== undefined 사용 가능
+  var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+      let x = nums.indexOf(target - nums[i]);
+      if (x > -1 && x !== i) {
+        return [i, x];
+      }
+    }
+    return [];
+  };
+
+*/
+
+/* 
+  시간복잡도: O(n) - 전체 배열을 한 번만 순회 O(n) + 키-값 쌍 저장에 O(1)
+  - Map 대신 객체(Object)를 사용해도 될듯 함
+  - 일반 객체는 {}, 접근은 obj[key]로 가능, has() 대신 (key in obj) 또는 obj[key] !== undefined 또는 key in obj 사용 가능 */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function (nums, target) {
   let minus = new Map();
   for (let i = 0; i < nums.length; i++) {
