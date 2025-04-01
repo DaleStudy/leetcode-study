3b2f75ad192bb3bfba4eb4993851d1abad22d3d4
Ujoonnee <dnwns0933@naver.com>
2025-04-01 10:35:16 +0900

    add solution of longest-consecutive-sequence (reduced runtime)

--- longest-consecutive-sequence/Ujoonnee.java
+++ longest-consecutive-sequence/Ujoonnee.java
@@ -1,33 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
-        // 중복 제거 후 오름차순 정렬
-        Set<Integer> distinct = new HashSet<>();
-        for (int num : nums) {
-            distinct.add(num);
+        if (nums.length == 0) {
+            return 0;
         }
-        List<Integer> list = new ArrayList<>(distinct);
-        Collections.sort(list);
 
-        // 연속된 숫자라면 스택에 저장
-        Stack<Integer> stack = new Stack<>();
-        int answer = 0;
-        for (int num : list) {
-            if (stack.isEmpty()) {
-                stack.add(num);
+        Arrays.sort(nums);
+
+        int answer = 1;
+        int current = 1;
+        for (int i=1; i<nums.length; i++) {
+            // 이전 숫자와 같다면 스킵
+            if (nums[i-1] == nums[i]) {
                 continue;
             }
 
-            // 연속된 숫자가 아니라면 현재까지 연속된 수를 저장 후 스택 초기화
-            if (stack.peek() + 1 != num) {
-                answer = Math.max(answer, stack.size());
-                stack.clear();
-            }
+            // 연속된 숫자라면 증가, 아니라면 초기화
+            if (nums[i-1] + 1 == nums[i]) {
+                current++;
+                answer = Math.max(answer, current);
 
-            stack.add(num);
+            } else {
+                current = 1;
+            }
         }
 
-        return Math.max(answer, stack.size());
+        return answer;
     }
 }
