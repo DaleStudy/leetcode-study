bd8eb041c2a2f5c28f6da6025819448728eb910a
choidabom <dabomi0305@naver.com>
2025-04-02 22:14:23 +0900

    fix: lint

--- top-k-frequent-elements/choidabom.ts
+++ top-k-frequent-elements/choidabom.ts
@@ -4,16 +4,16 @@
 // SC: O(n)
 
 function topKFrequent(nums: number[], k: number): number[] {
-    const map = new Map()
+  const map = new Map();
 
-    for (const num of nums) {
-      if (map.has(num)){
-        map.set(num, map.get(num) + 1)
-      } else {
-        map.set(num, 1)
-      }
+  for (const num of nums) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
     }
+  }
 
-    const sortedMap = [...map].sort((a, b)=> b[1]- a[1]) 
-    return sortedMap.splice(0, k).map((item)=> item[0])
-};
+  const sortedMap = [...map].sort((a, b) => b[1] - a[1]);
+  return sortedMap.splice(0, k).map((item) => item[0]);
+}
--- two-sum/choidabom.ts
+++ two-sum/choidabom.ts
@@ -4,33 +4,33 @@
 // SC: O(1)
 
 function twoSum(nums: number[], target: number): number[] {
-    for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++){
-        if (nums[i] + nums[j] === target){
-          return [i, j]
-        }
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
       }
     }
-    return []
-};
+  }
+  return [];
+}
 
 // TC: O(n)
 // SC: O(n)
 
 function twoSum(nums: number[], target: number): number[] {
-    const map = new Map()
+  const map = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i]
-        const diff = target - num
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const diff = target - num;
 
-        if (map.has(diff)) {
-            return [i, map.get(diff)]
-        } else {
-            map.set(num, i)
-        }
+    if (map.has(diff)) {
+      return [i, map.get(diff)];
+    } else {
+      map.set(num, i);
     }
-    return []
-};
+  }
+  return [];
+}
 
-console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([2, 7, 11, 15], 9));
