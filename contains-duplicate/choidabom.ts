4df0570373e97d596fd5a9eec6c433a4e6005bda
choidabom <dabomi0305@naver.com>
2025-04-02 15:24:46 +0900

    feat: contains-duplicate

---
+++ contains-duplicate/choidabom.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+// TC: O(n)
+// SC: O(n) 
+
+function containsDuplicate(nums: number[]): boolean {
+    const set = new Set(nums)
+    return set.size !== nums.length
+};
+
+function containsDuplicate(nums: number[]): boolean {
+    const set = new Set()
+
+    for (const num of nums){
+        if (set.has(num)){
+            return true
+        }
+        set.add(num)
+    }
+
+    return false
+}
+
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
