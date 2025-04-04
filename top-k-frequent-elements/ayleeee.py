ddbe97737043acbc985b1d763c5d60d371f0e2c1
ayleeee <86951396+ayleeee@users.noreply.github.com>
2025-04-04 21:39:45 +0900

    top k frequent elements edited

--- top-k-frequent-elements/ayleeee.py
+++ top-k-frequent-elements/ayleeee.py
@@ -8,3 +8,4 @@ class Solution:
                 dict_map[a] = 1
         lst = sorted(dict_map.items(), key=lambda x: x[1], reverse=True)  # 공백 수정
         return [item[0] for item in lst[0:k]]
+        
