a6a966b412bb14f5d54f8b92210175b5cfc4eaae
이호찬 <hochan.lee@brobotics.kr>
2025-04-04 23:57:19 +0900

    refactor longest-consecutive-sequence

--- longest-consecutive-sequence/lhc0506.js
+++ longest-consecutive-sequence/lhc0506.js
@@ -2,26 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var longestConsecutive = function(nums) {
     const numSet = new Set(nums);
 
     let maxLength = 0;
     let currentLength = 0;
 
-    nums.forEach(num => {
+    const countConsecutive = (num, step) => {
         let currentNum = num;
         while(numSet.has(currentNum)) {
-            numSet.delete(currentNum)
-            currentNum += 1;
+            numSet.delete(currentNum);
+            currentNum += step;
             currentLength += 1;
         }
+    }
 
-        currentNum = num - 1;
-        while(numSet.has(currentNum)) {
-            numSet.delete(currentNum)
-            currentNum -= 1;
-            currentLength += 1;
-        }
+    nums.forEach(num => {
+        countConsecutive(num, 1);
+        countConsecutive(num - 1, -1);
 
         maxLength = Math.max(maxLength, currentLength);
         currentLength = 0;
