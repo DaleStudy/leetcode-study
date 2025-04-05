6df57597b0197a0fec9f9c42b00cda32a03e0b37
Sangmin <sm9171@nate.com>
2025-04-05 18:09:45 +0900

    refactor: top-k-frequent-elements solution

--- top-k-frequent-elements/sm9171.java
+++ top-k-frequent-elements/sm9171.java
@@ -1,26 +1,20 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            if (hashMap.containsKey(num)) {
-                hashMap.put(num, hashMap.get(num) + 1);
-                continue;
-            }
-            hashMap.put(num, 1);
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer> list = hashMap.entrySet()
-                .stream()
+        List<Integer> list = hashMap.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
                 .limit(k)
                 .map(Map.Entry::getKey)
                 .toList();
 
-        int [] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
+        int[] result = list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
 
         return result;
     }
