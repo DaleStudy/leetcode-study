23984e63a56f4fd92d3b59c06eaa1d995320aa32
Sangmin <sm9171@nate.com>
2025-04-05 17:54:32 +0900

    refactor: contains-duplicate solution

--- contains-duplicate/sm9171.java
+++ contains-duplicate/sm9171.java
@@ -3,12 +3,11 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
             set.add(nums[i]);
         }
-
-        if (nums.length == set.size()) {
-            return false;
-        }
-        return true;
+        return false;
     }
 }
