821aa4a002e0f11052775567656c49b0b8548c3c
hi-rachel <rachel.uiux@gmail.com>
2025-04-05 19:54:45 +0900

    house robber solution 설명 추가

--- house-robber/hi-rachel.py
+++ house-robber/hi-rachel.py
@@ -1,4 +1,14 @@
 # O(n) time, O(n) space
+# dp[i]는 i번째 집까지 봤을 때의 최대 누적 금액
+
+# 두 가지 선택지를 고려:
+# 1. 이 집을 턴다:
+#    이전 집은 털 수 없으니 dp[i-2] + nums[i]
+# 2. 이 집을 안 턴다:
+#    그냥 전 집까지의 최대 금액 유지: dp[i-1]
+# 두 가지 선택지 중 큰 걸 선택
+# **dp[i] = max(dp[i-1], dp[i-2] + nums[i])**
+# nums 길이가 2인 경우 range(2, 2)는 for문 안 돈다.
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
@@ -13,7 +23,7 @@ class Solution:
             dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
         
         return dp[-1]
-    
+
 
 # TS 코드
 # function rob(nums: number[]): number {
