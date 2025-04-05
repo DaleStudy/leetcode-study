4212d9be6d791672132634977e04840fb13befdc
JEONGBEOM KO <beom9807@gmail.com>
2025-04-05 18:09:26 +0900

    add solution of longest-consecutive-sequence

---
+++ longest-consecutive-sequence/JEONGBEOMKO.java
@@ -0,0 +1,24 @@
+package week01.longest_consecutive_sequence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            countMap.put(nums[i], countMap.getOrDefault(nums[i] , 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> map : countMap.entrySet()) {
+            if (map.getValue() >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
