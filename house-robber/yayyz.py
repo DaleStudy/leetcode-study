b74f6ce2d3735ea3b4a23302e3a9383644ab5158
yayyz <yejiim.dev@gmail.com>
2025-04-05 16:52:10 +0900

    house robber solution & added newline

---
+++ house-robber/yayyz.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1]) 
+
+        # i 번째를 털지 않은 조건과 i번째를 털은 조건 중 max를 비교
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[-1]
+    
--- longest-consecutive-sequence/yayyz.py
+++ longest-consecutive-sequence/yayyz.py
@@ -20,3 +20,5 @@ class Solution:
         
         return result 
     
+    
+    
