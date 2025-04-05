6c958b8bbd9133645ee210ef728eaaa1e8acb609
hi-rachel <rachel.uiux@gmail.com>
2025-04-05 15:32:27 +0900

    improve top k frequent elements solution
    - 올바른 default 사용
    - 사용하지 않는 배열 삭제

--- top-k-frequent-elements/hi-rachel.py
+++ top-k-frequent-elements/hi-rachel.py
@@ -6,13 +6,9 @@ from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numdict = defaultdict(int);
-        result = []
 
         for num in nums:
-            if num in numdict:
-                numdict[num] += 1
-            else:
-                numdict[num] = 1
+            numdict[num] += 1
 
         sort_dict = dict(sorted(numdict.items(), key=lambda item: item[1], reverse=True))
 
