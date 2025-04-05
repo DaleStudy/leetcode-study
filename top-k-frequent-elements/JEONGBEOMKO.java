8031e406bdc474553aba00bc63b6699393846002
JEONGBEOM KO <beom9807@gmail.com>
2025-04-05 17:41:01 +0900

    add solution of top-k-frequent-elements

---
+++ top-k-frequent-elements/JEONGBEOMKO.java
@@ -0,0 +1,31 @@
+package week01.top_k_frequent_elements;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        Map.Entry<Integer, Integer>[] arr = new Map.Entry[frequencyMap.size()];
+        Iterator<Map.Entry<Integer, Integer>> iterator = frequencyMap.entrySet().iterator();
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = iterator.next();
+        }
+
+        Arrays.sort(arr, (e1, e2) -> e2.getValue() - e1.getValue());
+
+        int[] answer = new int[k];
+        for (int i=0; i<k; i++) {
+            answer[i] = arr[i].getKey();
+        }
+
+        return answer;
+    }
+}
