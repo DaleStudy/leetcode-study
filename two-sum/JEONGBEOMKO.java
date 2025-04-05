405949d5b220044574bfdd91ef1073a18f2daf31
JEONGBEOM KO <beom9807@gmail.com>
2025-04-05 16:58:41 +0900

    two-sum solution

---
+++ two-sum/JEONGBEOMKO.java
@@ -0,0 +1,22 @@
+package week01.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numberMap = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            numberMap.put(nums[i], i);
+        }
+
+        for(int i=0; i<nums.length; i++) {
+            int operand = target - nums[i];
+            if (numberMap.containsKey(operand) && numberMap.get(operand) != i) { // 자기 자신은 제외
+                return new int[] { numberMap.get(target - nums[i]), i };
+            }
+        }
+
+        return new int[] {};
+    }
+}
