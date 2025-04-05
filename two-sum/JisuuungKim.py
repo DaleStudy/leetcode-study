ea3cb48acd1e4b146b2050bd66a7482596368a04
jisung <cham0810@kookmin.ac.kr>
2025-04-06 00:22:18 +0900

    two sum solution

---
+++ two-sum/JisuuungKim.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            cur = nums[i]
+            x = target - cur
+            if x in d:
+                return [i, d[x]]
+            else:
+                d[cur] = i
