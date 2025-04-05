1de52da7a5cec4fc3fc8672a9613be9e4df57cfe
yayyz <yejiim.dev@gmail.com>
2025-04-05 15:41:45 +0900

    longest consecutive sequence solution & added newline

---
+++ longest-consecutive-sequence/yayyz.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0: 
+            return 0
+        
+        nums = sorted(set(nums))
+        
+        currLength = 1
+        lastNum = nums[0]
+        result = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] == lastNum + 1:
+                currLength += 1
+            else:
+                currLength = 1
+            
+            result = max(result, currLength)
+            lastNum = nums[i]
+        
+        return result 
+    
