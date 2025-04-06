c2aa005b333247cfc5b20787821b81fe81c80ce8
PDKhan <kkzoro1@gmail.com>
2025-04-05 23:42:59 -0700

    climbing stairs solution

---
+++ climbing-stairs/PDKhan.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        int climbStairs(int n) {
+            if(n < 2)
+                return 1;
+    
+            int curr = 0;
+            int prev1 = 1;
+            int prev2 = 1;
+            
+            for(int i = 2; i <= n; i++){
+                curr = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = curr;
+            }
+    
+            return curr;
+        }
+    };
+    
