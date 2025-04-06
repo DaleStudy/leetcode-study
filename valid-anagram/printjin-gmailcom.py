f01effa03ebd30d0ed531039478c0d114f2e4cc7
printjin <printjin@gmail.com>
2025-04-06 23:26:03 +0900

    Refactor : Valid Anagram

--- valid-anagram/printjin-gmailcom.py
+++ valid-anagram/printjin-gmailcom.py
@@ -1,9 +1,5 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str):
-        if len(s) != len(t):
-            return False
-        count = [0] * 26
-        for ch_s, ch_t in zip(s, t):
-            count[ord(ch_s) - ord('a')] += 1
-            count[ord(ch_t) - ord('a')] -= 1
-        return all(x == 0 for x in count)
+        return Counter(s) == Counter(t)
