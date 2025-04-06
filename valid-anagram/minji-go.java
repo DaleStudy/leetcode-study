f4191e91aa52120982844081c8b8522d255a66a2
minji-go <kohminji10@gmail.com>
2025-04-06 17:19:55 +0900

    feat: valid-anagram

--- valid-anagram/minji-go.java
+++ valid-anagram/minji-go.java
@@ -1,25 +1,23 @@
-/*
-    Problem: https://leetcode.com/problems/valid-anagram/
-    Description: return true if one string is an anagram of the other, one formed by rearranging the letters of the other
-    Concept:String, Hash Table, Sorting, Array, Counting, String Matching, Ordered Map, Ordered Set, Hash Function ...
-    Time Complexity: O(n), Runtime: 27ms
-    Space Complexity: O(n), Memory: 43.05MB
-*/
-import java.util.HashMap;
-import java.util.Map;
+/**
+ * <a href="https://leetcode.com/problems/valid-anagram/">week02-1.valid-anagram</a>
+ * <li> Description: return true if one string is an anagram of the other, one formed by rearranging the letters of the other   </li>
+ * <li> Concept:String, Hash Table, Sorting, Array, Counting, String Matching, Ordered Map, Ordered Set, Hash Function ...      </li>
+ * <li> Time Complexity: O(n), Runtime: 15ms    </li>
+ * <li> Space Complexity: O(n), Memory: 44.66MB </li>
+ */
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        if (s.length() != t.length()) return false;
 
-        Map<Character, Integer> charCount = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            charCount.put(s.charAt(i), charCount.getOrDefault(s.charAt(i), 0)+1);
-            charCount.put(t.charAt(i), charCount.getOrDefault(t.charAt(i), 0)-1);
-        }
-        for(Integer count : charCount.values()){
-            if(count !=0) return false;
-        }
-        return true;
+        Map<Integer, Integer> tmap = t.chars()
+                .boxed()
+                .collect(Collectors.toMap(i -> i, i -> 1, (i1, i2) -> i1 + i2));
+
+        Map<Integer, Integer> smap = s.chars()
+                .boxed()
+                .collect(Collectors.toMap(i -> i, i -> 1, (i1, i2) -> i1 + i2));
+
+        return tmap.equals(smap);
     }
 }
