d21dcff2098ead3eccd7ac0a33d69108e4382f5a
kut7728 <kut7728@hanyang.ac.kr>
2025-04-07 17:26:40 +0900

    climbing-stairs solved

--- valid-anagram/kut7728.swift
+++ valid-anagram/kut7728.swift
@@ -1,24 +1,20 @@
+//문자열 s, t를 받고, t가 s의 애너그램이면 true, 아니면 false 출력
+
 class Solution {
-    func climbStairs(_ n: Int) -> Int {
-        var result = 0
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        ///둘이 문자 갯수가 다르다면 바로 False
+        if s.count != t.count { return false }
         
-        for i in 0...(n/2) {
-            if i == 0 {
-                result += 1
-                continue
-            }
+        ///해쉬테이블 사용
+        var wordDic: [Character: Int] = [:]
 
-            let x = n - (2 * i)
-
-            result += ncm(x+i, i)
+        ///s의 글자들은 1씩 추가, t의 글자들은 1씩 감소
+        for (sChar, tChar) in zip(s, t) {
+            wordDic[sChar, default: 0] += 1
+            wordDic[tChar, default: 0] -= 1
         }
 
-        return result
-    }
-    
-    func ncm(_ n: Int, _ m: Int) -> Int {
-        if m == 1 { return n }
-        if m == n { return 1 }
-        return (1...m).reduce(1) { $0 * ($1 + n-m)/$1 }
+        ///딕셔너리의 모든 값이 상쇄되어 0이 되면 true
+        return wordDic.values.allSatisfy { $0 == 0 }
     }
 }
