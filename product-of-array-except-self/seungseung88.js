0383e004659560189280a178bc014a1794de0782
seungseung88 <lisu5336@gmail.com>
2025-04-08 22:54:57 +0900

    풀이2: Climbing Stairs #230

--- product-of-array-except-self/seungseung88.js
+++ product-of-array-except-self/seungseung88.js
@@ -6,37 +6,58 @@
  * 공간 복잡도: O(1)
  *  - 추가 배열 생성 X
  */
+// const productExceptSelf = (nums) => {
+//   let multiplyResult = 1;
+//   let countZero = 0;
+
+//   for (let i = 0; i < nums.length; i += 1) {
+//     if (nums[i] === 0) {
+//       countZero += 1;
+
+//       if (countZero >= 2) {
+//         multiplyResult = 0;
+//         break;
+//       }
+
+//       continue;
+//     }
+//     multiplyResult *= nums[i];
+//   }
+
+//   for (let i = 0; i < nums.length; i += 1) {
+//     if (countZero === 1) {
+//       if (nums[i] === 0) {
+//         nums[i] = multiplyResult;
+//       } else {
+//         nums[i] = 0;
+//       }
+//     } else if (countZero >= 2) {
+//       nums[i] = 0;
+//     } else {
+//       nums[i] = multiplyResult / nums[i];
+//     }
+//   }
+
+//   return nums;
+// };
+
+// 누적 합 이용
 const productExceptSelf = (nums) => {
-  let multiplyResult = 1;
-  let countZero = 0;
+  const result = Array(nums.length).fill(1);
+
+  let prefix = 1;
 
   for (let i = 0; i < nums.length; i += 1) {
-    if (nums[i] === 0) {
-      countZero += 1;
-
-      if (countZero >= 2) {
-        multiplyResult = 0;
-        break;
-      }
-
-      continue;
-    }
-    multiplyResult *= nums[i];
+    result[i] = prefix;
+    prefix *= nums[i];
   }
 
-  for (let i = 0; i < nums.length; i += 1) {
-    if (countZero === 1) {
-      if (nums[i] === 0) {
-        nums[i] = multiplyResult;
-      } else {
-        nums[i] = 0;
-      }
-    } else if (countZero >= 2) {
-      nums[i] = 0;
-    } else {
-      nums[i] = multiplyResult / nums[i];
-    }
+  let postfix = 1;
+
+  for (let i = nums.length - 1; i >= 0; i -= 1) {
+    result[i] *= postfix;
+    postfix *= nums[i];
   }
 
-  return nums;
+  return result;
 };
