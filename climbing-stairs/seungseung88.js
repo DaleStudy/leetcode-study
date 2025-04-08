56095179c5e3f0c260322702a120d0e414d8573d
seungseung88 <lisu5336@gmail.com>
2025-04-08 22:31:53 +0900

    풀이2. Climbing Stairs #230

--- climbing-stairs/seungseung88.js
+++ climbing-stairs/seungseung88.js
@@ -4,12 +4,30 @@
  * 공간복잡도: O(n)
  *  - arr O(n)
  */
-const climbStairs = (n) => {
-  const arr = [1, 2];
+// const climbStairs = (n) => {
+//   const arr = [1, 2];
 
-  for (let i = 2; i < n; i += 1) {
-    arr[i] = arr[i - 1] + arr[i - 2];
+//   for (let i = 2; i < n; i += 1) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   }
+
+//   return arr[n - 1];
+// };
+
+/**
+ * 시간복잡도: O(n)
+ *  - for문 O(n)
+ * 공간복잡도: O(1)
+ */
+const climbStairs = (n) => {
+  let one = 1;
+  let two = 1;
+
+  for (let i = 0; i <= n - 2; i += 1) {
+    let temp = one + two;
+    one = two;
+    two = temp;
   }
 
-  return arr[n - 1];
+  return two;
 };
