903bc2e3873fd99202d30e877a4af527d3fa7fe0
yayyz <yejiim.dev@gmail.com>
2025-04-09 23:32:59 +0900

    added 2nd solution to top-k-frequent-elements

--- top-k-frequent-elements/yayyz.py
+++ top-k-frequent-elements/yayyz.py
@@ -1,7 +1,21 @@
+# Solution 1: using Counter, heapq
 from collections import Counter
 import heapq 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_dict = Counter(nums) 
+        count_dict = Counter(nums)
         return heapq.nlargest(k, count_dict.keys(), key=count_dict.get)
+
+# Solution 2: create dict, use sorted function 
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         freq_dict = {}
+#         for num in nums: 
+#             if num in freq_dict: 
+#                 freq_dict[num] += 1
+#             else: 
+#                 freq_dict[num] = 1
+#         sorted_list = sorted(freq_dict.keys(), key = lambda x: freq_dict[x], reverse=True)
+#         return sorted_list[:k]
+        
