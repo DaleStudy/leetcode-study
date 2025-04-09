1ab2ab92c684e9516aa49000a97dc31b239fb0d1
김가은 <gaeun@danbiedu.co.kr>
2025-04-09 17:04:41 +0900

    solve: valid anagram

---
+++ valid-anagram/paran22.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    # time complexity: O(n)
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+    
+    
