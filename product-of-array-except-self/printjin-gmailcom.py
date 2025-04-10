914d94b31dc009caac2a7169e05645025a6e7de2
printjin <printjin@gmail.com>
2025-04-10 11:44:49 +0900

    Refactor : Product of Array Except Self

--- product-of-array-except-self/printjin-gmailcom.py
+++ product-of-array-except-self/printjin-gmailcom.py
@@ -1,10 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums):
-        ans = []
-        for i in range(len(nums)):
-            product = 1
-            for j in range(len(nums)):
-                if j != i:
-                    product *= nums[j]
-            ans.append(product)
-        return ans
+        n = len(nums)
+        answer = [1] * n
+        left = 1
+        for i in range(n):
+            answer[i] = left
+            left *= nums[i]
+        right = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= right
+            right *= nums[i]
+        return answer
