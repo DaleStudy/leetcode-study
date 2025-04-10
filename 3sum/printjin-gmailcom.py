09faf30ef38743d9be5769ebdccb2d55521fc31a
printjin <printjin@gmail.com>
2025-04-10 12:23:55 +0900

    Refactor : 3sum

--- 3sum/printjin-gmailcom.py
+++ 3sum/printjin-gmailcom.py
@@ -1,8 +1,10 @@
 class Solution:
     def threeSum(self, nums):
-        triplets = set()
+        triplets = []
         nums.sort()
         for i in range(len(nums) - 2):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
             low, high = i + 1, len(nums) - 1
             while low < high:
                 three_sum = nums[i] + nums[low] + nums[high]
@@ -11,6 +13,10 @@ class Solution:
                 elif three_sum > 0:
                     high -= 1
                 else:
-                    triplets.add((nums[i], nums[low], nums[high]))
+                    triplets.append([nums[i], nums[low], nums[high]])
+                    while low < high and nums[low] == nums[low + 1]:
+                        low += 1
+                    while low < high and nums[high] == nums[high - 1]:
+                        high -= 1
                     low, high = low + 1, high - 1
-        return list(triplets)
+        return triplets
