39309a04702f9a971491e81c0d194da59a072b05
jiyseo <jji5602@gmail.com>
2025-04-11 22:34:45 +0900

    [jiyseo] solve all problems

--- climbing-stairs/jiyseo.py
+++ climbing-stairs/jiyseo.py
@@ -1,17 +1,15 @@
 class Solution(object):
-    def fac(self, n) :
-        fac = 1
-        for i in range(1, n + 1) :
-            fac *= i
-        return fac
-
     def climbStairs(self, n):
-        res = 0
-        for i in range(n // 2 + 1) :
-            if i == 0 :
-                res += 1
-            else :
-                cnt = n - i
-                res += Solution().fac(cnt)/(Solution().fac(i) * Solution().fac(cnt - i))
-        return res
+        # 시간복잡도 = O(N)
+        if n < 2 :
+            return n
+
+        arr = [0] * (n + 1)
+        arr[1] = 1
+        arr[2] = 2
+
+        for i in range(3, n + 1) :
+            arr[i] = arr[i - 1] + arr[i - 2]
+
+        return arr[n]
         
