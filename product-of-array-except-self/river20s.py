8eecf23b61f115a8f5f923489c8fbab63e8d5cc6
river20s <river20s@naver.com>
2025-04-12 22:06:54 +0900

    feat: Add Solution to Product of Array Except Self #239

---
+++ product-of-array-except-self/river20s.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        TimeComplexity: O(n)
+        SpaceComplexity: O(n)
+        """
+        n = len(nums)
+        
+
+        # prefix product 수행
+        # left는 i번째 요소 왼쪽에 있는 요소들의 곱
+        left = [1] * n
+        for i in range(1, n):
+            left[i] = left[i - 1] * nums[i - 1]
+
+        # suffix product 수행
+        # right는 i번째 요소 오른쪽에 있는 요소들의 곱
+        right = [1] * n
+        for i in range(n - 2, -1, -1): # 배열의 마지막 인덱스, [n - 1]은 이미 1로 초기화 되어 있으므로 
+            right[i] = right[i + 1] * nums[i + 1]
+
+        # 두 배열을 이용하여 최종 출력
+        answer = [1] * n
+        for i in range(n):
+            answer[i] = left[i] * right[i]
+
+        return answer
+
+        
+        
