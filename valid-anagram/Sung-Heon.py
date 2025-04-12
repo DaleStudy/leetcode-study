5cbe34869897fc2b612c5366fe793356720493f6
E-1on <sungheon.jeong@dreamus.io>
2025-04-12 12:42:33 +0900

    valid anangram

---
+++ valid-anagram/Sung-Heon.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        try:
+            for i in s:
+                if len(t) == 0:
+                    return False
+                t.remove(i)
+        except:
+            return False
+        if len(t) == 0:
+            return True
+        else :
+            return False
