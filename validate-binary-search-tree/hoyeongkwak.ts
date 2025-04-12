0aee6c48ba06bb371b55ab198484e40b55893501
hoyeongkwak <ghy2002@hanmail.net>
2025-04-12 17:11:14 +0900

    validate binary search tree solution

---
+++ validate-binary-search-tree/hoyeongkwak.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+/*
+time complexity : O(n)
+space complexity : O(n)
+*/
+function isValidBST(root: TreeNode | null): boolean {
+    let prev = -Infinity
+    let isValid = true
+    const inOrder = (node: TreeNode | null): void => {
+        if (!isValid || !node) return
+        inOrder(node.left)
+        
+        if (prev >= node.val) {
+            isValid = false
+            return
+        }
+        prev = node.val
+        inOrder(node.right)
+    }
+    inOrder(root)
+    return isValid
+};
