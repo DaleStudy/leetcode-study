ab95a954e203b906f9eb0710a4902f41b4f6c785
hi-rachel <rachel.uiux@gmail.com>
2025-04-13 21:59:20 +0900

    3sum solution (py)

---
+++ 3sum/hi-rachel.py
@@ -0,0 +1,17 @@
+# O(n^2) time, O(n) space
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        triplets = set()
+
+        for i in range(len(nums) - 2):
+            seen = set()
+            for j in range(i + 1, len(nums)):
+                complement = -(nums[i] + nums[j])
+                if complement in seen:
+                    triplet = [nums[i], nums[j], complement]
+                    triplets.add(tuple(sorted(triplet)))
+                seen.add(nums[j])
+
+        return list(triplets)
+    
