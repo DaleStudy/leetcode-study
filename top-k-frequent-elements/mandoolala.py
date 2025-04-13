4048621f1db2987885801f76fac221115f45a6a3
MJ Kang <mj.kang@paywatchglobal.com>
2025-04-13 21:54:26 +0900

    eol

--- top-k-frequent-elements/mandoolala.py
+++ top-k-frequent-elements/mandoolala.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         countDict = {}
@@ -8,7 +9,7 @@ class Solution:
                 countDict[num] += 1
             else:
                 countDict[num] = 1
-        sortedDictList = sorted(countDict.items(), key=lambda item: item[1], reverse = True)
+        sortedDictList = sorted(countDict.items(), key=lambda item: item[1], reverse=True)
         freqElements = []
         for i in range(k):
             freqElements.append(sortedDictList[i][0])
