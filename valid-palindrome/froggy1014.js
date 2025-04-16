c6f76088f39b8572e1520db9a4e1c2fa8eb318fe
Jeongmin Lee <clcl6084@gmail.com>
2025-04-16 13:29:22 +0900

    valid palindrome solution

---
+++ valid-palindrome/froggy1014.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isPalindrome = function (s) {
+  const str = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
+console.log(isPalindrome(" "));
