41f23d70f0c0d52effc4178bc3f706b8f0936325
Jeongmin Lee <clcl6084@gmail.com>
2025-04-16 13:20:52 +0900

    Add climb-stairs solution

---
+++ climbing-stairs/froggy1014.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
+
+
+console.log(climbStairs(4));
