085e2978547524c25622a890c9c08fdeb12f7e0c
MJ Kang <mj.kang@paywatchglobal.com>
2025-04-19 16:25:24 +0900

    try using heap solution

--- top-k-frequent-elements/mandoolala.py
+++ top-k-frequent-elements/mandoolala.py
@@ -2,6 +2,29 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        Using (min)heap
+        [Complexity]
+        Time: O(log n)
+        Space: O(n+k)
+        '''
+        from heapq import heappush, heappop
+        counter = {}
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+        heap = []
+        for num, freq in counter.items():
+            heappush(heap, (freq, num))
+            if len(heap) > k:
+                heappop(heap)
+        return [num for _, num in heap]
+        '''
+        Using hash table 
+        [Complexity]
+        Time: O(n log n)
+        Space: O(n)
+        '''
+        '''
         countDict = {}
         for num in nums:
             if num in countDict:
@@ -13,3 +36,4 @@ class Solution:
         for i in range(k):
             freqElements.append(sortedDictList[i][0])
         return freqElements
+        '''
