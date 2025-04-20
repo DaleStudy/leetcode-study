35d78167f9fc1761a9721ac05caa98862337b957
printjin <printjin@gmail.com>
2025-04-20 10:11:33 +0900

    Solve : Maximum Depth of Binary Tree - DFS

--- maximum-depth-of-binary-tree/printjin-gmailcom.py
+++ maximum-depth-of-binary-tree/printjin-gmailcom.py
@@ -1,20 +1,12 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def maxDepth(self, root):
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-
-        queue = deque([root])
-        depth = 0
-
-        while queue:
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            depth += 1
-
-        return depth
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
