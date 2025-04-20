cbaca32bcb702ea21a4e82324ed302f91a61bb54
PDKhan <kkzoro1@gmail.com>
2025-04-19 22:10:03 -0700

    Maximum Depth of Binary Tree solution

---
+++ maximum-depth-of-binary-tree/PDKhan.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    public:
+        int maxDepth(TreeNode* root) {
+            if(root == nullptr)
+                return 0;
+            
+            return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        }
+    };
