f5e19826ea98f6c3f132d457d195875da117e724
JIHOON LEE <jihoon7705@gmail.com>
2025-04-21 10:57:43 +0900

    solving Merge Two Sorted Lists (#224)
    - https://github.com/DaleStudy/leetcode-study/issues/224

---
+++ merge-two-sorted-lists/jiji-hoon96.ts
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    
+};
