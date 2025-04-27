649197afbc4f5581b850c0eaa42503a4d3a6bac9
printjin <printjin@gmail.com>
2025-04-27 10:23:23 +0900

    Solve : Group Anagram 2

--- group-anagrams/printjin-gmailcom.py
+++ group-anagrams/printjin-gmailcom.py
@@ -4,6 +4,8 @@ class Solution:
     def groupAnagrams(self, strs):
         anagrams = defaultdict(list)
         for word in strs:
-            key = ''.join(sorted(word))
-            anagrams[key].append(word)
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            anagrams[tuple(count)].append(word)
         return list(anagrams.values())
