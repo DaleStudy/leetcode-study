1384fbf63a382354074eb306ba050d82f3478edc
printjin <printjin@gmail.com>
2025-04-27 10:40:57 +0900

    Solve : Word Break

---
+++ word-break/printjin-gmailcom.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s, wordDict):
+        wordSet = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+        
+        return dp[len(s)]
