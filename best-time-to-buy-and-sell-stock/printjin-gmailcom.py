eb177e6cd29b98cb4444a1f6da9ae59f7e9e545c
printjin <printjin@gmail.com>
2025-04-27 10:09:34 +0900

    Solve : Best Time to Buy And Sell Stock

---
+++ best-time-to-buy-and-sell-stock/printjin-gmailcom.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, price):
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        return max_profit
