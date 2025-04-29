7262bb7c9a449a6be59dab98236225634859ef93
river20s <river20s@naver.com>
2025-04-29 23:55:26 +0900

    feat: Add Solution to Group Anagrams #236

---
+++ group-anagrams/river20s.py
@@ -0,0 +1,14 @@
+import collections
+from typing import List
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram_groups = collections.defaultdict(list)
+        for s in strs:
+            key = tuple(sorted(s))
+            anagram_groups[key].append(s)
+        return list(anagram_groups.values())
