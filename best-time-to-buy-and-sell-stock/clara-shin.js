a01fdcffdb702d216dbcf270e05707ab455ddafe
Hyejin <hyejin.frontend@gmail.com>
2025-04-29 19:01:44 +0900

    update Best Time to Buy and Sell Stock solution

--- best-time-to-buy-and-sell-stock/clara-shin.js
+++ best-time-to-buy-and-sell-stock/clara-shin.js
@@ -16,17 +16,8 @@ var maxProfit = function (prices) {
 
   // 두 번째 날부터
   for (let i = 1; i < prices.length; i++) {
-    // 현재 가격이 최저 가격보다 낮으면 최저 가격 업데이트
-    if (prices[i] < minPrice) {
-      minPrice = prices[i];
-    }
-    // 현재 가능한 이익 계산 (현재 가격 - 최저 가격)
-    const currentProfit = prices[i] - minPrice;
-
-    // 최대 이익 업데이트
-    if (currentProfit > maxProfit) {
-      maxProfit = currentProfit;
-    }
+    minPrice = Math.min(minPrice, prices[i]); // 최저 가격 갱신
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice); // 최대 이익 갱신
   }
 
   return maxProfit;
