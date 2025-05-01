293aa8381555fd9bc8f64c22f1355c4ba19b0fd9
Yn3-3xh <tong13579@naver.com>
2025-05-02 00:29:15 +0900

    best time to buy and sell stock solution apply feedback from minji-go

--- best-time-to-buy-and-sell-stock/Yn3-3xh.java
+++ best-time-to-buy-and-sell-stock/Yn3-3xh.java
@@ -11,13 +11,9 @@ class Solution {
         int min = prices[0];
         int max = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (min > prices[i]) {
-                min = prices[i];
-                continue;
-            }
+            min = Math.min(min, prices[i]);
             max = Math.max(max, prices[i] - min);
         }
         return max;
     }
 }
-
