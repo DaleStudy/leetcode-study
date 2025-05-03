1f36003ed9d61c27511c35382d2c36eec308a14d
printjin <printjin@gmail.com>
2025-05-04 00:51:38 +0900

    Solve : Container With Most Water

--- container-with-most-water/printjin-gmailcom.py
+++ container-with-most-water/printjin-gmailcom.py
@@ -1,13 +1,8 @@
 class Solution:
     def maxArea(self, height):
-        left, right = 0, len(height) - 1
         max_area = 0
-        while left < right:
-            h = min(height[left], height[right])
-            w = right - left
-            max_area = max(max_area, h * w)
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
+        for i in range(len(height)):
+            for j in range(i + 1, len(height)):
+                area = (j - i) * min(height[i], height[j])
+                max_area = max(max_area, area)
         return max_area
