4102d1a13606cae9d28eb192b5d3ab6a8e6eb707
jinhyungrhee <rheejinhyung@gmail.com>
2025-05-07 11:57:38 +0900

    add solution of valid-parentheses

---
+++ valid-parentheses/jinhyungrhee.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> table = new HashMap<>();
+        table.put(')', '(');
+        table.put(']', '[');
+        table.put('}', '{');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (table.containsKey(s.charAt(i))) {
+
+                if ((table.get(s.charAt(i))).equals(stack.peek())) {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
+
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
