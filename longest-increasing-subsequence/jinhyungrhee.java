d8fac4be7f8545a4466910f03742fefd3dc6fae6
jinhyungrhee <rheejinhyung@gmail.com>
2025-05-09 15:04:11 +0900

    add solution of longest-increasing-subsequence

---
+++ longest-increasing-subsequence/jinhyungrhee.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1 ; i < nums.length; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxVal = 0;
+        for (int num : dp) {
+            if (num > maxVal) maxVal = num;
+        }
+
+        return maxVal;
+    }
+}
