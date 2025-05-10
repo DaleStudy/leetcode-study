09c809556f1b6c052d0ef9a464ed474c146529ef
minji-go <kohminji10@gmail.com>
2025-05-10 17:34:28 +0900

    refactor: valid-palindrome space complexity

--- valid-palindrome/minji-go.java
+++ valid-palindrome/minji-go.java
@@ -2,17 +2,41 @@
  * <a href="https://leetcode.com/problems/valid-palindrome/">week03-1.valid-palindrome</a>
  * <li>Description: return true if it is a palindrome </li>
  * <li>Topics: Two Pointers, String             </li>
- * <li>Time Complexity: O(N), Runtime 13ms      </li>
- * <li>Space Complexity: O(N), Memory 45.55MB   </li>
+ * <li>Time Complexity: O(N), Runtime 2ms       </li>
+ * <li>Space Complexity: O(1), Memory 42.75MB   </li>
  */
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.toLowerCase().replaceAll("[^0-9a-z]", "");
+        int left = 0;
+        int right = s.length() - 1;
 
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+        while (left < right) {
+            char charLeft = s.charAt(left);
+            char charRight = s.charAt(right);
+            if (isNotValidCharacter(charLeft)) {
+                left++;
+                continue;
+            }
+            if (isNotValidCharacter(charRight)) {
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(charLeft) != Character.toLowerCase(charRight)) {
                 return false;
             }
+            left++;
+            right--;
+        }
+        return true;
+
+    }
+
+    private boolean isNotValidCharacter(char c) {
+        if ((c >= '0' && c <= '9')
+                || (c >= 'a' && c <= 'z')
+                || (c >= 'A' && c <= 'Z')) {
+            return false;
         }
         return true;
     }
