1f2cb01ff74771c37cb00d6bfcd9e0fa65ec2175
printjin <printjin@gmail.com>
2025-05-11 10:12:24 +0900

    Fear : Reverse Linked List

---
+++ reverse-linked-list/printjin-gmailcom.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        current = head
+        while current:
+            nxt = current.next
+            current.next = prev
+            prev = current
+            current = nxt
+        return prev
