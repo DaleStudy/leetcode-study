240f8f175df032e6194d7d6432df9176dc2507ec
seungriyou <seungri0826@gmail.com>
2025-05-12 23:10:51 +0900

    solve(w07): 62. Unique Paths (modified)

--- unique-paths/seungriyou.py
+++ unique-paths/seungriyou.py
@@ -14,14 +14,10 @@ class Solution:
                          = dp[r - 1][c] + dp[r][c - 1]
             이때, dp table의 첫 번째 row와 column에 있는 칸들은 모두 도달할 수 있는 unique path의 개수가 1이므로 초기화해준다.
         """
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        # 첫 번째 row & column은 1로 초기화
+        dp = [[1 for _ in range(n)] for _ in range(m)]
 
-        # 첫 번째 row & column 초기화
-        for r in range(m):
-            dp[r][0] = 1
-        for c in range(n):
-            dp[0][c] = 1
-
+        # 두 번째 row & column 부터 순회
         for r in range(1, m):
             for c in range(1, n):
                 dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
@@ -39,15 +35,15 @@ class Solution:
             rolling array 기법을 이용해 1D DP로 space optimization이 가능하다.
             따라서 길이가 n인 1D dp list를 이용해
                 - c(= 1 ~ n - 1)를 순회하며 dp[c] += dp[c - 1]로 업데이트하는 것을 (2D DP에서의 dp[r - 1][c]가 dp[c] 값이므로!)
-                - row 개수인 m 번 반복
+                - row 개수인 m 번 반복 (단, 첫 번째 row를 1로 초기화 한 효과를 얻기 위해 dp를 1로 초기화 하고 m - 1번 반복)
             하면 된다.
         """
 
-        dp = [0] * n
-        dp[0] = 1
+        # 첫 번째 row는 1로 초기화 한 효과
+        dp = [1] * n
 
-        for _ in range(m):
-            for c in range(1, n):
+        for _ in range(1, m):  # m - 1 번 반복
+            for c in range(1, n):  # 첫 번째 col은 1로 초기화 한 효과
                 dp[c] += dp[c - 1]
 
         return dp[n - 1]
