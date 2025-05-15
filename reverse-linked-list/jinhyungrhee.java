6316472a16a8914cf486a69a64e616ae741100cd
jinhyungrhee <rheejinhyung@gmail.com>
2025-05-15 23:25:24 +0900

    add solution of reverse-linked-list

---
+++ reverse-linked-list/jinhyungrhee.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
