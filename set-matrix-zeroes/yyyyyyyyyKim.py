da62e1314e39e6f24be70358aef7657c10e48514
yyyyyyyyy <dadaa-@naver.com>
2025-05-15 19:37:07 +0900

    set-matrix-zeroes solution

--- set-matrix-zeroes/yyyyyyyyyKim.py
+++ set-matrix-zeroes/yyyyyyyyyKim.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         # 0이 있는 행, 열을 기록해두고 그 기록을 토대로 바꾸기(시간복잡도 O(m*n), 공간복잡도 O(m+n))
+        # Follow up : 공간복잡도 O(1) 생각해볼것 
         m, n = len(matrix), len(matrix[0])
         # rows, cols = [] , []  -> 중복되어 들어갈 수 있음. 불필요함. 중복제거를 위해 set 사용.
         rows, cols = set(), set()
