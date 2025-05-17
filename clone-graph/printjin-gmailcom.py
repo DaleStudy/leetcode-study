cee05de9a7d2090dc669cd00606f438d7c96272d
printjin <printjin@gmail.com>
2025-05-17 10:17:40 +0900

    Solve ; Clone Graph

--- clone-graph/printjin-gmailcom.py
+++ clone-graph/printjin-gmailcom.py
@@ -1,15 +1,17 @@
+from collections import deque
 from typing import Optional
+
 class Solution:
-    def cloneGraph(self, node):
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        old_to_new = {}
-        def dfs(n):
-            if n in old_to_new:
-                return old_to_new[n]
-            copy = Node(n.val)
-            old_to_new[n] = copy
-            for neighbor in n.neighbors:
-                copy.neighbors.append(dfs(neighbor))
-            return copy
-        return dfs(node)
+        old_to_new = {node: Node(node.val)}
+        queue = deque([node])
+        while queue:
+            current = queue.popleft()
+            for neighbor in current.neighbors:
+                if neighbor not in old_to_new:
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                old_to_new[current].neighbors.append(old_to_new[neighbor])
+        return old_to_new[node]
