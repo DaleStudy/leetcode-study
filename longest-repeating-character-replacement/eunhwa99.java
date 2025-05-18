d57871aeae58f9f0d9bdc301d55bb41c0581021f
eunhwa99 <ekdusp89@gmail.com>
2025-05-18 11:56:12 +0900

    longest repeating character replacement

--- longest-repeating-character-replacement/eunhwa99.java
+++ longest-repeating-character-replacement/eunhwa99.java
@@ -5,13 +5,15 @@ class Solution{
         int left = 0;
         int result = 0;
 
-        for (int right = 0; right < s.length(); right++) {
+        for (int right = 0; right < s.length(); right++) { // sliding window에서의 right pointer
             char currentChar = s.charAt(right);
-            count[currentChar - 'A']++;
+            count[currentChar - 'A']++; // 현재 문자 카운트 증가
             maxCount = Math.max(maxCount, count[currentChar - 'A']);
 
-            while (right - left + 1 - maxCount > k) {
-                count[s.charAt(left) - 'A']--;
+            while (right - left + 1 - maxCount > k) { // 현재 window의 길이 - 가장 많이 등장한 문자 개수 > k
+                 // 윈도우 크기 - maxCount > k가 되면, k번의 변경으로는 모든 문자를 동일하게 만들 수 없다는 뜻이므로
+                // 윈도우의 왼쪽 포인터(left)를 증가시켜 윈도우 크기를 줄인다.
+                count[s.charAt(left) - 'A']--; // left pointer의 문자 카운트 감소
                 left++;
             }
 
@@ -22,3 +24,5 @@ class Solution{
         return result;
     }
 }
+// Time Complexity: O(n)
+// Space Complexity: O(1)
