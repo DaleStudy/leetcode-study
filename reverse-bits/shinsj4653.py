17c7d2e1bbce7b2331c69c8dd754efa4a8700d7b
SEONG JUN SHIN <shinsj4653@gmail.com>
2025-05-23 19:11:08 +0900

    feat: add solutions

--- reverse-bits/shinsj4653.py
+++ reverse-bits/shinsj4653.py
@@ -13,3 +13,30 @@
 """
 
 
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        st = []
+
+        # while n > 0:
+        #    st.append(n % 2)
+        #    n //= 2  => 32 bit 길이 맞춰야함!
+
+        while len(st) < 32:
+            print('st.append: ', n % 2)
+            st.append(n % 2)
+            n //= 2
+
+        ret, num = 0, 0
+        print("st: ", st)
+
+        # 6 : 110
+        # [0 1 1]
+
+        while st:
+            print('st.pop(): ', st[-1])
+            ret += st.pop() * (2 ** num)
+            num += 1
+
+        return ret
+
+
