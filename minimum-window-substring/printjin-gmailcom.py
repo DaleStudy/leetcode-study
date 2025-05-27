bd5ec2506f7144e4d254d4bb92d4907e29bb5fc3
printjin <printjin@gmail.com>
2025-05-28 08:07:10 +0900

    Refactor : Minimum Window Substring

--- minimum-window-substring/printjin-gmailcom.py
+++ minimum-window-substring/printjin-gmailcom.py
@@ -1,23 +1,20 @@
 from collections import Counter
 
 class Solution:
-    def minWindow(self, s, t):
+    def minWindow(self, s: str, t: str) -> str:
         need = Counter(t)
         missing = len(t)
         left = start = end = 0
-        window = {}
-        for right, char in enumerate(s, 1): 
+        for right, char in enumerate(s):
             if need[char] > 0:
                 missing -= 1
             need[char] -= 1
-            if missing == 0:  
-                while left < right and need[s[left]] < 0:
-                    need[s[left]] += 1
-                    left += 1
+            while missing == 0:
                 if end == 0 or right - left < end - start:
-                    start, end = left, right
+                    start, end = left, right + 1
                 need[s[left]] += 1
-                missing += 1
+                if need[s[left]] > 0:
+                    missing += 1
                 left += 1
         return s[start:end]
     
