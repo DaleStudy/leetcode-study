b941159ff5db0c2a55454076232b5d4fa9a9b8de
krokerdile <crazykroker@gmail.com>
2025-05-30 16:47:17 +0900

    clone graph solution

---
+++ clone-graph/krokerdile.js
@@ -0,0 +1,26 @@
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(node) {
+    if (!node) return null;
+
+    const visited = new Map(); // 원본 노드 -> 복제 노드
+
+    function dfs(curr) {
+        if (visited.has(curr)) {
+            return visited.get(curr); // 이미 복제한 경우
+        }
+
+        const copy = new _Node(curr.val);
+        visited.set(curr, copy); // 복제한 노드 저장
+
+        for (const neighbor of curr.neighbors) {
+            copy.neighbors.push(dfs(neighbor)); // 이웃도 재귀적으로 복사
+        }
+
+        return copy;
+    }
+
+    return dfs(node);
+};
