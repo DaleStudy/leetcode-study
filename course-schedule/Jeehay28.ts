e6b8cf75536412f565a6476214ad9d2ae94bafe0
Jeehay28 <user01@MacBook-Pro.local>
2025-06-04 16:30:30 +0900

    Add course-schedule solution in TS

---
+++ course-schedule/Jeehay28.ts
@@ -0,0 +1,45 @@
+// TC: O(V + E), V = numCourses, E = prerequisites.length
+// SC: O(V + E)
+
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+  // 0: [1]
+  // 1: []
+
+  const graph = new Map<number, number[]>();
+
+  // Initialize all courses
+  for (let i = 0; i < numCourses; i++) {
+    graph.set(i, []);
+  }
+
+  // Build the graph
+  for (const [crs, pre] of prerequisites) {
+    graph.get(crs)!.push(pre);
+  }
+
+  const traversing = new Set<number>();
+  const finished = new Set<number>();
+
+  const canFinish = (crs: number): boolean => {
+    if (traversing.has(crs)) return false; // cycle detected
+
+    if (finished.has(crs)) return true; // already visited
+
+    traversing.add(crs);
+
+    for (const pre of graph.get(crs)!) {
+      if (!canFinish(pre)) return false;
+    }
+
+    traversing.delete(crs);
+    finished.add(crs);
+
+    return true;
+  };
+
+  for (const crs of graph.keys()) {
+    if (!canFinish(crs)) return false;
+  }
+
+  return true;
+}
