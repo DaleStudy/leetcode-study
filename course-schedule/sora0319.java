9083b8b7954f972385e20eda911f06a0e50bf10c
sora0319 <sojeoung536985@gmail.com>
2025-06-07 22:27:27 +0900

    solve course schedule

---
+++ course-schedule/sora0319.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] degree = new int[numCourses];
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] p : prerequisites) {
+            int course = p[0];
+            int pre = p[1];
+
+            degree[course]++;
+
+            if (!graph.containsKey(pre)) {
+                graph.put(pre, new ArrayList<>());
+            }
+            graph.get(pre).add(course);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int finishedCourses = 0;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            finishedCourses++;
+
+            if (graph.containsKey(curr)) {
+                for (int neighbor : graph.get(curr)) {
+                    degree[neighbor]--;
+                    if (degree[neighbor] == 0) {
+                        q.offer(neighbor);
+                    }
+                }
+            }
+        }
+
+        if(finishedCourses == numCourses) return true;
+
+        return false;
+    }
+}
