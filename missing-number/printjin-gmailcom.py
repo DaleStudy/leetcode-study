37cada7510c5a66ec1e1f134dec8da5dd97cb2de
printjin <printjin@gmail.com>
2025-06-08 14:44:56 +0900

    Solve : Missing Number

---
+++ missing-number/printjin-gmailcom.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
