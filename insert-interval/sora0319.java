1426c173161a3bc6f26ad7704b99b667029ddb4c
sora0319 <sojeoung536985@gmail.com>
2025-06-28 23:07:40 +0900

    solve insert interval

---
+++ insert-interval/sora0319.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> list = new ArrayList<>();
+
+        int order = 0;
+        while (order < intervals.length && intervals[order][0] < newInterval[0]) {
+            list.add(intervals[order]);
+            order++;
+        }
+        list.add(newInterval);
+        while (order < intervals.length) {
+            list.add(intervals[order]);
+            order++;
+        }
+
+        List<int[]> output = new ArrayList<>();
+        output.add(list.get(0));
+
+        for (int i = 1; i < list.size(); i++) {
+            int[] last = output.get(output.size() - 1);
+            int[] current = list.get(i);
+            if (last[1] < current[0]) {
+                output.add(current);
+            } else {
+                last[1] = Math.max(last[1], current[1]);
+            }
+        }
+
+        return output.toArray(new int[output.size()][]);
+    }
+}
