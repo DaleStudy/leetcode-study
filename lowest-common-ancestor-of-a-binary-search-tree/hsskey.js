a53b3bccbd97b9127ab91afbd9e0474fdcb40acc
hsskey <slingshot800@gmail.com>
2025-06-28 10:41:09 +0900

    week13 solved

---
+++ lowest-common-ancestor-of-a-binary-search-tree/hsskey.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    let cur = root;
+  
+    while (cur) {
+      if (p.val > cur.val && q.val > cur.val) {
+        cur = cur.right;
+      } else if (p.val < cur.val && q.val < cur.val) {
+        cur = cur.left;
+      } else {
+        return cur;
+      }
+    }
+  
+    return null;
+  };
+
+  
