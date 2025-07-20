b66f205a0451a2c5b5bbd131cec727a77173efbb
hu6r1s <darmaa333@gmail.com>
2025-07-21 00:14:43 +0900

    fix: Add missing newline characters for proper formatting

--- contains-duplicate/hu6r1s.py
+++ contains-duplicate/hu6r1s.py
@@ -10,3 +10,4 @@ class Solution:
                 return True
         else:
             return False
+        
--- two-sum/hu6r1s.py
+++ two-sum/hu6r1s.py
@@ -15,3 +15,4 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+                
