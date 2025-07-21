f9870c8f8904e9bb917e9550b944a85335f5eb93
kyoungwon <mkkw2047@gmail.com>
2025-07-21 21:54:07 +0900

    change: map -> set

--- contains-duplicate/mkwkw.java
+++ contains-duplicate/mkwkw.java
@@ -1,18 +1,20 @@
-//using map
+//1. using map
+//2. using set : why? It doesn't have to use a pair of key and value.
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Boolean> appearance = new HashMap<>();
+        //Map<Integer, Boolean> appearance = new HashMap<>();
+        Set<Integer> appearance = new HashSet<>();
 
         for(int i=0; i<nums.length; i++)
         {
-            if(appearance.containsKey(nums[i]))
+            if(appearance.contains(nums[i]))
             {
                 return true;
             }
             else
             {
-                appearance.put(nums[i], true);
+                appearance.add(nums[i]);
             }
         }
 
