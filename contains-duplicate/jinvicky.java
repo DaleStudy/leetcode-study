2261b21251e521276989122a2ef704711a568eb6
jinvicky <jinvicky@intube.kr>
2025-07-21 11:16:24 +0900

    contains duplicate solution

---
+++ contains-duplicate/jinvicky.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.add(num)) return true;
+        }
+        return false;
+    }
+}
