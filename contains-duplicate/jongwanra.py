1e822efec9329020d50d711f40d72b97cc455ba9
jongwanra <najongsi@gmail.com>
2025-07-21 09:04:46 +0900

    contains-duplicate solution

---
+++ contains-duplicate/jongwanra.py
@@ -0,0 +1,18 @@
+
+"""
+[문제]
+https://leetcode.com/problems/contains-duplicate/description/
+
+[문제 접근 방법]
+Set 자료구조를 활용하여 중복 여부를 개수로 확인한다.
+
+[Complexity]
+N: nums 길이
+TC: O(N)
+SC: O(N)
+"""
+
+class Solution(object):
+    def containsDuplacate(self, nums):
+        num_set = set(nums)
+        return len(num_set) != len(nums)
