2ac451811a70d6add2b6d4f42dfc671862e0f2fa
owen <owen1127@naver.com>
2025-07-21 12:32:43 +0900

    feat : two-sum 문제풀이 완료

---
+++ two-sum/Kyojin-Hwang.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+}
