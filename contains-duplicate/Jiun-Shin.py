6fd573f55bb96d0c4b35a17ca7cb3414d8384db0
jiunshinn <jiun.dev@gmail.com>
2025-07-21 17:36:23 +0100

    contains duplicate solution

---
+++ contains-duplicate/Jiun-Shin.py
@@ -0,0 +1,12 @@
+# 시간 복잡도 : o(n) for loop
+# 공간 복잡도 o(1) hash map 
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap = {}
+
+        for i, n in enumerate(nums):
+            if n in hashmap:
+                return True
+            hashmap[n] = i
+        return False
