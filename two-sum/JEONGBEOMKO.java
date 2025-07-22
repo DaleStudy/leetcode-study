0fefe2c6949fb0a01f70d2e5ce53c495e645a015
JEONGBEOM KO <beom9807@gmail.com>
2025-07-23 00:18:16 +0900

    feat: two sum

--- two-sum/JEONGBEOMKO.java
+++ two-sum/JEONGBEOMKO.java
@@ -1,25 +1,24 @@
-package week01.twosum;
-
 import java.util.HashMap;
 import java.util.Map;
+/*
+시간복잡도: O(n)
+공간복잡도: O(n)
+ */
 
-public class JEONGBEOMKO {
-
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            Map<Integer, Integer> numberMap = new HashMap<>();
-            for (int i=0; i<nums.length; i++) {
-                numberMap.put(nums[i], i);
-            }
-
-            for(int i=0; i<nums.length; i++) {
-                int operand = target - nums[i];
-                if (numberMap.containsKey(operand) && numberMap.get(operand) != i) { // 자기 자신은 제외
-                    return new int[] { numberMap.get(target - nums[i]), i };
-                }
-            }
-
-            return new int[] {};
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numberMap = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            numberMap.put(nums[i], i);
         }
+
+        for(int i=0; i<nums.length; i++) {
+            int operand = target - nums[i];
+            if (numberMap.containsKey(operand) && numberMap.get(operand) != i) {
+                return new int[] { numberMap.get(target - nums[i]), i };
+            }
+        }
+
+        return new int[] {};
     }
 }
