5ed99e8319e20593b32bb23467df10b300b01355
abc <>
2025-07-22 22:00:20 +0900

    two sum solution

---
+++ two-sum/SeongA.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dictionary: [Int: Int] = [:]
+        for (index, value) in nums.enumerated() {
+            let difference = target - value
+            if let otherIndex = dictionary[difference] {
+                return [otherIndex, index]
+            }
+            dictionary[value] = index
+        }
+        return []
+    }
+}
