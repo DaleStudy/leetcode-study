48c07375c5ba8361395b3b8d6a83cfccee7f149c
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-07-22 10:20:17 +0900

    feat: top k frequent 문제 풀이

--- top-k-frequent-elements/grapefruitgreentealoe.js
+++ top-k-frequent-elements/grapefruitgreentealoe.js
@@ -27,3 +27,31 @@ var topKFrequent = function (nums, k) {
 };
 
 //O(n) + O(n log n) + O(n) + O(k) = O(n log n)
+
+
+//7.22 소요시간 10분
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    //우선은, Map으로 만들어서 숫자 각각에 대한 개수를 구한다. 
+    const numsMap  = new Map();
+    for(let num of nums){
+        if(numsMap.has(num)){
+            numsMap.set(num,numsMap.get(num)+1)
+        }else{
+            numsMap.set(num,1)
+        }
+    }
+    //그런다음 Map에 대해서, 가장 큰 값을 가진 순으로 정렬을 한다. 
+    const ret = [...numsMap].sort((a,b)=>b[1]-a[1]).slice(0,k).map(x=>x[0]);
+    return ret
+};
+
+// 이전 답과 비슷하지만, for문 처리에 대해서, 조금 풀어서 작성을 하게 된거같다.
+// 시간복잡도와 공간복잡도는 위의 코드와 같다. 
+
+// 시간 복잡도 : for문 - O(n) , sort O(nlogn)
+// 공간 복잡도 : O(n)
