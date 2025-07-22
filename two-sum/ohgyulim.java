319db1e75111eb83e0b7636dd33a8acc631607fe
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-07-22 16:37:08 +0900

    two sum solution

--- two-sum/ohgyulim.java
+++ two-sum/ohgyulim.java
@@ -1,6 +1,12 @@
 import java.util.*;
 
 class Solution {
+/* 시간 복잡도: O(N), nums.length를 n이라고 할 때, 시간 복잡도는 O(N)
+ * - for 루프: O(N)
+ * - HashMap 연산 (containsKey, put): 평균 O(1)
+ * 
+ * 공간 복잡도: O(N), HashMap에 최대 n개의 요소를 저장할 수 있음
+ */ 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numToIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
