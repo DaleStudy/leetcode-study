d5033487acf8373939a23764340029a17e7ba0fd
yejin <yejinoh58@gmail.com>
2025-07-22 15:35:02 +0900

    longest consecutive sequence solution

--- top-k-frequent-elements/devyejin.py
+++ top-k-frequent-elements/devyejin.py
@@ -1,7 +1,9 @@
 from collections import Counter
+import heapq
 
 
 class Solution(object):
     def topKFrequent(self, nums, k):
         counter = sorted(Counter(nums).items(), key=lambda item: -item[1])
         return list(num for num, count in counter[:k])
+
--- two-sum/devyejin.py
+++ two-sum/devyejin.py
@@ -12,3 +12,4 @@ class Solution(object):
                 left += 1
             else:
                 right -= 1
+
