1e37b253331ec1488600683b05707be051d00dd3
hj4645 <hjpark4645@gmail.com>
2025-07-22 19:01:49 +0900

    Solve: two-sum

---
+++ two-sum/hj4645.py
@@ -0,0 +1,12 @@
+class Solution:
+    # 2개의 수를 합해 target이 되는 경우를 찾는 문제
+    # 순서가 보장되는 python dictionary를 사용해서,
+    # 요소 x에 대해서 target-x 가 딕셔너리 내에 있는지를 찾는다.
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i, num in enumerate(nums):
+            remain = target - num
+            if remain in dict:
+                return [dict[remain], i]
+            dict[num] = i
+            
