ba36c6cbf017a06ff27bf612e0f1f310864e214a
dohee789 <dohee@doheeui-MacBookPro.local>
2025-07-23 20:05:18 +0900

    :speech_balloon: modify line breaks

--- contains-duplicate/dohee789.java
+++ contains-duplicate/dohee789.java
@@ -4,12 +4,14 @@ https://leetcode.com/problems/contains-duplicate/
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet();
+
         for(int n: nums){
             if(set.contains(n)){
                 return true;
             }
             set.add(n);
         }
+
         return false;
     }
 }
