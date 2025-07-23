2206296aa413c29788b37868e54cdba436c57100
Noh <nhw3152@gmail.com>
2025-07-23 15:11:53 +0900

    Contains Duplicate solved

---
+++ contains-duplicate/do-heewan.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        sets = set(nums)
+
+        if len(nums) != len(sets):
+            return True
+        return False
