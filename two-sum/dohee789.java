f6ef448c32076bb40a863501feef726c82bca4f8
dohee789 <dohee@doheeui-MacBookPro.local>
2025-07-23 20:00:12 +0900

    :necktie: two-sum solution

---
+++ two-sum/dohee789.java
@@ -0,0 +1,20 @@
+/*
+https://leetcode.com/problems/two-sum/
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])
+                    && map.get(target - nums[i]) != i){
+                int j = map.get(target - nums[i]);
+                return new int[]{i, j};
+            }
+        }
+        return null;
+    }
+}
