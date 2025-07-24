0d7e8da1b65a515b175889c87701869a188d35b4
delight010 <seonga.oh00@gmail.com>
2025-07-24 23:25:09 +0900

    Add comment to two-sum

--- two-sum/delight010.swift
+++ two-sum/delight010.swift
@@ -2,6 +2,7 @@ class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dictionary: [Int: Int] = [:]
         for (index, value) in nums.enumerated() {
+            // nums배열의 개수만큼 반복합니다. O(n)
             let difference = target - value
             if let otherIndex = dictionary[difference] {
                 return [otherIndex, index]
