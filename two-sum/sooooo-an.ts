84a8898a1799801f6d30063e55b4d548c7777e89
sooooo-an <devsoo0527@gmail.com>
2025-07-24 22:47:37 +0900

    two-sum

---
+++ two-sum/sooooo-an.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+/**
+ * runtime: 30ms / Beats 38.67%
+ * memory 56.56MB / Beats 63.38%
+ */
