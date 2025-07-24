7720eff76612fe42016758dc373212e435cb2d0a
최준영[chris][Tech] <chrisjune@kreamcorp.com>
2025-07-24 10:29:39 +0900

    two sum refactoring

--- two-sum/chrisjune.py
+++ two-sum/chrisjune.py
@@ -3,9 +3,9 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dic = {num: idx for idx, num in enumerate(nums)}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j:
-                    continue
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            remain = target - nums[i]
+            exists_idx = nums_dic.get(remain)
+            if exists_idx and exists_idx != i:
+                return i, exists_idx
