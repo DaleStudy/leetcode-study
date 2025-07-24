ffaa6d8f6bc6807a7589ce4fe3644d2fa1c0da46
delight010 <seonga.oh00@gmail.com>
2025-07-24 23:22:14 +0900

    Add comments to top-k-frequent-element

--- top-k-frequent-elements/delight010.swift
+++ top-k-frequent-elements/delight010.swift
@@ -1,11 +1,19 @@
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
         var dictionary: [Int: Int] = [:]
-        for num in nums {
+        for num in nums { // for loop를 돌면서 O(n)의 시간복잡도
             dictionary[num, default: 0] += 1
+            // Swift에서 dictionary 검색의 시간복잡도는 O(1)
         }
         
-        return dictionary.sorted(by: { $0.value > $1.value }).prefix(k).map(\.key)
+        return dictionary
+            .sorted(by: { $0.value > $1.value })
+        // dictionary sorted()의 시간복잡도 O(n log n)
+            .prefix(k)
+        // k의 개수만큼 탐색합니다. 고로 시간복잡도는 O(n)
+            .map(\.key)
+        // prefix에서 k만큼 탐색하였습니다.
+        // .map의 시간복잡도는 O(n)입니다.
     }
 }
  
