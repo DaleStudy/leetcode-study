41e563904a05fffc505a17d5f744b6759bad2cf5
JIA <shchj66@gmail.com>
2025-07-26 00:27:31 +0900

    top-k-frequent 문제를 잘못 커밋해서 다시 수정하여 커밋

--- contains-duplicate/Geegong.java
+++ contains-duplicate/Geegong.java
@@ -1,80 +1,22 @@
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Geegong {
 
-    public int[] topKFrequent(int[] nums, int k) {
-        int[] result = new int[k];
 
-        // key : num element in nums / value : frequency of num elements
-        Map<Integer, Integer> numMap = new HashMap<>();
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> uniques = new HashSet<>();
 
-        // key : frequency of num elements / value : HashSet<Integer> num elements
-        Map<Integer, HashSet<Integer>> frequencyMap = new HashMap<>();
-
-        // most frequent numbers
-        int maxCount = 0;
-
-        // initialize numMap
         for (int num : nums) {
-            if (numMap.containsKey(num)) {
-                Integer alreadyCounted = numMap.get(num);
-                numMap.put(num, alreadyCounted + 1);
-            } else {
-                numMap.put(num, 1);
+            if (uniques.contains(num)) {
+                return true;
             }
+
+            uniques.add(num);
         }
 
-
-        //numHashSetMap
-        for (int num : numMap.keySet()) {
-            int frequencyOfNum = numMap.get(num);
-            maxCount = Math.max(maxCount, frequencyOfNum);
-
-            if (frequencyMap.containsKey(frequencyOfNum)) {
-                HashSet<Integer> alreadySet = frequencyMap.get(frequencyOfNum);
-                alreadySet.add(num);
-
-                frequencyMap.put(frequencyOfNum, alreadySet);
-
-            } else {
-                HashSet<Integer> newHashSet = new HashSet<>();
-                newHashSet.add(num);
-
-                frequencyMap.put(frequencyOfNum, newHashSet);
-            }
-        }
-
-
-        // maxCount 부터 decreasing
-        int resultIndex=0;
-        for(int frequency=maxCount; frequency>=0; frequency--) {
-            if (resultIndex >= result.length) {
-                return result;
-            }
-
-            if (frequencyMap.containsKey(frequency)) {
-                HashSet<Integer> numElements = frequencyMap.get(frequency);
-
-                for (int numElement : numElements) {
-                    result[resultIndex] = numElement;
-                    resultIndex++;
-
-
-                    if (resultIndex >= result.length) {
-                        return result;
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public boolean checkIsFull(int[] arr, int currIndex) {
-        return currIndex >= arr.length;
+        return false;
     }
 
 }
