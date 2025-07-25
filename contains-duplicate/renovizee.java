f13c67cfccc8f86ea534336e940ffc1496191838
renovizee <renovizee@gmail.com>
2025-07-25 21:40:16 +0900

    feat(leetcode/217): Solve Contains Duplicate (Easy)

--- contains-duplicate/renovizee.java
+++ contains-duplicate/renovizee.java
@@ -1,9 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
 
+// tag renovizee 1week
 // https://github.com/DaleStudy/leetcode-study/issues/217
 // https://leetcode.com/problems/contains-duplicate/
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-        return true;
+        // 시간복잡도 : O(n)
+        // 공간복잡도 : O(n)
+        Map<Integer,Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            int count = countMap.getOrDefault(num, 0);
+            int addCount = count + 1;
+            countMap.put(num, addCount);
+            if (addCount == 2) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
+//-------------------------------------------------------------------------------------------------------------
+// 기본 문법 피드백
+// 1) Map 기본 문법, ~.getOrDefault()
+//-------------------------------------------------------------------------------------------------------------
