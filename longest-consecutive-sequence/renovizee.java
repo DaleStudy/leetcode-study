b8140d5437fa138d138e2fcce1bdc05b525ad77e
renovizee <renovizee@gmail.com>
2025-07-25 21:49:58 +0900

    feat(leetcode/128): Solve Longest Consecutive Sequence (Medium)

--- longest-consecutive-sequence/renovizee.java
+++ longest-consecutive-sequence/renovizee.java
@@ -1,10 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
 
+// tag renovizee 1week unresolved
 // https://github.com/DaleStudy/leetcode-study/issues/240
 // https://leetcode.com/problems/longest-consecutive-sequence/
 class Solution {
     public int longestConsecutive(int[] nums) {
+        // 시간복잡도 : O(n)
+        // 공간복잡도 : O(n)
 
-        return 1;
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
 
+        int maxCount = 0;
+        for (int num : nums) {
+            if (numSet.contains(num - 1)) continue;
+            int currentCount = 1;
+            while (numSet.contains(num + currentCount)) {
+                currentCount++;
+            }
+            maxCount = Math.max(maxCount, currentCount);
+        }
+        return maxCount;
     }
 }
+
+
+//-------------------------------------------------------------------------------------------------------------
+// 기본 문법 피드백
+// 1) Set<Integer> numSet = new HashSet<>();
+// 2) Math 활용 Math.max(maxCount, currentCount);
+//-------------------------------------------------------------------------------------------------------------
