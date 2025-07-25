ffc11b7d5aa5142bd645b4532c012e18fb67abb6
JIA <shchj66@gmail.com>
2025-07-26 00:20:54 +0900

    two-sum

---
+++ two-sum/Geegong.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Geegong {
+
+    /**
+     * time complexity : O(n)
+     * space complexity : O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        // if target = -9 / num = 1 , num = -10
+        for (int index=0; index<nums.length; index++) {
+            map.put(nums[index], index);
+        }
+
+        for (int index=0; index<nums.length; index++) {
+            int difference = target - nums[index];
+
+            if (map.containsKey(difference)
+                    && map.get(difference) != index) {
+                result[0] = index;
+                result[1] = map.get(difference);
+                return result;
+            }
+        }
+
+        return result;
+    }
+}
