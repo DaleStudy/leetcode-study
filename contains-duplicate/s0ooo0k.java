ff72247168063b59442d7c7b6e42af8cdb92e0bd
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-07-25 14:30:07 +0900

    Add contains-duplicate solution - s0ooo0k

---
+++ contains-duplicate/s0ooo0k.java
@@ -0,0 +1,17 @@
+/*
+ * 시간복잡도 O(n)
+ * 공간복잡도 O(n)
+ */
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int n : nums) {
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
