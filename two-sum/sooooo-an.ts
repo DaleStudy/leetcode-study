2d73efd3d46a5044a15b8b2e507dc207b04263f2
sooooo-an <devsoo0527@gmail.com>
2025-07-26 11:48:08 +0900

    Refactor two-sum

--- two-sum/sooooo-an.ts
+++ two-sum/sooooo-an.ts
@@ -1,14 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
+  const visited: { [key: string]: boolean } = {};
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+    const complement = target - nums[i];
+    if (visited[complement]) {
+      const complementIdx = nums.indexOf(complement);
+      return [i, complementIdx];
     }
+    visited[nums[i]] = true;
   }
+  return [];
 }
 
 /**
- * runtime: 30ms / Beats 38.67%
- * memory 56.56MB / Beats 63.38%
+ * runtime: O(n)
+ * memory O(n)
  */
