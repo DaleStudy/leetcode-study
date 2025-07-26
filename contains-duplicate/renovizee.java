31290f2dd59fc5e8598005832dbf689be0d8f5de
renovizee <renovizee@gmail.com>
2025-07-26 09:44:09 +0900

    feat(leetcode/217): Solve Contains Duplicate (Easy) - feedback map to set

--- contains-duplicate/renovizee.java
+++ contains-duplicate/renovizee.java
@@ -1,24 +1,42 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 // tag renovizee 1week
 // https://github.com/DaleStudy/leetcode-study/issues/217
 // https://leetcode.com/problems/contains-duplicate/
 class Solution {
+
+    //    Solv2: hash set (feedback)
     public boolean containsDuplicate(int[] nums) {
         // 시간복잡도 : O(n)
         // 공간복잡도 : O(n)
-        Map<Integer,Integer> countMap = new HashMap<>();
+        Set<Integer> numsSet = new HashSet<>();
         for (int num : nums) {
-            int count = countMap.getOrDefault(num, 0);
-            int addCount = count + 1;
-            countMap.put(num, addCount);
-            if (addCount == 2) {
+            if (numsSet.contains(num)) {
                 return true;
             }
+            numsSet.add(num);
         }
         return false;
     }
+
+//    Solv1: hash map
+//    public boolean containsDuplicate(int[] nums) {
+//        // 시간복잡도 : O(n)
+//        // 공간복잡도 : O(n)
+//        Map<Integer,Integer> countMap = new HashMap<>();
+//        for (int num : nums) {
+//            int count = countMap.getOrDefault(num, 0);
+//            int addCount = count + 1;
+//            countMap.put(num, addCount);
+//            if (addCount == 2) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
 
 //-------------------------------------------------------------------------------------------------------------
