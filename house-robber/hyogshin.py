80ad178766f963444b7b818255625406466d8579
Hyogyeong Shin <haydenhyo@gmail.com>
2025-07-26 12:20:34 +0900

    feat: add 0264 House Robber solution

---
+++ house-robber/hyogshin.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0] * (len(nums))
+        dp[0] = nums[0]
+        if len(nums) > 1:
+            dp[1] = max(dp[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        
+        return max(dp)
+        
