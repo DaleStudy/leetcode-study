2e824d594a1afe37ac5c057c43ca234f501a8b56
jeongyunjae <yunjae2295@soongsil.ac.kr>
2025-07-26 14:45:23 +0900

    [main] two-sum review

--- two-sum/jeongyunjae.py
+++ two-sum/jeongyunjae.py
@@ -3,9 +3,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         my_dict = {}
 
-        for i in range(len(nums)):
-            remaining = target - nums[i]
+        for i, data in enumerate(nums):
+            remaining = target - data
             if remaining not in my_dict:
-                my_dict[nums[i]] = i
+                my_dict[data] = i
             else:
                 return [my_dict[remaining], i]
