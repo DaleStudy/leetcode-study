5ccadaac7ce5c12c65943e6208f4f3c12d9a5fa0
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-07-28 12:16:23 +0900

    feat: valid-anagram 풀이

---
+++ valid-anagram/grapefruitgreentealoe.js
@@ -0,0 +1,20 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    const map = new Map();
+
+    for (let ch of s) {
+        map.set(ch, (map.get(ch) || 0) + 1);
+    }
+
+    for (let ch of t) {
+        if (!map.has(ch)) return false;
+        map.set(ch, map.get(ch) - 1);
+        if (map.get(ch) < 0) return false;
+    }
+
+    return true;
+};
+
+//시간복잡도 : O(n)
+//공간복잡도 : O(1) (영문자니까 들어갈 수 있는 수가 한정되어있으므로. )
