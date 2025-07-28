2dd7074fa7a75d66698cf6b87d49a8fc799cd224
yejin <yejinoh58@gmail.com>
2025-07-28 22:37:24 +0900

    valid-anagram solution

--- valid-anagram/devyejin.py
+++ valid-anagram/devyejin.py
@@ -1,7 +1,14 @@
 from collections import Counter
 class Solution(object):
     def isAnagram(self, s, t):
-        return Counter(s) == Counter(t)
+        # return Counter(s) == Counter(t)
 
+        # str에서 제공하는 count 함수 활용
+        if len(s) != len(t):
+            return False
+        for i in set(s):
+            if s.count(i) != t.count(i):
+                return False
+        return True
 
 
