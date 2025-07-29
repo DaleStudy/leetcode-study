13a5cdfa32e856f4a77af2cc6ffeac3b073b484b
Noh <nhw3152@gmail.com>
2025-07-29 15:25:42 +0900

    climbing stairs

---
+++ climbing-stairs/do-heewan.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * 46
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
