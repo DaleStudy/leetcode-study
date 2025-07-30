2cde15abf760ee03b8f461d5b6f0a72b979211f5
hu6r1s <darmaa333@gmail.com>
2025-07-30 23:13:39 +0900

    refactor: data structure list -> set to 3sum problem

--- 3sum/hu6r1s.py
+++ 3sum/hu6r1s.py
@@ -8,30 +8,21 @@ class Solution:
         공간 복잡도는 둘다 O(1)
     """
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        result = set()
         nums.sort()
 
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            
+        for i in range(len(nums)):            
             left, right = i + 1, len(nums) - 1
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
 
                 if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
+                    result.add((nums[i], nums[left], nums[right]))
                     left += 1
                     right -= 1
-
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
                 elif total < 0:
                     left += 1
                 else:
                     right -= 1
 
-        return result
+        return list(result)
