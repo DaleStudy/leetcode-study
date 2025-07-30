342e4abcddbc07678b24d2b604314d8bed508916
kyoungwon <mkkw2047@gmail.com>
2025-07-30 21:08:38 +0900

    solve: valid-anagram [week2]

--- valid-anagram/mkwkw.cpp
+++ valid-anagram/mkwkw.cpp
@@ -1,1 +1,37 @@
-//set-up
+//map: key:alphabet, value:count
+//if t is an anagram of s
+// 1. same length
+// 2. same content of map
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        //1. different length -> false
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+
+        //2. different map -> false
+        map<char, int> sMap, tMap;
+        for(int i=0; i<s.length(); i++)
+        {
+            sMap[s[i]]++;
+            tMap[t[i]]++;
+        }
+
+        for(auto c : sMap)
+        {
+            if(tMap.find(c.first)==tMap.end())
+            {
+                return false;
+            }
+            if(tMap[c.first] != c.second)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
