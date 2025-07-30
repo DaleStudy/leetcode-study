6c92f110db52db828c6ecd569b5eccdce648574a
jangwonyoon <wkddnjs7310@naver.com>
2025-07-30 18:38:23 +0900

    📝 Docs: sovled 1 - 3sum

---
+++ 3sum/jangwonyoon.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ *
+ * 1. 투포인터를 사용하기 위해 정렬
+ * 2. 투포인터를 사용하여 합이 0인 경우 추가
+ * 3. 중복 된 값 스킵
+ *
+ * 시간 복잡도: O(n^2)
+ * 공간 복잡도: O(K + kLogK) (K: 결과 배열의 크기, kLogK: 정렬 공간)
+ */
+var threeSum = function(nums) {
+    const arr = [];
+
+    // 투포인터를 사용하기 위해 정렬
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] ===  nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                arr.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+
+    return arr;
+};
