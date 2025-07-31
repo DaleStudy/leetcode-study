9eb1636182ae0a78860d9b40c1ffe7be8abd7765
youngduck <dudejr5839@naver.com>
2025-07-31 09:30:10 +0900

    누적곱 풀이 주석수정

--- product-of-array-except-self/youngduck.js
+++ product-of-array-except-self/youngduck.js
@@ -7,8 +7,9 @@ var productExceptSelf = function (nums) {
 
   const result = new Array(numsLength).fill(1);
 
-  // for문 하나로 처리할경우 O(n^2).
+  // 일반적으로 for문 하나로 자기자신 제외하면서 곱셈을하면서 처리할경우 O(n^2).
   // for문 두개로 나눠서 처리할경우 O(n). 누적곱 개념을 활용해줘야함
+  // for문 하나로 누적곱 개념을 활용해서 처리할경우 O(n).
 
   let left = 1;
   let right = 1;
