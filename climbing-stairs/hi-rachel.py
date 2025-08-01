8e8234a467f2bda04683fb610b9730fbf5ffa4de
hi-rachel <rachel.uiux@gmail.com>
2025-08-01 23:46:01 +0900

    climbing-stairs 공간 복잡도 최적화 sol (py)

--- climbing-stairs/hi-rachel.py
+++ climbing-stairs/hi-rachel.py
@@ -34,3 +34,21 @@ class Solution:
         for i in range(2, n):
             steps[i] = steps[i - 2] + steps[i - 1]
         return steps[n - 1]
+
+"""
+변수 2개로 최적화
+공간 복잡도 O(1) 개선 풀이
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+
+        prev1 = 1
+        prev2 = 2
+        
+        for i in range(2, n):
+            prev1, prev2 = prev2, prev1 + prev2
+        return prev2
