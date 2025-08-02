e065a0d97c9414ee7d0919c59c5328c05b1af889
Tasha <45252527+Lustellz@users.noreply.github.com>
2025-08-02 20:03:58 +0900

    solution on valid-anagram

--- valid-anagram/Lustellz.ts
+++ valid-anagram/Lustellz.ts
@@ -1,1 +1,14 @@
 // week 2's goal
+// only focusing on not using for... not a good approach :(
+
+function isAnagram(s: string, t: string): boolean {
+    if(s.length !== t.length) return false
+    let sArray : string[] = s.split('').sort()
+    s = sArray.join('')
+    let tArray : string[] = t.split('').sort()
+    t = tArray.join('')
+    if(s === t) return true
+    else return false
+};
+
+// reflection: not to be hurry & have more time for pondering
