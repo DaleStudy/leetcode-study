c1aeef3eeb1cd3a296f117cc9fbabc3a77c23c52
renovizee <renovizee@gmail.com>
2025-08-03 18:21:35 +0900

    feat: Set up Week 3 problems
    Adds the initial boilerplate files for the week 3 problems, including:
    - Valid Palindrome (Easy)
    - Number of 1 Bits (Easy)
    - Combination Sum (Medium)
    - Decode Ways (Medium)
    - Maximum Subarray (Medium)

---
+++ number-of-1-bits/renovizee.java
@@ -0,0 +1,18 @@
+
+
+// tag renovizee 3week
+// https://github.com/DaleStudy/leetcode-study/issues/232
+// https://leetcode.com/problems/valid-palindrome/ #191 #Easy
+class Solution {
+    // Solv1 :
+    // 시간복잡도 : O(n)
+    // 공간복잡도 : O(n)
+    public int hammingWeight(int n) {
+
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Java 문법 피드백
+//
+//-------------------------------------------------------------------------------------------------------------
---
+++ valid-palindrome/renovizee.java
@@ -0,0 +1,18 @@
+
+
+// tag renovizee 3week
+// https://github.com/DaleStudy/leetcode-study/issues/220
+// https://leetcode.com/problems/valid-palindrome/ #125 #Easy
+class Solution {
+    // Solv1 :
+    // 시간복잡도 : O(n)
+    // 공간복잡도 : O(n)
+    public boolean isPalindrome(String s) {
+
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Java 문법 피드백
+//
+//-------------------------------------------------------------------------------------------------------------
