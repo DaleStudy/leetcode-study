1620167454b4028269e592a060bf2751f03c7596
dohee789 <dohee@doheeui-MacBookPro.local>
2025-08-05 21:00:34 +0900

    :necktie: longest-consecutive-sequence solution

---
+++ longest-consecutive-sequence/dohee789.java
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/longest-consecutive-sequence/
+ */
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }
+
+        int maxLength = 0;
+        for(int num: set){
+            if(!set.contains(num-1)){
+                int currentNum = num;
+                int length = 1;
+                while(set.contains(currentNum+1)){
+                    currentNum++;
+                    length++;
+                }
+                maxLength = Math.max(maxLength,length);
+            }
+        }
+        return maxLength;
+    }
+}
