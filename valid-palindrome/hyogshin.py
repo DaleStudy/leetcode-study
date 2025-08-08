f5cbc1c8330f82a9ced5cc0e8293fea6d9802a15
Hyogyeong Shin <haydenhyo@gmail.com>
2025-08-09 00:16:12 +0700

    docs: add time and space complexity

--- valid-palindrome/hyogshin.py
+++ valid-palindrome/hyogshin.py
@@ -1,3 +1,15 @@
+'''
+풀이
+- alphanumeric만 저장하는 alnum 배열 생성
+- alnum 배열의 절반 (소수점 버림) 길이를 순회하며 Palindrome 인지 확인
+
+시간 복잡도: O(n)
+- for loop * 2 -> O(2n) => O(n)
+
+공간 복잡도: O(n)
+- alphanumeric만 저장하는 alnum 배열이 최악의 경우 O(n) 일 수 있음
+'''
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         alnum = []
