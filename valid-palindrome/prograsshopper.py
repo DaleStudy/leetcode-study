9f4c8a9209acaeac3ff2f43396355dd68b569efa
prograsshopper <tjalwled12@gmail.com>
2025-08-08 19:54:29 +0900

    week 3: Valid Palindrome

---
+++ valid-palindrome/prograsshopper.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        formatted_string = "".join(elem.lower() for elem in s if elem.isalnum())
+
+        # sol 1
+        return formatted_string == formatted_string[::-1]
+
+        # sol 2
+        left = 0
+        right = len(formatted_string) - 1
+        while left < right:
+            if formatted_string[left] != formatted_string[right]:
+                return False
+        left += 1
+        right -= 1
+        return True
