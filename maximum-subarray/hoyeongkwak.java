eddfaee6a8a6a482b4eac81b0dda816fdb645006
hoyeongkwak <ghy2002@hanmail.net>
2025-08-09 10:10:10 +0900

    Week 3 Solutions

---
+++ maximum-subarray/hoyeongkwak.java
@@ -0,0 +1,23 @@
+/*
+Time complexity : O(n)
+Space complexity : O(1)
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
