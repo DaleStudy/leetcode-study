4e490caeb42e7afa9f137ea70185e57a724de917
jinvicky <jinvicky@intube.kr>
2025-08-12 12:58:23 +0900

    find minimum in rotated sorted array solution

---
+++ find-minimum-in-rotated-sorted-array/jinvicky.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        // 0번째값이 length-1번재 값보다 작다면 rotate되지 않았음을 보장합니다.
+        // 2,3,4,5,1  0번째 > length-1번째
+        // 3,4,5,1,2  0번째 > length-1번째
+        // 1,2,3,4,5  0번째 < length-1번째
+        if (nums[right] >= nums[left]) return nums[0];
+
+        while (left < right) {
+            // 단순히 (left+right)/2 보다 범위 계산 오차가 없습니다.
+            int mid = left + (right - left) / 2;
+
+            // 중간값이 오른쪽값보다 크다면 왼쪽 포인터를 중간값+1로 증가합니다.
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                // 중간값보다 오른쪽값이 크다면 중간값이 최대 범위가 되어야 합니다.
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
