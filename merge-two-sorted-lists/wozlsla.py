f056ea547311261b7267b788fdc176f26c50cc28
jimin <jiminseo0307@gmail.com>
2025-08-15 22:43:51 +0900

    solution: merge-two-sorted-lists

---
+++ merge-two-sorted-lists/wozlsla.py
@@ -0,0 +1,28 @@
+"""
+# Intuition
+-
+# Approach
+-
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
+        if not (list1 and list2):
+            return list1 or list2
+
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
