095850ef392745113147f678b55c11e60dd7df24
Jihyun Son <sonjh1217@naver.com>
2025-08-16 18:14:45 +0900

    Int overflow를 방지하는 스타일로 변경

--- find-minimum-in-rotated-sorted-array/sonjh1217.swift
+++ find-minimum-in-rotated-sorted-array/sonjh1217.swift
@@ -4,7 +4,7 @@ class Solution {
         var high = nums.count - 1
         
         while low <= high {
-            let mid = (low + high) / 2
+            let mid = low + (high - low) / 2
             
             if nums[mid] < nums[mid - 1] {
                 return nums[mid]
