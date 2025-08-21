b9abf48b0976d2b74b68cd92386b547675323d53
Hyogyeong Shin <haydenhyo@gmail.com>
2025-08-21 20:52:06 +0700

    docs: add time and space complexity

--- best-time-to-buy-and-sell-stock/hyogshin.py
+++ best-time-to-buy-and-sell-stock/hyogshin.py
@@ -1,3 +1,14 @@
+'''
+문제 풀이
+- 이중 for 문으로 구현시 O(n^2) 으로 시간 초과
+- least_num에 현재 날짜 이전에 가장 싸게 살 수 있는 금액을 업데이트
+- dp로 해당 날짜까지 가장 큰 수익을 저장
+시간 복잡도: O(n)
+- for 문 하나 -> O(n)
+공간 복잡도: O(n)
+- dp 리스트 -> O(n)
+'''
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp = [0] * (len(prices) + 1)
