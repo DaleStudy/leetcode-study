7f4ce768872159fe32e7392a8dbd5c3fcd6cadc5
Hyogyeong Shin <haydenhyo@gmail.com>
2025-08-21 20:54:58 +0700

    refactor: enhance space complexity by removing list

--- best-time-to-buy-and-sell-stock/hyogshin.py
+++ best-time-to-buy-and-sell-stock/hyogshin.py
@@ -11,9 +11,10 @@
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = [0] * (len(prices) + 1)
-        least_num = prices[0]
+        largest = 0
+        least = prices[0]
         for i in range(len(prices)):
-            least_num = min(prices[i], least_num)
-            dp[i] = max(prices[i] - least_num, dp[i-1])
-        return max(dp)
+            least = min(prices[i], least)
+            largest = max(prices[i] - least, largest)
+        return largest
+            
