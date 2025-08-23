f72c94c80196290553d4484b665e12fd2c885068
yhkee0404 <yhkee0404@gmail.com>
2025-08-23 21:24:57 +0900

    word break solution with pruning 2d

--- word-break/yhkee0404.scala
+++ word-break/yhkee0404.scala
@@ -4,15 +4,13 @@ object Solution {
         dp(0) = true
         (0 to s.length - 1).exists { i =>
             if (! dp(i)) false
-            else {
-                for (word <- wordDict if ! dp(s.length)) {
-                    val j = i + word.length
-                    if (j <= s.length && ! dp(j) && s.substring(i, j) == word) {
-                        dp(j) = true // T(s, wordDict, word) = O(s.length * wordDict.length * word.length)
+            else wordDict.exists { word =>
+                        val j = i + word.length
+                        if (j <= s.length && ! dp(j) && s.substring(i, j) == word) {
+                            dp(j) = true // T(s, wordDict, word) = O(s.length * wordDict.length * word.length)
+                        }
+                        dp(s.length)
                     }
-                }
-                dp(s.length)
-            }
         }
     }
 }
