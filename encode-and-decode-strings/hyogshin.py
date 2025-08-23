2e431717bf3e11342a402b3f67cda731f16c82ce
Hyogyeong Shin <haydenhyo@gmail.com>
2025-08-24 00:14:22 +0700

    docs: add time and space complexity

--- encode-and-decode-strings/hyogshin.py
+++ encode-and-decode-strings/hyogshin.py
@@ -1,3 +1,16 @@
+"""
+풀이 방법
+- 암호화 시  (단어의 개수) + '#' + (단어) 형식 사용
+
+시간 복잡도: O(n)
+- encode: join 이 모든 문자열을 이어붙임 -> O(n)
+- decode: while 문 -> O(n)
+
+공간 복잡도: O(n)
+- encode: 새로운 문자열 생성 -> O(n)
+- decode: ans 리스트 -> O(n)
+"""
+
 from typing import List
 class Solution:
     """
@@ -39,3 +52,5 @@ if __name__ == "__main__":
         enc = sol.encode(arr)
         dec = sol.decode(enc)
         print(arr == dec, arr, "->", enc[:50] + ("..." if len(enc) > 50 else ""))
+
+
