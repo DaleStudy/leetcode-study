4350260be8e4f87388822a11744ebf1ec7a55ba8
yhkee0404 <yhkee0404@gmail.com>
2025-08-23 21:19:20 +0900

    word break solution with pruning

--- word-break/yhkee0404.scala
+++ word-break/yhkee0404.scala
@@ -2,13 +2,17 @@ object Solution {
     def wordBreak(s: String, wordDict: List[String]): Boolean = {
         val dp = Array.fill(s.length + 1)(false) // S(s, wordDict, word) = O(s.length)
         dp(0) = true
-        for (i <- 1 to s.length) {
-            for (word <- wordDict) {
-                if (i >= word.length && dp(i - word.length) && s.substring(i - word.length, i) == word) {
-                    dp(i) = true // T(s, wordDict, word) = O(s.length * wordDict.length * word.length)
+        (0 to s.length - 1).exists { i =>
+            if (! dp(i)) false
+            else {
+                for (word <- wordDict if ! dp(s.length)) {
+                    val j = i + word.length
+                    if (j <= s.length && ! dp(j) && s.substring(i, j) == word) {
+                        dp(j) = true // T(s, wordDict, word) = O(s.length * wordDict.length * word.length)
+                    }
                 }
+                dp(s.length)
             }
         }
-        dp(s.length)
     }
 }
