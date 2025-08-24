3af8518ecd2a9f0597c901dcdfaf4462d712d09f
Jihyun Son <sonjh1217@naver.com>
2025-08-24 19:35:15 +0900

    반환 값이 없기 때문에 forEach로 변경

--- group-anagrams/sonjh1217.swift
+++ group-anagrams/sonjh1217.swift
@@ -2,7 +2,7 @@ class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
         var stringsByCount = [[Int]: [String]]()
         
-        strs.map { str in
+        strs.forEach { str in
             var countsByAlphabet = Array(repeating: 0, count: 26)
             for char in str.unicodeScalars {
                 countsByAlphabet[Int(char.value) - 97] += 1
