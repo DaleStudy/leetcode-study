e53c316de7fe251240b92f080676db93e01aae8b
조혜린 <hyer1205@gmail.com>
2025-08-30 07:10:31 +0900

    longest increasing subsequence solution

---
+++ longest-increasing-subsequence/hyer0705.ts
@@ -0,0 +1,14 @@
+function lengthOfLIS(nums: number[]): number {
+  const n = nums.length;
+  const dp: number[] = Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+}
