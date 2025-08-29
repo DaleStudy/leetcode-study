8a415e35cb731ab5f3a357ef0825cee5b75ef731
Jihyun Son <sonjh1217@naver.com>
2025-08-29 09:21:14 +0900

    solve

---
+++ design-add-and-search-words-data-structure/sonjh1217.swift
@@ -0,0 +1,55 @@
+class WordDictionary {
+    class TrieNode {
+        var children: [Character: TrieNode] = [:]
+        var isEndOfWord = false
+    }
+    
+    private var root: TrieNode
+
+    init() {
+        root = TrieNode()
+    }
+    
+    // O(n) time / O(n) space
+    func addWord(_ word: String) {
+        var node = root
+        
+        for character in word {
+            if node.children[character] == nil {
+                node.children[character] = TrieNode()
+            }
+            
+            node = node.children[character]!
+        }
+        
+        node.isEndOfWord = true
+    }
+    
+    // O(m) ~ O(26^m) time / O(1) space
+    func search(_ word: String) -> Bool {
+        return dfs(word: Array(word), index: 0, node: root)
+    }
+    
+    private func dfs(word: [Character], index: Int, node: TrieNode) -> Bool {
+        if index == word.count {
+            return node.isEndOfWord
+        }
+        
+        let character = word[index]
+        
+        if character == "." {
+            for child in node.children.values {
+                if dfs(word: word, index: index + 1, node: child) {
+                    return true
+                }
+            }
+            return false
+        } else {
+            guard let child = node.children[character] else {
+                return false
+            }
+            return dfs(word: word, index: index + 1, node: child)
+        }
+    }
+}
+
