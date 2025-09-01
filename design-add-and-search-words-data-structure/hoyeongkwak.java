599b8ab07c37c41bbf9608ec7ad427c56025ba9a
hoyeongkwak <ghy2002@hanmail.net>
2025-09-01 12:24:06 +0900

    lint modify

--- design-add-and-search-words-data-structure/hoyeongkwak.java
+++ design-add-and-search-words-data-structure/hoyeongkwak.java
@@ -70,12 +70,3 @@ class WordDictionary {
         }
     }
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * boolean param_2 = obj.search(word);
- */
- 
- 
