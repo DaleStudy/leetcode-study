cd53256564798680887ffa09b8d51b8f809fa169
delight010 <seonga.oh00@gmail.com>
2025-09-04 18:41:19 +0900

    solve problem

---
+++ longest-substring-without-repeating-characters/delight010.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        if s.isEmpty {
+            return 0
+        }
+        var maxLength = 0
+        var startIndex = 0
+        var charSet: Set<Character> = []
+        let charArray = Array(s)
+        
+        for right in 0..<charArray.count {
+            while charSet.contains(charArray[right]) {
+                charSet.remove(charArray[startIndex])
+                startIndex += 1
+            }
+            
+            charSet.insert(charArray[right])
+            maxLength = max(maxLength, right - startIndex + 1)
+        }
+        
+        return maxLength
+    }
+}
+ 
