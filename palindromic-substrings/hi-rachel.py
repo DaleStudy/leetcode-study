de43128621dbd84a6db5c81ee48389f6c42db534
hi-rachel <rachel.uiux@gmail.com>
2025-09-08 20:43:41 +0900

    palindromic-substrings sol (py)

--- palindromic-substrings/hi-rachel.py
+++ palindromic-substrings/hi-rachel.py
@@ -55,3 +55,38 @@ class Solution:
                     cnt += 1
 
         return cnt
+
+
+# 25/09/08 복습
+
+"""
+“모든 회문은 중심에서 좌우 대칭이다”
+→ 따라서 모든 중심점에서 좌우로 확장하면서 회문인지 확인하면
+→ 모든 회문 substring을 탐색 가능!
+
+- 중심점의 개수는 총 2n - 1개:
+- 홀수 길이: center = 0 ~ n-1 (expand(i, i))
+- 짝수 길이: center = 0 ~ n-1 (expand(i, i+1))
+
+TC: O(N^2)
+SC: O(1)
+"""
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for center in range(len(s)):
+            print(center)
+            count += self.expand(s, center, center)
+
+            count += self.expand(s, center, center + 1)
+        return count
+
+    def expand(self, s, left, right):
+        count = 0
+        # 범위를 벗어나지 않고, palindrome이면 확장
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        return count
