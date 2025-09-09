8373128992aad2aad8ffd98fb2594cfe608345bd
jinvicky <jinvicky17@gmail.com>
2025-09-09 23:06:38 +0900

    longest common subsequence solution

--- longest-common-subsequence/jinvicky.java
+++ longest-common-subsequence/jinvicky.java
@@ -18,4 +18,23 @@ class Solution {
 
         return dp[m][n];
     }
+
+    // -1을 참조해 이전 케이스를 재사용하는 경우는 1부터 m,n까지 하면 그렇게 할 수 있다.
+    public int longestCommonSubsequence2(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;  // 문자가 같으면 대각선 값 + 1
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);  // 위 or 왼쪽 중 큰 값
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 }
