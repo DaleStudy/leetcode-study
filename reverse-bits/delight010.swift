78c499fe444d00e04c6a0b0a186cdf8d27b442c1
delight010 <seonga.oh00@gmail.com>
2025-09-10 17:20:26 +0900

    solve problem

---
+++ reverse-bits/delight010.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func reverseBits(_ n: Int) -> Int {
+        var number = n
+        var result = 0
+        
+        for i in 0..<32 {
+            let bit = number & 1
+            result = result << 1
+            result = result | bit
+            number = number >> 1
+        }
+        
+        return result
+    }
+}
+ 
