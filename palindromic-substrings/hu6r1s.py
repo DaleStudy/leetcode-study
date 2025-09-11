824d1a095ec7cfbffa170509d46a16f8fba4f2f8
hu6r1s <darmaa333@gmail.com>
2025-09-11 11:28:41 +0900

    feat: Solve palindromic-substrings problem

--- palindromic-substrings/hu6r1s.py
+++ palindromic-substrings/hu6r1s.py
@@ -1,12 +1,13 @@
 class Solution:
   """
-  
+  브루트포스로 모두 탐색하면서 집어넣는 방법
+  O(n^2)
   """
-    def countSubstrings(self, s: str) -> int:
-        cnt = []
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                sub = s[i:j+1]
-                if sub and sub == sub[::-1]:
-                    cnt.append(sub)
-        return len(cnt)
+  def countSubstrings(self, s: str) -> int:
+      cnt = []
+      for i in range(len(s)):
+          for j in range(i, len(s)):
+              sub = s[i:j+1]
+              if sub and sub == sub[::-1]:
+                  cnt.append(sub)
+      return len(cnt)
