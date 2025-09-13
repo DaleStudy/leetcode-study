2bb561c36f2f232961256807fcf6b2528f1e3349
yhkee0404 <yhkee0404@gmail.com>
2025-09-13 21:13:36 +0900

    longest repeating character replacement solution without priority queue

--- longest-repeating-character-replacement/yhkee0404.kt
+++ longest-repeating-character-replacement/yhkee0404.kt
@@ -1,34 +1,26 @@
 class Solution {
     fun characterReplacement(s: String, k: Int): Int {
         val cnts = MutableList<Int>('Z'.code - 'A'.code + 1) {0}
-        val queue = PriorityQueue<List<Int>>() {a, b -> b.first() - a.first()}
-        queue.offer(listOf(0, -1))
+        var max_cnt = 0
         var ans = 0
         var i = 0
         var j = 0
-        while (j != s.length) { // T(n) = S(n) = O(nlogn)
-            while (queue.size > 1) {
-                val u = queue.peek()
-                if (u.first() == cnts[u.last()]) {
-                    break
-                }
-                queue.poll()
-            }
+        while (j != s.length) { // T(n) = S(n) = O(n)
             while (j != s.length) {
-                val diff = j - i - queue.peek().first()
+                val diff = j - i - max_cnt
                 if (diff > k) {
                     break
                 }
                 val d = s[j].code - 'A'.code
-                if (diff == k && queue.peek().first() != cnts[d]) {
+                if (diff == k && max_cnt != cnts[d]) {
                     break
                 }
                 j++
-                queue.offer(listOf(++cnts[d], d)) // O(logn)
+                max_cnt = maxOf(max_cnt, ++cnts[d])
             }
             ans = maxOf(ans, j - i)
             val d = s[i++].code - 'A'.code
-            queue.offer(listOf(--cnts[d], d)) // O(logn)
+            cnts[d]--
         }
         return ans
     }
