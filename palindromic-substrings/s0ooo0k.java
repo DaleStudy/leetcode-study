c035853311e1ccf016f5a6fe7657b6a11de985fd
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-09-14 19:42:47 +0900

    Update palindromic-substrings/s0ooo0k.java
    Co-authored-by: yhkee0404 <yhkee0404@gmail.com>

--- palindromic-substrings/s0ooo0k.java
+++ palindromic-substrings/s0ooo0k.java
@@ -4,12 +4,12 @@ class Solution {
         int cnt = 0;
 
         for(int i=0; i<s.length(); i++) {
-            cnt += palindrom(s, i, i);
-            cnt += palindrom(s, i, i+1);
+            cnt += palindrome(s, i, i);
+            cnt += palindrome(s, i, i+1);
         }
         return cnt;
     }
-    public int palindrom(String s, int left, int right) {
+    public static int palindrome(final String s, int left, int right) {
         int cnt = 0;
         while(left>=0 && right < s.length() && s.charAt(left)==s.charAt(right)) {
             cnt++;
