0b49d706613200f0b2b40095b315a270a0f87df2
yhkee0404 <yhkee0404@gmail.com>
2025-09-20 17:25:04 +0900

    linked list cycle solution with comment

--- linked-list-cycle/yhkee0404.go
+++ linked-list-cycle/yhkee0404.go
@@ -8,12 +8,12 @@
 func hasCycle(head *ListNode) bool {
     u := head
     v := head
-    for u != nil && v != nil {
+    for u != nil && v != nil { // T(n) = O(n), S(n) = O(1)
         u = u.Next
         if (u == nil) {
             break
         }
-        u = u.Next
+        u = u.Next // https://hyp.is/gLsBIOHBEe6E_9M9T5MJNw/yhkee0404.github.io/posts/algorithms/leetcode/linked-list-cycle/
         v = v.Next
         if u == v {
             return true
