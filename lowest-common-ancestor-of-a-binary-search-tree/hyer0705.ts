96c50c3f9abc8574c3c3704e1a5a24d1ac65d3da
조혜린 <hyer1205@gmail.com>
2025-10-14 23:55:28 +0900

    lowest common ancestor of a binary search tree solution

---
+++ lowest-common-ancestor-of-a-binary-search-tree/hyer0705.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ *
+ * Time Complexity: O(h) where h is the height of the tree
+ * Space Complexity: O(1)
+ */
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+  if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root;
+}
