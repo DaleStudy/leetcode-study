53d44a664a968eeeecbcda57fcc410a23ef876a9
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2025-11-05 21:28:37 -0500

    hongseoupyun_two_sum_solution

---
+++ two-sum/hongseoupyun.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:      
+                return [seen[complement], i]
+            seen[num] = i               
+        return []
