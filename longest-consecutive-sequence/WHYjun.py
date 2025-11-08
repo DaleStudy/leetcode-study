55a53cc36b8b36012b19b1f2d0d7b2fc751cf16a
Young Choi <WHYjun@users.noreply.github.com>
2025-11-08 14:44:46 -0800

    [WHYjun] WEEK 01 solutions - (4/5)

---
+++ longest-consecutive-sequence/WHYjun.py
@@ -0,0 +1,47 @@
+import heapq
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # remove duplicates by using set
+        numsSet = set(nums)
+        
+        if len(numsSet) == 0 or len(numsSet) == 1:
+            return len(numsSet)
+
+        # Use priority queue to sort in O(n)
+        sorted = []
+        for num in numsSet:
+            heapq.heappush(sorted, num)
+        
+        prev = heapq.heappop(sorted)
+        answer, count = 1, 1
+
+        for i in range(len(numsSet) - 1):
+            popped = heapq.heappop(sorted)
+            if prev + 1 == popped:
+                count += 1
+            else:
+                count = 1
+            
+            prev = popped
+            answer = max(answer, count)
+    
+        return answer
+
+    def longestConsecutiveUsingSetOnly(self, nums: List[int]) -> int:
+        # remove duplicates by using set
+        numsSet = set(nums)
+
+        answer = 0
+
+        for num in numsSet:
+            # continue if it's not the longest consecutive
+            if num - 1 in numsSet:
+                continue
+            else:
+                count = 1
+                while num + count in numsSet:
+                    count += 1
+                answer = max(answer, count)
+
+        return answer
