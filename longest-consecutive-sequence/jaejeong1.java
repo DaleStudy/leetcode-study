bfd0d2c8cef8b3be859fc7463dae13ebca519fc5
장재정 <jangjaejeong@baejieun-ui-MacBookPro.local>
2025-11-09 23:04:00 +0900

    Longest Consecutive Sequence

--- longest-consecutive-sequence/jaejeong1.java
+++ longest-consecutive-sequence/jaejeong1.java
@@ -4,43 +4,33 @@ import java.util.Set;
 class SolutionLongestConsecutiveSequence {
 
   public int longestConsecutive(int[] nums) {
-    // 정렬되지 않은 정수 nums 배열이 주어지면 가장 긴 연속 요소 시퀀스 길이를 반환
-    // O(N) 시간 내 실행되야함
-    // 전부 해시맵에 때려넣고, 키를 꺼내 연속 요소가 있는지 확인한다
-    // 연속 요소가 있으면 answer를 1 증가시키고, 연속 요소는 제거한다
-    // 시간복잡도: O(N), 공간복잡도: O(N)
-
-    Set<Integer> set = new HashSet<>();
-    for (var num : nums) {
-      set.add(num);
-    }
-    var answer = 0;
-    for (var num : nums) {
-      var length = 1;
-
-      if (set.contains(num-1)) {
-        set.remove(num);
-        var minusKey = num;
-        while (set.contains(--minusKey)) {
-          length++;
-          set.remove(minusKey);
-        }
+      // 시퀀스 조회를 O(1)에 수행 가능하고, 중복은 무시할 수 있는 조건이므로 Set이 적합한 자료구조
+      // 시간복잡도: O(N), 공간복잡도: O(N)
+      Set<Integer> num_set = new HashSet<Integer>();
+      for (int num : nums) {
+          num_set.add(num);
       }
 
-      if (set.contains(num+1)) {
-        set.remove(num);
-        var plusKey = num;
-        while (set.contains(++plusKey)) {
-          length++;
-          set.remove(plusKey);
-        }
+      int longestSequence = 0;
+
+      // 시간복잡도: O(N)
+      for (int num : num_set) {
+          // 시퀀스 중간에 있는 숫자가 아닌 시작하는 숫자를 찾음
+          // 시작하는 숫자는 - 1 값이 Set에 없을 것
+          if (!num_set.contains(num - 1)) {
+              int currentNum = num;
+              int currentLength = 1;
+              // + 1 값이 Set에 있는 지 확인하면서 증가
+              while (num_set.contains(currentNum + 1)) {
+                  currentNum += 1;
+                  currentLength += 1;
+              }
+
+              // 순회가 끝나면 최대 시퀀스 길이인지 확인하고 적용
+              longestSequence = Math.max(longestSequence, currentLength);
+          }
       }
 
-      if (length > answer) {
-        answer = length;
-      }
-    }
-
-    return answer;
+      return longestSequence;
   }
 }
---
+++ longest-consecutive-sequence/test.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // 시퀀스 조회를 O(1)에 수행 가능하고, 중복은 무시할 수 있는 조건이므로 Set이 적합한 자료구조
+        // 시간복잡도: O(N), 공간복잡도: O(N)
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestSequence = 0;
+
+        // 시간복잡도: O(N)
+        for (int num : num_set) {
+            // 시퀀스 중간에 있는 숫자가 아닌 시작하는 숫자를 찾음
+            // 시작하는 숫자는 - 1 값이 Set에 없을 것
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLength = 1;
+                // + 1 값이 Set에 있는 지 확인하면서 증가
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentLength += 1;
+                }
+
+                // 순회가 끝나면 최대 시퀀스 길이인지 확인하고 적용
+                longestSequence = Math.max(longestSequence, currentLength);
+            }
+        }
+
+        return longestSequence;
+    }
+}
