3feb45545a86e8502cd00c24d467a2351d6dc477
hyeok <cod3holic@gmail.com>
2025-11-10 02:36:13 +0900

    contains duplicated solution

---
+++ contains-duplicate/dylan-jung.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        bool containsDuplicate(vector<int>& nums) {
+            unordered_set<int> s;
+            for(auto item: nums) {
+                if(s.count(item) > 0) return true;
+                s.insert(item);
+            }
+            return false;
+        }
+    };
+    
