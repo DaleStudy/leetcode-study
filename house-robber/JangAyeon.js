5304e3976ad7156e700d6acb7f1d74103ba37eb5
JangAyeon <ayeon3052@ewhain.net>
2025-11-11 07:21:50 +0900

    house robber solution DP

--- house-robber/JangAyeon.js
+++ house-robber/JangAyeon.js
@@ -5,19 +5,11 @@
 var rob = function (nums) {
   const N = nums.length;
   let answer = 0;
-  function dfs(lastVisited, curr, t) {
-    if (curr == N) {
-      console.log(t);
-      answer = Math.max(answer, t);
-      return;
-    }
-    for (let idx = curr; idx < N; idx++) {
-      if (idx != lastVisited + 1) {
-        dfs(idx, idx + 1, t + nums[idx]);
-      }
-      dfs(lastVisited, idx + 1, t);
-    }
+  let [rob1, rob2] = [0, 0];
+  for (let num of nums) {
+    let temp = Math.max(num + rob1, rob2);
+    rob1 = rob2;
+    rob2 = temp;
   }
-  dfs(-2, 0, 0);
-  return answer;
+  return rob2;
 };
