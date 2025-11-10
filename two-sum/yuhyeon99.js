3a595a9dce46c1e2cefd6d396799846f55750275
yuhyeon99 <jujini31@gmail.com>
2025-11-10 23:52:45 +0900

    two sum solution

--- two-sum/yuhyeon99.js
+++ two-sum/yuhyeon99.js
@@ -14,7 +14,7 @@ var twoSum = function(nums, target) {
         if(!numsMap.has(num)) numsMap.set(num, []);
         numsMap.get(num).push(i);
     });
-    
+
     for(let i = 0; i < nums.length; i ++) {
         for(let j = i + 1; j < nums.length; j ++) {
             if(nums[i] + nums[j] === target) {
