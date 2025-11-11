d606f2649202095fe3919e1cdc99621d88aa5ac6
조성민(Sam Cho) <samcho9968@gmail.com>
2025-11-12 08:59:34 +0900

    add improved solution for top k frequent elements

--- top-k-frequent-elements/samcho0608.java
+++ top-k-frequent-elements/samcho0608.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
-class Solution {
+// Time complexity: O(Nlogk)
+// Space complexity: O(N)
+class Solution1 {
     // return: top k most freq elements
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> freq = new HashMap<>();
@@ -22,6 +24,42 @@ class Solution {
         int[] result = new int[k];
         for(int i = 0; i < k; i++) {
             result[i] = heap.poll()[0]; // O(log k)
+        }
+
+        return result;
+    }
+}
+
+// Time complexity: O(N)
+// Space complexity: O(N)
+class Solution2 {
+    public int[] topKFrequent(int[] nums, int k) {
+        // count frequencies
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // bucket: index = frequency, value = list of numbers
+        List<List<Integer>> buckets = new ArrayList<>(nums.length + 1);
+        for (int i = 0; i <= nums.length; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (var entry : freq.entrySet()) {
+            int num = entry.getKey();
+            int count = entry.getValue();
+            buckets.get(count).add(num);
+        }
+
+        // gather top k frequent elements
+        int[] result = new int[k];
+        int idx = 0;
+        for (int i = nums.length; i >= 0 && idx < k; i--) {
+            for (int num : buckets.get(i)) {
+                result[idx++] = num;
+                if (idx == k) break;
+            }
         }
 
         return result;
