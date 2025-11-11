41ca9be082e71dfa5f12ebe242a833df8b638434
daiyongg-kim <134879427+daiyongg-kim@users.noreply.github.com>
2025-11-11 09:20:57 -0800

    adding house robber

---
+++ house-robber/daiyongg-kim.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob_prev_prev = 0
+        rob_prev = 0
+        max_today = 0
+        for rob_current in nums:
+            contains_rob_current = rob_current + rob_prev_prev
+            not_contains_rob_current = rob_prev
+
+            max_today = max(contains_rob_current, not_contains_rob_current)
+
+            rob_prev_prev = rob_prev
+            rob_prev = max_today
+        
+        return max_today
+    
