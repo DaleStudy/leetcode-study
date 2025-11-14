b064d08fb8d717c46f641d9ad8977aec61067e99
HYUNAHKO <hy.na311@gmail.com>
2025-11-15 01:33:55 +0900

    contains-duplicate solution

---
+++ contains-duplicate/HYUNAHKO.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) >= 1 and len(nums) <= 100000:
+            for i in range(0, len(nums)):
+                src = nums[i]
+                for j in range(i+1, len(nums)):
+                    if src == nums[j]:
+                        return True
+            return False
+        return False
