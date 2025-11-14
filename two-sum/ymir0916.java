5d7cff75bba5249d8207304e53876442fa6ac99a
ymir0804 <duswn916@kakao.com>
2025-11-15 01:39:31 +0900

    "op K Frequent Elements 문제"

--- two-sum/ymir0916.java
+++ two-sum/ymir0916.java
@@ -3,6 +3,8 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // 시간 복잡도를 생각해서 HashMap을 사용
+
         Map<Integer, Integer> idx = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int c = target - nums[i];
