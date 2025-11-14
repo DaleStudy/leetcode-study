6cbf6b0dd8763bdb3258175402d7dff274a189f5
Seongyul Bae <devyulbae@gmail.com>
2025-11-14 19:03:17 +0900

    4 solved

---
+++ longest-consecutive-sequence/devyulbae.py
@@ -0,0 +1,11 @@
+"""
+Blind75 - Longest Consecutive Sequence
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+조건 : O(n) 시간복잡도
+"""
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
