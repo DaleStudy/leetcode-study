6dc68e4359347754cba898e3f3540617e5880823
hyh <dihexyldecyl@gmail.com>
2025-11-14 22:21:35 +0900

    two sum

---
+++ two-sum/deepInTheWoodz.py
@@ -0,0 +1,10 @@
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    passed = dict()
+    
+    for i, num in enumerate(nums):
+      other = target - num
+      if other in passed:
+        return [passed[other], i]
+      passed[num] = i
+      
