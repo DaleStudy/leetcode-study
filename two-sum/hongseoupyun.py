a196c1109260dbf1b90e84c2333271edf5422306
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2025-11-13 20:23:18 -0500

    new commit

--- two-sum/hongseoupyun.py
+++ two-sum/hongseoupyun.py
@@ -1,3 +1,9 @@
+# Create a empty hash table(seen) and store numbers as a key and their indices as a value
+# Iterate through the given array(nums) 
+# For each numbers in given array, calculate complement (target - num)
+# If complement is in hash table(seen), return array of index of the currnet number and index of complement in hash table(seen)
+# If complement is not in has table(seen), store the current number in hash table(seen) and continue checking the numbers
+#
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
---
+++ two-sum/hongseoupyun.ts
@@ -0,0 +1,18 @@
+// # Create a empty hash table(seen) and store numbers as a key and their indices as a value
+// # Iterate through the given array(nums) 
+// # For each numbers in given array, calculate complement (target - num)
+// # If complement is in hash table(seen), return array of index of the currnet number and index of complement in hash table(seen)
+// # If complement is not in has table(seen), store the current number in hash table(seen) and continue checking the numbers
+//
+
+function twoSum2(nums: number[], target: number): number[] {
+    const seen:{[key:number] : number} = {};
+    for (let i = 0; i<nums.length; i++){
+      let complement = target - nums[i]
+      if (complement in seen) {
+        return [seen[complement],i]
+      }
+      seen[nums[i]] = i
+    }
+    return []
+};
