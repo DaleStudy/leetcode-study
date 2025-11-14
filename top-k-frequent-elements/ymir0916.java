814fa1ec95a839737af3f72ffaf526900955c337
ymir0804 <duswn916@kakao.com>
2025-11-15 01:42:50 +0900

    "top K Frequent Elements 문제"

---
+++ top-k-frequent-elements/ymir0916.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Set<Integer> unique = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        Map<Integer, Integer> value = new HashMap<>();
+        for (int n : unique) {
+            int cnt = 0;
+            for (int m : nums) {
+                if (n == m) {
+                    cnt++;
+                }
+            }
+            value.put(n, cnt);
+        }
+        List<Map.Entry<Integer, Integer>> sortedByValueDesc = value.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .toList();
+        return sortedByValueDesc.stream()
+                .limit(k)
+                .map(Map.Entry::getKey)
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
