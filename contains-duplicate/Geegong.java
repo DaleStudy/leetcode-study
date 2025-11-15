37fa2b20f4234db06273db146c4b0e0a1f644343
JIA <shchj66@gmail.com>
2025-11-15 11:22:32 +0900

    - 1주차 풀이 커밋

--- contains-duplicate/Geegong.java
+++ contains-duplicate/Geegong.java
@@ -12,14 +12,23 @@ public class Geegong {
      * @return
      */
     public boolean containsDuplicate(int[] nums) {
+
+
+
         HashSet<Integer> uniques = new HashSet<>();
 
         for (int num : nums) {
-            if (uniques.contains(num)) {
+
+            // 명확하게 hashSet에 값이 있는지 체크하는 메소드로 확인이 가능하지만
+//            if (uniques.contains(num)) {
+//                return true;
+//            }
+//            uniques.add(num);
+
+            // hashSet 의 Add 는 이미 값이 있다면 FALSE를 리턴하기에 아래처럼도 동작 가능 (더 빠른 결과확인)
+            if (!uniques.add(num)) {
                 return true;
             }
-
-            uniques.add(num);
         }
 
         return false;
