ebd205a030bb3817a2ed06ec730e8448f335bff5
sangyoon <lsyo6171@gmail.com>
2025-11-17 21:57:37 +0900

    Week 02: Valid anagram solution

---
+++ valid-anagram/mandel-17.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def count_char(target_str):
+            dict_char = {}
+            for c in target_str:
+              dict_char[c] = dict_char.get(c, 0) + 1
+            return dict_char
+
+        dict_s = count_char(s)          
+        dict_t = count_char(t)
+
+        if dict_s == dict_t:
+            return True
+        return False
+    
