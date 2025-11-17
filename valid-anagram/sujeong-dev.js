bcda32e748db7d973fdf924c511657189ce4625a
구수정 <9sujeong.dev@gmail.com>
2025-11-17 16:26:25 +0900

    feat: solve valid-anagram

---
+++ valid-anagram/sujeong-dev.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  const frequencyMap = new Map();
+
+  for (const x of s) {
+    frequencyMap.set(x, (frequencyMap.get(x) || 0) + 1);
+  }
+
+  for (const y of t) {
+    if (!frequencyMap.has(y)) return false;
+    frequencyMap.set(y, frequencyMap.get(y) - 1);
+    if (frequencyMap.get(y) === 0) frequencyMap.delete(y);
+  }
+
+  return frequencyMap.size === 0;
+};
