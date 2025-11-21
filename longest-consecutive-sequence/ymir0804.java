4e5b3625a6e02bfbdde71bd37e520db48698e062
ymir0804 <duswn916@kakao.com>
2025-11-21 23:43:38 +0900

    fix: posix lint 추가 및 house-robber 문제

--- longest-consecutive-sequence/ymir0804.java
+++ longest-consecutive-sequence/ymir0804.java
@@ -1,32 +1,32 @@
 import java.util.HashSet;
 
-class ymir0804 {
+class Solution  {
     public int longestConsecutive(int[] nums) {
         HashSet<Integer> numsSet = new HashSet<>();
         int maxNum = 0;
-        for (int num: nums) {
+        for (int num : nums) {
             numsSet.add(num);
         }
 
-        if(numsSet.isEmpty()) {
+        if (numsSet.isEmpty()) {
             return 1;
         } else if (numsSet.size() == 1) {
             return 0;
         }
 
-        for (int num: numsSet) {
+        for (int num : numsSet) {
             boolean isStartPoint = !numsSet.contains(num - 1);
             if (isStartPoint) {
                 int current = num;
                 int length = 1;
-                while(numsSet.contains(++current)) {
+                while (numsSet.contains(++current)) {
                     length++;
                 }
-                if(length > maxNum) {
+                if (length > maxNum) {
                     maxNum = length;
                 }
             }
         }
         return maxNum;
-        }
     }
+}
