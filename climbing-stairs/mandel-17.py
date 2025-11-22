0a8e5bafb76855c6f88ce999965d79d09c4aacb6
sangyoon <lsyo6171@gmail.com>
2025-11-22 19:12:26 +0900

    Week 2: Chage the code

--- climbing-stairs/mandel-17.py
+++ climbing-stairs/mandel-17.py
@@ -1,22 +1,14 @@
+import collections
+
 class Solution:
+    num_dict = collections.defaultdict(int)
+
     def climbStairs(self, n: int) -> int:
-        def factorial(n):
-            result = 1
-            for i in range(1, n+1):
-                result *= i
-            return result
+        if n <= 2:
+            return n
 
-        def combination(n, r):
-            top = factorial(n)
-            bottom = factorial(n-r) * factorial(r)
-            return top / bottom
-
-        share = n // 2
-        total = 1
-        for i in range(1, share + 1):
-            combination_n = n - i
-            combination_r = i
-
-            total += combination(combination_n, combination_r)
-
-        return int(total)
+        if self.num_dict[n]:
+            return self.num_dict[n]
+        
+        self.num_dict[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.num_dict[n]
