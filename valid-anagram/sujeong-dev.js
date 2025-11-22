4953d89d6b268a83b7263004d1dbf78444d8f4d8
구수정 <9sujeong.dev@gmail.com>
2025-11-22 14:41:00 +0900

    feat: easy 복잡도 계산 주석 추가

--- valid-anagram/sujeong-dev.js
+++ valid-anagram/sujeong-dev.js
@@ -2,10 +2,18 @@
  * @param {string} s
  * @param {string} t
  * @return {boolean}
+ * 
+ * 시간복잡도 계산
+ * 문자열 s 길이만큼 Map에 set => n
+ * 문자열 t 길이만큼 Map에 set => n
+ * 따라서 O(n)
+ * 
+ * 공간복잡도 계산
+ * 문자열 s, t의 길이만큼 Map에 할당되니까 O(n)
  */
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
-  
+
   const frequencyMap = new Map();
 
   for (const x of s) {
