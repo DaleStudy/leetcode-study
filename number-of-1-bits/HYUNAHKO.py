38d4c3cc771f14c8a73f85cafe301a09cbe39071
HYUNAHKO <hy.na311@gmail.com>
2025-11-27 23:16:30 +0900

    Week 03 soultions

---
+++ number-of-1-bits/HYUNAHKO.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result = 1
+        while (n//2 != 0):
+            remainder = n % 2
+            if (remainder==1):
+                result+=1
+            n = n//2
+        return result
+    
--- product-of-array-except-self/HYUNAHKO.py
+++ product-of-array-except-self/HYUNAHKO.py
@@ -38,3 +38,4 @@ class Solution:
 
         return result_list
         
+        
