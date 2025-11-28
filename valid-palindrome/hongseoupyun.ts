6944cfd09a3a13f0842ecd68d65251b62fd1eaac
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2025-11-27 21:38:01 -0500

    feat: add two-pointer approach for isPalindrome function with detailed comments on complexity

--- valid-palindrome/hongseoupyun.ts
+++ valid-palindrome/hongseoupyun.ts
@@ -15,3 +15,42 @@ function isPalindrome(s: string): boolean {
         return false
     }
 };
+
+//Palindrom example: "A man, a plan, a canal: Panama"
+
+// Using Two-pointer; It searches from given string directly so the Space complexity is O(n) - better method
+// By using two index, searchs and compares from start and end of the string like this-><- 
+// Do this while start index is less than end index
+// Skip non-alphanumeric characters
+// If both pointer are in characters, then convert to lowercase and compare
+// If not equal, return false
+// If all characters are equal, return true
+
+// Time complexity = O(n), as it checks and converts to lowercase all characters in the string
+// Space complexity = O(1), as it uses only a constant amount of extra space
+function isPalindrome2(s: string): boolean {
+
+    let left: number = 0;
+    let right: number = s.length - 1;
+
+    let isAlphaNum = (str: string): boolean => {
+        return /^[a-zA-Z0-9]$/.test(str)
+    }
+
+    while (left < right) {
+        if (!isAlphaNum(s[left])) {
+            left++
+            continue
+        }
+        if (!isAlphaNum(s[right])) {
+            right--
+            continue
+        }
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false
+        }
+        left++
+        right--
+    }
+     return true
+};
