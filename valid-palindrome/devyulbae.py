43213a1b0c47f7b9a0e3378a857b49799527d288
Seongyul Bae <devyulbae@gmail.com>
2025-11-29 14:53:47 +0900

    debug: add indent

--- valid-palindrome/devyulbae.py
+++ valid-palindrome/devyulbae.py
@@ -20,3 +20,4 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         filtered_s = ''.join(char.lower() for char in s if char.isalnum())
         return filtered_s == filtered_s[::-1]
+    
