d459373a0b80b79f0da8a861782669d4663e79d4
Seongyul Bae <devyulbae@gmail.com>
2025-11-29 15:17:51 +0900

    feat: solve 3

--- number-of-1-bits/devyulbae.py
+++ number-of-1-bits/devyulbae.py
@@ -1,4 +1,17 @@
+"""
+Blind75 - Number of 1 Bits
+https://leetcode.com/problems/number-of-1-bits/
+시간복잡도 : O(log n)
+공간복잡도 : O(1)
+
+풀이 :
+    파이썬의 내장함수 bin() -> O(log n)
+    문자열 메서드 count() -> O(log n)
+
+"""
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        
+        return str(bin(n)).count('1')
+    
 
