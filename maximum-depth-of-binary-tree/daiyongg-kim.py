5cb3d981b74d5755e0cca20426288c0985cca823
daiyongg-kim <134879427+daiyongg-kim@users.noreply.github.com>
2025-12-02 09:33:30 -0800

    update the maxdepth using pq

--- maximum-depth-of-binary-tree/daiyongg-kim.py
+++ maximum-depth-of-binary-tree/daiyongg-kim.py
@@ -1,11 +1,28 @@
 class Solution:
 
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
         if not root:
             return 0
-        
-        left_depth = self.maxDepth(root.left)
-        right_depth = self.maxDepth(root.right)
+        queue = deque([root])
+        print(queue)
+        depth = 0
 
-        return max(left_depth, right_depth) + 1
+        while queue:
+            depth += 1
+            print(len(queue))
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth
+
+
+#        if not root:
+#            return 0
+        
+#        left_depth = self.maxDepth(root.left)
+#        right_depth = self.maxDepth(root.right)
+
+#        return max(left_depth, right_depth) + 1
