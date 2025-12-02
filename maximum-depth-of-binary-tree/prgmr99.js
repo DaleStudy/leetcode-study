87f6b5ad04c9cad8801b52af37ef0297c9fd52c7
prgmr99 <johnyeom24@naver.com>
2025-12-02 17:40:40 +0900

    maximum-depth-of-binary-tree solution

---
+++ maximum-depth-of-binary-tree/prgmr99.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+};
