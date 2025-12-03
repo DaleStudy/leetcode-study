49e7491fcf5d3a669656f7c97c1f6172ef2cc6fb
changhyumm <kch9132@gmail.com>
2025-12-04 00:26:22 +0900

    주석

--- maximum-depth-of-binary-tree/changhyumm.py
+++ maximum-depth-of-binary-tree/changhyumm.py
@@ -6,8 +6,11 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # root가 없는경우 depth 0
         if root is None:
             return 0
+        # depth를 같이 포함시킴
+        # dfs의 경우 각 경로별로 끝까지 탐색하는데, 각 경로마다 depth가 다르므로 표기하면서 stack에 추가해야함
         stack = [[root, 1]]
         max_depth = 1
         while stack:
