7c255309a14d3ccdae541513e0997f061ff256d0
8804 <8804who@naver.com>
2025-12-04 21:41:34 +0900

    Week 5

---
+++ best-time-to-buy-and-sell-stock/8804who.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        answer = 0
+
+        min_price = prices[0]
+        for price in prices:
+            if min_price>price:
+                min_price=price
+            
+            if answer<price-min_price:
+                answer=price-min_price
+        return answer
