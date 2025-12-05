744b5ac4312f5c0cacaefaa4c1b12ba0716dd23d
ymir0804 <duswn916@kakao.com>
2025-12-05 10:53:22 +0900

    Product of Array Except Self 문제

---
+++ product-of-array-except-self/ymir0804.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int [] result = new int [nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int suffixProduct = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+        return result;
+    }
+}
