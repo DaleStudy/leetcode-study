5f7a92b2aeb829e64921abf226ed34a74c2c3c46
junyoungKim <wnsdud1900427@gmail.com>
2025-12-06 18:00:26 +0900

    Maximum Depth of Binary Tree solution

--- maximum-depth-of-binary-tree/kimjunyoung90.java
+++ maximum-depth-of-binary-tree/kimjunyoung90.java
@@ -4,22 +4,3 @@ public class Solution {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
