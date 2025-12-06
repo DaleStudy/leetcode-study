7fe3df4554339ab5b2bbe522cfdd887e6a0250f5
kim <se6816@naver.com>
2025-12-07 00:06:58 +0900

    find minumum in rotated sorted array(v2)

--- find-minimum-in-rotated-sorted-array/se6816.java
+++ find-minimum-in-rotated-sorted-array/se6816.java
@@ -13,3 +13,30 @@ class Solution {
         return result;
     }
 }
+
+/**
+    이분 탐색을 통해, 최소 요소를 구하는 방식
+     nums의 길이 -> N
+     시간 복잡도 : O(logN)
+     공간 복잡도 : O(1)
+*/
+class Solution {
+    public int findMin(int[] nums) {
+        return nums[binarySearch(nums)];
+    }
+
+    public int binarySearch(int[] nums) {
+        int start = 0;
+        int end = nums.length -1;
+        while(start < end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
+
