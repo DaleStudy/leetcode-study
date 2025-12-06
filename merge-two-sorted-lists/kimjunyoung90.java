b7ad44ac6993847932ec1e8254efe5584359d15b
junyoungKim <wnsdud1900427@gmail.com>
2025-12-06 18:00:45 +0900

    Maximum Depth of Binary Tree solution

--- merge-two-sorted-lists/kimjunyoung90.java
+++ merge-two-sorted-lists/kimjunyoung90.java
@@ -15,11 +15,3 @@ public class Solution {
         }
     }
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
