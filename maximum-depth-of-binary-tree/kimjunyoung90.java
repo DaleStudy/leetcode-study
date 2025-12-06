d757f871bc8e2bdecfa0814a99bb512aacff6365
Junyoung Kim <37926916+kimjunyoung90@users.noreply.github.com>
2025-12-06 18:08:45 +0900

    Revert "[kimjunyoung90] WEEK 04 solutions"

--- maximum-depth-of-binary-tree/kimjunyoung90.java
+++
@@ -1,6 +0,0 @@
-public class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    }
-}
