758a3547bd212e14860812fc20c34376ea675fea
daiyongg-kim <134879427+daiyongg-kim@users.noreply.github.com>
2025-12-08 08:13:51 -0800

    update best time to buy and sell stock

---
+++ best-time-to-buy-and-sell-stock/daiyongg-kim.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices[1:]:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+            
+        return max_profit
