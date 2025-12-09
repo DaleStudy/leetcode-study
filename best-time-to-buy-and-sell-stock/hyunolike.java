d2f40e984b9c9dce10c9caea1382b94bc3e85196
장현호 Hyunho Jang <fallear1@gmail.com>
2025-12-09 14:04:55 +0000

    best time to buy and sell stock solution

---
+++ best-time-to-buy-and-sell-stock/hyunolike.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 주식 최대 이익
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
