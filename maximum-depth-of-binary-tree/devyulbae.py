3ad6f7701c96d99c289364befca52c96eb9b8f08
Seongyul Bae <devyulbae@gmail.com>
2025-12-13 16:50:04 +0900

    [week 4]solved 2

--- maximum-depth-of-binary-tree/devyulbae.py
+++ maximum-depth-of-binary-tree/devyulbae.py
@@ -1,7 +1,24 @@
 """
 Blind 75 - Maximum Depth of Binary Tree
 LeetCode Problem Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
-
-
+시간복잡도 : O(n)
+공간복잡도 : O(n)
+풀이 : 재귀를 이용한 깊이 우선 탐색(DFS)
+자식 노드부터 부모 노드로 거슬러 올라가며 최대 깊이를 계산합니다.
+종료 조건 - left와 right가 모두 None인 경우 (1+max(0,0))을 반환하며 재귀가 종료됩니다.
 """
 
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
--- merge-two-sorted-lists/devyulbae.py
+++ merge-two-sorted-lists/devyulbae.py
@@ -1,7 +1,35 @@
 """
 Blind 75 - Merge Two Sorted Lists
 LeetCode Problem Link: https://leetcode.com/problems/merge-two-sorted-lists/
-
-
+시간복잡도 : O(n+m)
+공간복잡도 : O(1)
+풀이 : (알고달레 답안)
+각 링크드 리스트에 포인터를 두고, 더 작은 값을 가진 노드를 결과 리스트에 추가하다가 한 쪽 리스트가 끝나면 나머지 리스트를 결과 리스트에 추가한다.
+마지막에 첫번째 노드를 반환한다.
 """
+from typing import Optional
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+            
+        tail.next = list1 or list2
+        return dummy.next
+    
