4442084045223dfcae74034bddac88bb01139dd8
YOUNGJIN NA <120540450+ppxyn1@users.noreply.github.com>
2025-12-20 19:03:16 +0900

    [:solved] 3 problems

---
+++ longest-increasing-subsequence/ppxyn1.py
@@ -0,0 +1,13 @@
+# idea: DP 
+# O(N^2)
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1+LIS[j])
+        return max(LIS)
+
+
+
