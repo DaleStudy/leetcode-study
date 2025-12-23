e8f91935ea58c1320891771a2bed850d275156ca
JuhaIm <juha.im@postgrad.manchester.ac.uk>
2025-12-23 23:26:43 +0000

    week 7

---
+++ reverse-linked-list/imgolden77.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        
+        while curr is not None: # once was "while curr.next is not None:" but that showed AttributeError: 'NoneType' object has no attribute 'next'
+            if prev:
+                post = curr.next
+                curr.next = prev
+                prev = curr
+                curr = post
+            else:
+                post = curr.next
+                prev = curr
+                curr.next = None
+                curr = post
+
+        return prev
+    
+# First time, return Curr, but the result was [] so I changed to "return prev" as the new head of the reversed Linked list.
