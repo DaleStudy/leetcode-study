5f65c66e079f27b721cc989acf5d59234b256978
ymir0804 <duswn916@kakao.com>
2025-12-28 16:51:39 +0900

    Number of 1 Bits 문제 풀이

---
+++ number-of-1-bits/ymir0804.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class Solution {
+    public int hammingWeight(int n) {
+        int result = 1;
+        if(n <= 2) {
+            return result;
+        }
+        while (true) {
+            if (n % 2 == 1) {
+                result++;
+            }
+            n /= 2;
+            if(n <= 2) {
+                break;
+            }
+        }
+        return result;
+    }
+}
