fed9e23d1a5365137a47ddd7ec70ae547041b85c
8804 <8804who@naver.com>
2026-01-13 21:52:33 +0900

    Week 10

---
+++ search-in-rotated-sorted-array/8804who.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+        while start <= end:
+            mid = start+(end-start)//2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid-1
+                else:
+                    start = mid+1
+            else:
+                if nums[mid] < target <= nums[end]:
+                    start = mid+1
+                else:
+                    end = mid-1
+        return -1
+    
