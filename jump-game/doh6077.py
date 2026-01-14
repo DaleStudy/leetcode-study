f282432dac02134a16148cf8175883d6ed97096d
doh6077 <kimdohee@sheridancollege.ca>
2026-01-14 16:35:35 -0500

    Add another solution

--- jump-game/doh6077.py
+++ jump-game/doh6077.py
@@ -34,3 +34,20 @@ class Solution:
             if i + max_jump >= target:
                 target = i 
         return target == 0
+
+
+# Greedy 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        
+        maxValue = 0 
+        reach = len(nums) - 1
+        for i in range(0, len(nums)):
+            maxValue = max(nums[i] +  i, maxValue)
+            if maxValue >= reach: return True
+            if maxValue == i and nums[i] == 0:
+                return False
+            
+
+        return False
