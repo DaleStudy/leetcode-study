9ea4c6c5a4118bd7a9955c9d730da3e7fe6acbaa
doh6077 <kimdohee@sheridancollege.ca>
2026-01-23 12:06:10 -0500

    Add comments

--- binary-tree-maximum-path-sum/doh6077.py
+++ binary-tree-maximum-path-sum/doh6077.py
@@ -36,11 +36,13 @@ class Solution:
                 return 0 
             leftMax = dfs(root.left)
             rightMax = dfs(root.right)
+            # 자식 값이 음수일경우 0으로 처리 
             leftMax = max(leftMax, 0)
             rightMax = max(rightMax, 0)
-            
+            # "지금까지 발견한 경로 중 최고의 합"을 res[0]에 저장
+            # Job 1: 나를 꺾임점(Anchor)으로 하는 '아치형' 경로 계산
             res[0] = max(res[0], root.val + leftMax + rightMax)
-
+            # Job 2: 부모에게 올릴 '직선' 경로 리턴
             return root.val + max(leftMax, rightMax)
         dfs(root)
         return res[0]
