50c7acb11c2c1fd4ca6d3881f68b4b27d50cf291
yeonjukim164 <duswn916@kakao.com>
2026-01-26 06:56:40 +0900

    fix: CRLF -> LF 로 변경

--- merge-two-sorted-lists/yeonjukim164.java
+++ merge-two-sorted-lists/yeonjukim164.java
@@ -1,1 +1,25 @@
-class Solution {    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {        ListNode dummy = new ListNode(-1);        ListNode curr = dummy;        while (list1 != null && list2 != null) {            if (list1.val <= list2.val) {                curr.next = list1;                list1 = list1.next;            } else {                curr.next = list2;                list2 = list2.next;            }            curr = curr.next;        }        if (list1 != null) {            curr.next = list1;        } else if (list2 != null) {            curr.next = list2;        }        return dummy.next;    }}
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else if (list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
