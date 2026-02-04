2b2a9514c0f0dcf278f36086edb11a2df6cd76d5
Seoya0512 <seoya.lee512@gmail.com>
2026-02-04 17:48:37 +0900

    feat: week13 - common LCA

---
+++ lowest-common-ancestor-of-a-binary-search-tree/Seoya0512.py
@@ -0,0 +1,12 @@
+'''
+Time Complexity: O(H)
+- H는 트리의 높이
+'''
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
