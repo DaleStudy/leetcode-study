342f8d86160a93e60add1dc6a0694973813eb74e
dohee <kimdohee@sheridancollege.ca>
2026-02-18 17:20:20 -0500

    Refactor kthSmallest to use the nonlocal keyword for tracking the result

--- kth-smallest-element-in-a-bst/doh6077.py
+++ kth-smallest-element-in-a-bst/doh6077.py
@@ -8,19 +8,16 @@ class Solution:
     # Time Complexity O(N)
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         ans = []
-        final = [0]
-
+        final = 0 
         def dfs(node):
-            
+            nonlocal final
             if not node:
                 return 
-            
             dfs(node.left)
-
             ans.append(node.val)
             if len(ans) == k:
-                final[0] = node.val
+                final = node.val
             if len(ans) < k:
                 dfs(node.right)
         dfs(root)
-        return final[0]
+        return final
