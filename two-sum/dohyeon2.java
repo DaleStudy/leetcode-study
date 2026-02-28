c86b2012edd7a52fc0888b04b8f7379b9d044d90
Dohyeon Ju <syate@naver.com>
2026-03-01 03:59:29 +0900

    파일의 마지막에 개행문자를 추가

--- two-sum/dohyeon2.java
+++ two-sum/dohyeon2.java
@@ -2,29 +2,30 @@ import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Approach : using HashMap to get index with the element in O(n) time complexity
+        // Approach : using HashMap to get index with the element in O(n) time
+        // complexity
         // SpaceComplexity is also O(n)
-        HashMap<Integer,Integer> numIndexMap = new HashMap<Integer,Integer>();
+        HashMap<Integer, Integer> numIndexMap = new HashMap<Integer, Integer>();
 
         // Make key and value HashMap
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            numIndexMap.put(num,i);
+            numIndexMap.put(num, i);
         }
 
         // Search for the other operand looping nums
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int operand = target - num;
             Integer index = numIndexMap.get(operand);
             boolean indexExists = index != null;
             boolean indexExistsAndIndexIsNotTheNum = indexExists && i != index;
-            if(indexExistsAndIndexIsNotTheNum){
+            if (indexExistsAndIndexIsNotTheNum) {
                 // Manual sort
-                if( i < index){
-                    return new int[]{ i, index };
-                }else{
-                    return new int[]{ index, i };
+                if (i < index) {
+                    return new int[] { i, index };
+                } else {
+                    return new int[] { index, i };
                 }
             }
         }
