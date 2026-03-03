e1cbafe787108ceb633512f6c49d66a77e0eb50e
hyejj19 <hyejj19@naver.com>
2026-03-03 14:13:36 +0900

    containsDuplicate solution2

--- contains-duplicate/hyejj19.ts
+++ contains-duplicate/hyejj19.ts
@@ -1,12 +1,3 @@
 function containsDuplicate(nums: number[]): boolean {
-  const numsMap = new Map();
-  nums.forEach(n => {
-    numsMap.set(n, (numsMap.get(n) || 0) + 1);
-  });
-
-  for (const [_, count] of numsMap) {
-    if (count >= 2) return true;
-  }
-
-  return false;
+  return new Set(nums).size !== nums.length;
 }
