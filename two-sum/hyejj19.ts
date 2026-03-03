2d8a742e49710f2b3e28c3e1be4a56ea24244aff
hyejj19 <hyejj19@naver.com>
2026-03-03 15:10:24 +0900

    twoSum solution3

--- two-sum/hyejj19.ts
+++ two-sum/hyejj19.ts
@@ -1,11 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
   const table = new Map();
-  nums.forEach((num, idx) => table.set(num, idx));
 
   for (let i = 0; i < nums.length; i++) {
-    const result = target - nums[i];
-    if (table.has(result) && table.get(result) !== i) {
-      return [i, table.get(result)];
+    const complement = target - nums[i];
+
+    if (table.has(complement)) {
+      return [table.get(complement)!, i];
     }
+
+    table.set(nums[i], i);
   }
+
+  return [];
 }
