7f99b920a9b7ed876e2d996bf81eb8616b57f354
hyerijung <hyerijung0903@gmail.com>
2026-03-04 16:11:12 +0900

    top-l-frequent-elements solution2

--- top-k-frequent-elements/hyeri0903.py
+++ top-k-frequent-elements/hyeri0903.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         table = {}
@@ -6,7 +8,17 @@ class Solution:
                 table[n] += 1
             else:
                 table[n] = 1
-        sorted_table = sorted(table.items(), key=lambda x:x[1], reverse = True)
-    
-        return [ key for key, freq in sorted_table[:k]]
-    
+        #sol 1) sorting 사용한 case
+        # sorted_table = sorted(table.items(), key=lambda x:x[1], reverse = True)
+        # return [ key for key, freq in sorted_table[:k]]
+
+        #sol 2) min heap 사용한 case
+        #convert the frequencies to negative values so it return first
+        heap = [(-freq, key) for key, freq in table.items()]
+        heapq.heapify(heap)
+
+        res = []
+        for _ in range(k):
+            freq, key = heapq.heappop(heap)
+            res.append(key)
+        return res
