ea5ed1f815e3feac26ee0e91cba21b6b8f1fcbef
sadie100 <03sadie@gmail.com>
2026-03-06 14:12:59 +0900

    sadie100: two sum solution 수정

--- two-sum/sadie100.ts
+++ two-sum/sadie100.ts
@@ -1,31 +1,24 @@
 /*
 풀이
-nums를 값과 index가 있는 객체 배열 newNums로 재정의한다
-newNums를 값 오름차순으로 정렬하고 두 포인터 start, end를 둔다
-nums[start] + nums[end]가 target보다 작으면 start+1하고, target보다 크면 end-1한다. 같으면 nums[start]과 nums[end]의 오리진 index를 리턴한다.
-만약 start>=end가 되면 빈 배열을 리턴한다
+해시맵을 이용해 현재 값의 보수(target - num)가 이미 등장했는지 확인한다.
+
 
 시간복잡도
-O(nLogN) - 정렬. 포인터 순회는 O(N)이므로 최종적으로 O(nLogN)
+O(N) - nums 순회
 
 공간복잡도
-O(N) : newNums 배열
+O(N) - numObj 생성
 */
 
 function twoSum(nums: number[], target: number): number[] {
-  const newNums = nums.map((value, idx) => ({ value, idx }))
-  const sortedNums = newNums.sort((a, b) => a.value - b.value)
-  let start = 0
-  let end = nums.length - 1
+  const numObj: { [key: number]: number } = {}
 
-  while (start < end) {
-    const total = sortedNums[start].value + sortedNums[end].value
-    if (total === target) {
-      return [sortedNums[start].idx, sortedNums[end].idx]
-    } else if (total > target) {
-      end -= 1
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]
+    if (target - num in numObj) {
+      return [i, numObj[target - num]]
     } else {
-      start += 1
+      numObj[num] = i
     }
   }
 
