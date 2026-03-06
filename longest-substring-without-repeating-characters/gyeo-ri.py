bc5da915d12bfee3b229314a5b31cb4e423cdfc8
gyeo-ri <gyeori.world@gmail.com>
2026-03-07 03:11:58 +0900

    feat: dict을 사용한 구현

--- longest-substring-without-repeating-characters/gyeo-ri.py
+++ longest-substring-without-repeating-characters/gyeo-ri.py
@@ -1,5 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        used_characters = dict()
+        longest_length = 0
+        start_idx = 0
+
+        for end_idx, c in enumerate(s):
+            if c in used_characters:
+                duplicated_idx = used_characters[c]
+                if duplicated_idx >= start_idx:
+                    start_idx = duplicated_idx + 1
+
+            used_characters[c] = end_idx
+            longest_length = max(longest_length, end_idx - start_idx + 1)
+
+        return longest_length
+
+
+"""
+# Set을 이용한 구현(Dict와 복잡도 상으로는 큰 차이 없음)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         used_characters = set()
         longest_length = 0
         start_idx = 0
@@ -10,10 +30,11 @@ class Solution:
                 start_idx += 1
 
             used_characters.add(s[end_idx])
+            # 속도는 if문보다 약간 느리지만 가독성 크게 개선
             longest_length = max(longest_length, end_idx - start_idx + 1)
 
         return longest_length
-
+"""
 
 if __name__ == "__main__":
     solution = Solution()
