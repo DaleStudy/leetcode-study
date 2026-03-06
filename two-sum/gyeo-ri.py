938bce8dd87ea2290cf173d178ec7b49d510a2a5
gyeo-ri <gyeori.world@gmail.com>
2026-03-07 01:00:57 +0900

    two-sum: final answer

--- two-sum/gyeo-ri.py
+++ two-sum/gyeo-ri.py
@@ -1,11 +1,15 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        nums_dict = {}
 
         for idx1, num1 in enumerate(nums):
-            back = nums[idx1 + 1 :]
-            for idx2, num2 in enumerate(back):
-                if num1 + num2 == target:
-                    return [idx1, idx1 + idx2 + 1]
+            num2 = target - num1
+            idx2 = nums_dict.get(num2)
+
+            if idx2 is None:
+                nums_dict.update({num1: idx1})
+            else:
+                return [idx1, idx2]
 
         raise Exception("Invalid nums and target")
 
