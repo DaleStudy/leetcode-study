1d76061e248e2702d5d0eb4f53ecb500e68c104b
mrlee7 <jplee227@gmail.com>
2026-03-08 22:34:55 +0900

    top k frequent elements

--- top-k-frequent-elements/mrlee7.py
+++ top-k-frequent-elements/mrlee7.py
@@ -2,9 +2,9 @@ from typing import List
 
 """
 Ideation
-    해시맵에 각 원소별 
+    해시맵에 각 원소별 빈도수를 저장한 뒤, 빈도수 높은 순으로 정렬한 다음 앞에서 k개를 반환합니다.
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(n + m log m)
 """
 
 class Solution:
