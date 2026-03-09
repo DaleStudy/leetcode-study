f039b5f39b84f08f1d12cc4ae611f83810b4b51f
mrlee7 <jplee227@gmail.com>
2026-03-10 07:34:07 +0900

    valid anagram solution - add annotation

--- valid-anagram/mrlee7.py
+++ valid-anagram/mrlee7.py
@@ -1,3 +1,10 @@
 class Solution:
+    """
+    Ideation:
+        애너그램은 문자열 내 배치만 다를 뿐, 동일한 문자들이 동일한 카운트로 보장되어야 한다.
+        이를 위해 문자 기준으로 정렬하고 같은지 확인합니다.
+    Time Complexity: O(n log n)
+    Space Complexity: O(1)
+    """
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
