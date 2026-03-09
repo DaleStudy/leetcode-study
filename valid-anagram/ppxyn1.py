7b8a25c54bc7074de93987115e68b560ceddd46b
YOUNGJIN NA <120540450+ppxyn1@users.noreply.github.com>
2026-03-09 22:52:25 +0900

    [:fixed] week2 review

--- valid-anagram/ppxyn1.py
+++ valid-anagram/ppxyn1.py
@@ -1,27 +1,26 @@
-#idea: dictionary
 from collections import Counter 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         s_dic = Counter(sorted(s))
+#         t_dic = Counter(sorted(t))
+#         return s_dic==t_dic
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s_dic = Counter(sorted(s))
-        t_dic = Counter(sorted(t))
-        print(s_dic, t_dic)
-        return s_dic==t_dic
+# fixed
+# do not need to both Counter and sorting 
 
-
-
-# Trial and Error
-'''
-When you call sorted() on a dictionary, it only extracts and sorts the keys,and the values are completely ignored.
+# Ans 1 (Only Counter)
+# Time Complexity: O(N)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s_dic = Counter(s)
         t_dic = Counter(t)
-        print(s_dic, t_dic)
-        return sorted(s_dic)==sorted(t_dic)
-'''
+        return s_dic==t_dic
 
-
-
-
+# Ans 2  (Only Sorting)
+# Time Complexity: O(Nlog(N))
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_arr = sorted(s)
+        t_arr = sorted(t)
+        return s_arr==t_arr
 
