16556394a050b54bf6d0d78d013f89ad656d8dcc
Yunyoung Chung <double.y.0525@gmail.com>
2026-03-11 16:34:43 +0900

    [7th batch] week 2 - product of array except self

--- product-of-array-except-self/liza0525.py
+++ product-of-array-except-self/liza0525.py
@@ -20,3 +20,32 @@ class Solution:
             answers[i] *= after_total_prod
 
         return answers
+
+
+# 7기 풀이
+# 시간 복잡도: O(n)
+#  - nums의 길이만큼이 최대 연산 횟수가 된다. for문 두 번 돌린 것은 O(2n) -> O(n)으로 표기 가능
+# 공간 복잡도: O(n)
+#  - nums의 길이만큼 res를 만든다.
+#  - 단 이는 return variable이라서 리트코드에서는 공간 복잡도 계산에서 제외, 조건의 O(1)와 동일하다고 할 수 있다.
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # 정답에 대한 리스트 추가
+        res = [1 for _ in range(len(nums))]
+
+        previous_res = 1
+        for i in range(len(nums)):
+            # i번째 인덱싀 이전까지의 값들을 res[i]에 먼저 곱해준 후,
+            # 자기 자신을 previous_res에 계산한다. 이는 다음 loop, 즉 i+1번째일 때 계산된 값을 그대로 곱하게 된다.
+            res[i] *= previous_res
+            previous_res *= nums[i]
+
+        next_res = 1
+        for i in range(len(nums) -1, -1, -1):
+            # 리스트의 맨 뒷쪽 index부터 계산해주면 i번째 인덱스 이후의 값들의 곱들을 계산할 수 있다.
+            # i번째 인덱싀 이후까지의 값들을 res[i]에 먼저 곱해준 후,
+            # 자기 자신을 next_res 계산한다. 이는 다음 loop, 즉 i-1번째일 때 계산된 값을 그대로 곱하게 된다.
+            res[i] *= next_res
+            next_res *= nums[i]
+
+        return res
