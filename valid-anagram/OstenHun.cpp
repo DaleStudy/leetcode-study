30a4b2bbd6d98cdbd7be78b0ef0825097e97e5ce
DongHun <hune0329@gmail.com>
2026-03-14 00:48:20 +0900

    Refine valid-anagram solution

--- valid-anagram/OstenHun.cpp
+++ valid-anagram/OstenHun.cpp
@@ -5,11 +5,50 @@
     An anagram is a word or phrase formed by rearranging
     the letters of a different word or phrase,
     using all the original letters exactly once.
+
+    Input: s = "anagram", t = "nagaram"
+    Output: true
 */
 
 #include <string>
+#include <unordered_map>
 using namespace std;
 
+#pragma region FirstIdea_TwoArrays
+namespace first_idea {
+
+class Solution {
+public:
+    bool isAnagram(const string& s, const string& t) {
+        int freq_s[26] = {};
+        int freq_t[26] = {};
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        for (size_t i = 0; i < s.length(); i++) {
+            freq_s[s[i] - 'a']++;
+        }
+
+        for (size_t i = 0; i < t.length(); i++) {
+            freq_t[t[i] - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq_s[i] != freq_t[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+}  // namespace first_idea
+#pragma endregion
+
+#pragma region FinalSolution_OneArray
 class Solution {
 public:
     bool isAnagram(const string& s, const string& t) {
@@ -33,3 +72,37 @@ public:
         return true;
     }
 };
+#pragma endregion
+
+#pragma region Alternative_UnorderedMap
+namespace unordered_map_idea {
+
+class Solution {
+public:
+    bool isAnagram(const string& s, const string& t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> freq;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        for (char ch : t) {
+            freq[ch]--;
+        }
+
+        for (const auto& [ch, count] : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+}  // namespace unordered_map_idea
+#pragma endregion
