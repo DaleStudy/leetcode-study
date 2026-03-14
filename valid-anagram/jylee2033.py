25e760712a95ee98ac45f16a80994eaaa83c2d74
jla670 <jla670@sfu.ca>
2026-03-14 02:11:21 -0700

    fix review comments

--- valid-anagram/jylee2033.py
+++ valid-anagram/jylee2033.py
@@ -7,11 +7,14 @@ class Solution:
         # Build a hashmap counting characters in t
         char_count = {}
 
+        # for letter in t:
+        #     if letter not in char_count:
+        #         char_count[letter] = 1
+        #     else:
+        #         char_count[letter] += 1
+
         for letter in t:
-            if letter not in char_count:
-                char_count[letter] = 1
-            else:
-                char_count[letter] += 1
+            char_count[letter] = char_count.get(letter, 0) + 1
 
         # Decrease counts using characters from s
         for letter in s:
@@ -25,3 +28,6 @@ class Solution:
                 return False
 
         return True
+
+# Time Complexity: O(n)
+# Space Complexity: O(1) since the hashmap stores at most 26 letters
