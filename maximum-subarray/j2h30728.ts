4f95ad631aa43e43a471088b222d93c8ebeb41fa
jihyeon <rachel2148072@gmail.com>
2026-03-14 20:38:56 +0900

    53. Maximum Subarray

---
+++ maximum-subarray/j2h30728.ts
@@ -0,0 +1,11 @@
+function maxSubArray(nums: number[]): number {
+  let current = nums[0];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    current = Math.max(nums[i], current + nums[i]);
+    max = Math.max(current, max);
+  }
+
+  return max;
+}
