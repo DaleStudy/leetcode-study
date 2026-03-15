14a2041958389445a8af1ec67e31c2fcd8bf758d
mrlee7 <jplee227@gmail.com>
2026-03-15 23:42:46 +0900

    formatting

--- climbing-stairs/mrlee7.py
+++ climbing-stairs/mrlee7.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        stairs = {1: 1, 2:2}
+        stairs = {1: 1, 2: 2}
 
         for i in range(3, n + 1):
             stairs[i] = stairs[i - 1] + stairs[i - 2]
