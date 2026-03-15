fbbbae138e38c95e77336571c8043564cffc1ca5
mrlee7 <jplee227@gmail.com>
2026-03-15 23:37:36 +0900

    climbing stairs solution

---
+++ climbing-stairs/mrlee7.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        stairs = {1: 1, 2:2}
+
+        for i in range(3, n + 1):
+            stairs[i] = stairs[i - 1] + stairs[i - 2]
+
+        return stairs[n]
