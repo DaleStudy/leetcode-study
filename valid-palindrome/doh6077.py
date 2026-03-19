235ff39fe7376c1b23b99b1d2c987420587121bd
dohee <kimdohee@sheridancollege.ca>
2026-03-19 15:02:18 -0400

    Valid Palindrome Solution

--- valid-palindrome/doh6077.py
+++ valid-palindrome/doh6077.py
@@ -14,3 +14,16 @@
 #                 return False 
         
 #         return True
+# 7기 
+# use two pointers 
+# Time Complexity: O(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        l, r = 0, len(cleaned_s) -  1
+        while l < r :
+            if cleaned_s[l] != cleaned_s[r]:
+                return False 
+            l += 1
+            r -= 1
+        return True 
