909b6d744d4bdb658373f2a9a0d6b3f3b211c6db
jla670 <jla670@sfu.ca>
2026-03-20 22:12:54 -0700

    number of 1 bits solution refactor

--- number-of-1-bits/jylee2033.py
+++ number-of-1-bits/jylee2033.py
@@ -1,25 +1,38 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # 1. Find highest power of 2 range that covers n
-        # 2. Greedily subtract from largest power of 2 to smallest
+        # # 1. Find highest power of 2 range that covers n
+        # # 2. Greedily subtract from largest power of 2 to smallest
 
-        # powers : 1 + 2 + 4 +  8 + 16 + ...
-        # total  : 1 + 3 + 7 + 15 + 31 + ...
-        # index  : 0   1   2    3    4
+        # # powers : 1 + 2 + 4 +  8 + 16 + ...
+        # # total  : 1 + 3 + 7 + 15 + 31 + ...
+        # # index  : 0   1   2    3    4
 
-        i = 0
-        total = 1
+        # i = 0
+        # total = 1
 
-        while n > total:
-            i += 1
-            total += 2 ** i
+        # while n > total:
+        #     i += 1
+        #     total += 2 ** i
+
+        # count = 0
+
+        # for j in range(i, -1, -1):
+        #     if n >= 2 ** j:
+        #         n -= 2 ** j
+        #         count += 1
+
+        # return count
+
+# Time Complexity : O(log n)
+# Space Complexity : O(1)
 
         count = 0
 
-        for j in range(i, -1, -1):
-            if n >= 2 ** j:
-                n -= 2 ** j
+        while n > 0:
+            if n % 2 == 1:
                 count += 1
+
+            n //= 2
 
         return count
 
