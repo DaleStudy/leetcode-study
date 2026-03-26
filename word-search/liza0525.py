08a7d6cb5d87cb0720800195fa6adac97f0ac594
Yunyoung Chung <double.y.0525@gmail.com>
2026-03-26 23:22:23 +0900

    [7th batch] week 4 - word search

--- word-search/liza0525.py
+++ word-search/liza0525.py
@@ -65,3 +65,73 @@ class Solution:
 
         # 모든 시도를 해도 못 찾으면 False
         return False
+
+
+# 7기 풀이
+# 시간 복잡도: O((n * m) * 4 ^ d)
+#  - depth의 길이(d)만큼 네 방향을 탐색하며, 출발점의 개수 n * m 모두 탐색할 때 최악의 시간
+# 공간 복잡도: O(n * m)
+#  - visited 변수를 사용하기 때문에 행 * 열의 개수만큼의 공간을 필요로 함
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # 보드의 행, 열의 길이를 미리 저장
+        num_rows = len(board)
+        num_cols = len(board[0])
+
+        # dfs 탐색 시 이미 방문한 경우를 체크하기 위한 visited를 생성
+        visited = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+
+        # dfs 탐색 시 한 칸을 기준으로 하여 4방향으로 탐색 가능하게 directions을 만든다.
+        # 순서대로 아래로 진출, 위로 진출, 왼쪽으로 진출, 오른쪽으로 진출
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+        def check_exist_word(i, j, depth):
+            if depth == len(word) - 1:
+                # depth가 word의 길이에 가까워지면(배열이라 -1 해줌)
+                # 탐색을 마쳤으므로 True를 반환
+                return True
+
+            # 현재의 칸은 방문했다는 의미로 1로 변경
+            visited[i][j] = 1
+            for dir_i, dir_j in directions:
+                # 네 방향을 순차로 탐색한다.
+                # 다음 칸의 위치를 나타내는 next_i와 next_j를 먼저 계산
+                next_i, next_j = i + dir_i, j + dir_j
+                if not (
+                    0 <= next_i < num_rows  # 다음 칸의 행 번호가 행의 길이 내에 있고
+                    and 0 <= next_j < num_cols  # 다음 칸의 열 번호가 열의 길이 내에 있고
+                    and not visited[next_i][next_j]  # 아직 방문하지 않았으며
+                    and board[next_i][next_j] == word[depth + 1]  # 다음 글자가 word의 다음 글자에 해당해야 함
+                ):
+                    # 그렇지 않은 경우에는 탐색할 필요가 없으므로 다음 방향을 찾는다
+                    continue
+                
+                # 위의 조건을 모두 만족하면 탐색해도 된다.
+                exist_word = check_exist_word(
+                    next_i,
+                    next_j,
+                    depth + 1  # word의 다음 index를 찾아야 함
+                )
+                if exist_word:
+                    # 단어를 찾았다고 하면 early return
+                    return True
+                
+            # 백트래킹을 위해 현재 칸의 visited 값을 원복한다.
+            visited[i][j] = 0
+
+            # 단어를 찾지 못했을 땐 False 반환
+            return False
+
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if board[i][j] != word[0]:
+                    # 출발 글자가 word의 첫 글자랑 같지 않으면 탐색할 필요가 없으므로 넘긴다.
+                    continue
+
+                exist_word = check_exist_word(i, j, 0)
+                if exist_word:
+                    # 단어를 찾았으면 탐색을 더이상 하지 않아도 되므로 early return
+                    return True
+
+        # 보드의 모든 탐색을 해도 찾지 못한 경우에는 False 반환
+        return False
