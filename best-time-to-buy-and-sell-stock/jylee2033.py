7a1d0fb3ff3e135cd0f93d61189c8b0245eb25ac
jla670 <jla670@sfu.ca>
2026-03-30 12:20:45 -0700

    best time to buy and sell stock solution

---
+++ best-time-to-buy-and-sell-stock/jylee2033.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # When length is 1, no profit possible
+        if len(prices) == 1:
+            return 0
+
+        buy = 10 ** 5
+        profit = 0
+
+        # Iterate through prices
+        for i, price in enumerate(prices):
+            if price < buy:
+                buy = price
+
+                for j in range(i + 1, len(prices)):
+                    if prices[j] <= buy:
+                        continue
+
+                    if prices[j] - buy > profit:
+                        profit = prices[j] - buy
+
+        return profit
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
