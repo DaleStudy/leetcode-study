2b976cde74193ea02fb078f34bdd11fda2e53d36
yihyun-kim1 <hyunnyfarm@naver.com>
2026-04-04 14:17:34 +0900

    add solution: best-time-to-buy-and-sell-stock

---
+++ best-time-to-buy-and-sell-stock/yihyun-kim1.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = (prices) => {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+  }
+
+  return maxProfit;
+};
