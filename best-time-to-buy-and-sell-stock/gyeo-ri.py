ecc9423ba106064e22a4b06f8473c2c139fb4252
gyeo-ri <gyeori.world@gmail.com>
2026-04-04 22:19:27 +0900

    fix: 성능은 비슷하나 코드를 간소화한 버전

--- best-time-to-buy-and-sell-stock/gyeo-ri.py
+++ best-time-to-buy-and-sell-stock/gyeo-ri.py
@@ -1,17 +1,25 @@
+"""
+[결과 요약]
+# 시도한 로직 수: 4
+    1. if/else 문으로 분기 처리(시간: O(n) / 공간: O(1))
+        -
+    2. if문 대신 min / max를 계속 계산하는 로식
+        - None 대신 float(inf)를 사용하여 None 검증 분기 제거
+        - inf 사용을 위해서 마지막 return에 int() 타입 변환 필요
+        - 성능은 1과 큰 차이 없으며 코드가 간소화
+"""
+
+
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         max_profit = 0
-        buy_at = None
+        min_price = float("inf")
 
         for p in prices:
-            if buy_at is None:
-                buy_at = p
-            elif buy_at > p:
-                buy_at = p
-            elif buy_at < p:
-                max_profit = max(max_profit, p - buy_at)
+            min_price = min(min_price, p)
+            max_profit = max(max_profit, p - min_price)
 
-        return max_profit
+        return int(max_profit)
 
 
 if __name__ == "__main__":
@@ -19,6 +27,7 @@ if __name__ == "__main__":
         ([7, 1, 5, 3, 6, 4], 5),
         ([7, 6, 4, 3, 1], 0),
         ([2, 4, 1], 2),
+        ([1, 2, 4, 10000, 2, 1, 3], 9999),
     ]
 
     solution = Solution()
