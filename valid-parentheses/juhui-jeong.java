128600aa6d98747a3200b71b30d744db37b3a95c
juhui <wngml0108@gmail.com>
2026-04-10 17:36:03 +0900

    WEEK 06 Solutions

---
+++ valid-parentheses/juhui-jeong.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        } 
+        return stack.isEmpty();
+    }
+}
