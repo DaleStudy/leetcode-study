dd2cee8742c27b24322cfb71981ae0d6cbc6d098
Ju Hwijung <contact@juhwijung.com>
2026-04-11 18:09:09 +0900

    시간복잡도를 개선한 풀이 추가
    - Longest Increasing Subsequence

--- longest-increasing-subsequence/hwi-middle.cpp
+++ longest-increasing-subsequence/hwi-middle.cpp
@@ -1,3 +1,4 @@
+// O(n^2) 풀이
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -19,5 +20,29 @@ public:
 
         // d의 최댓값 반환
         return *max_element(d.begin(), d.end());
+    }
+};
+
+// O(n log n) 풀이
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> d; // d[i] = 길이가 i + 1인 LIS의 가장 작은 마지막 값
+
+        for (int i = 0; i < n; ++i)
+        {
+            auto it = lower_bound(d.begin(), d.end(), nums[i]);
+            if (it == d.end()) // nums[i]가 d의 모든 원소보다 큰 경우
+            {
+                d.push_back(nums[i]);
+            }
+            else // nums[i]의 자리를 찾은 경우
+            {
+                *it = nums[i]; // 그냥 대체하는게 이득임 -> 길이는 유지됐고, 더 이어붙일 수 있는 수의 범위는 늘어나므로
+            }
+        }
+
+        return d.size();
     }
 };
