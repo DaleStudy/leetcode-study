3771ae247b91f0f4405c770241146b56142c8098
gyeo-ri <gyeori.world@gmail.com>
2026-04-11 23:27:33 +0900

    feat: 결과 요약 작성

--- container-with-most-water/gyeo-ri.py
+++ container-with-most-water/gyeo-ri.py
@@ -1,3 +1,12 @@
+"""
+[결과 요약]
+# 시도한 로직 수: 3
+    1. 모든 결과룰 순회하면서 시간복잡도 O(n^2)을 시도 -> LeetCode에서 Time Limit Exceed 발생
+    2. 리스트 양쪽 끝에서 포인터를 이동하는 알고리즘: 시간/공간 모두 O(n)
+    3. (2)의 로직은 유지한 채로 코드 가독성을 개선(알고리즘의 흐름을 쉽게 이해할 수 있도록)
+"""
+
+
 class Solution:
     def maxArea(self, height: list[int]) -> int:
         left, right = 0, len(height) - 1
