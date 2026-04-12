6b7b61982f18ffae7702f33d78ce51190c93ed51
yihyun-kim1 <hyunnyfarm@naver.com>
2026-04-12 23:25:10 +0900

    add solution: longest-increasing-subsequence

---
+++ longest-increasing-subsequence/yihyun-kim1.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const lengthOfLIS = (nums) => {
+  const dp = new Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
