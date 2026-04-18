0b94abbc140583bb8f9e8e92f23b502c0603c324
gyeo-ri <gyeori.world@gmail.com>
2026-04-18 22:56:02 +0900

    fix: 재귀를 사용한 풀이

--- reverse-linked-list/gyeo-ri.py
+++ reverse-linked-list/gyeo-ri.py
@@ -5,6 +5,29 @@ class ListNode:
         self.next: ListNode | None = next
 
 
+# 재귀를 사용한 최종 풀이
+class Solution:
+    def reverseList(self, head: ListNode | None) -> ListNode | None:
+        # 1. 리스트가 비었거나(head is None) 마지막 노드일 때(head.next is None)는 종료
+        if head is None or head.next is None:
+            return head
+
+        # 2. 현재 노드(=head)를 제외하고, 다음 노드(next_node)부터 끝까지를 재귀 호출(뒤쪽 리스트를 뒤집기)
+        next_node = head.next
+        reversed_node = self.reverseList(next_node)
+
+        # 3. 현재 노드를 뒤집힌 이전 노드(reversed_node)의 뒤에 붙이기
+        # reversed_node의 가장 끝은 head.next이므로, head를 next_node 뒤에 붙인 다음 마지막 노드로 표시(head.next = None)
+        next_node.next = head
+        head.next = None
+
+        # 4. 최종 노드를 반환
+        # 2의 reversed_node는 head(=마지막 노드)가 빠진 상태였지만 3에서 next_node -> head를 연결하면서 완성됨
+        return reversed_node
+
+
+"""
+# 반복문을 사용한 풀이
 class Solution:
     def reverseList(self, head: ListNode | None) -> ListNode | None:
         current_node = head
@@ -19,6 +42,7 @@ class Solution:
 
             reversed_node = current_node
             current_node = next_node
+"""
 
 
 if __name__ == "__main__":
