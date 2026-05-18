54c671fdbc09d7ae25d15508e912565c1812fd52
juhui <wngml0108@gmail.com>
2026-05-18 14:29:03 +0900

    WEEK 12: Solutions

--- same-tree/juhui-jeong.java
+++ same-tree/juhui-jeong.java
@@ -10,3 +10,29 @@ class Solution {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queueP = new LinkedList<>();
+        Queue<TreeNode> queueQ = new LinkedList<>();
+
+        queueP.offer(p);
+        queueQ.offer(q);
+
+        while(!queueP.isEmpty() && !queueQ.isEmpty()) {
+            TreeNode curP = queueP.poll();
+            TreeNode curQ = queueQ.poll();
+
+            if (curP == null && curQ == null) continue;
+            if (curP == null || curQ == null) return false;
+
+            if (curP.val != curQ.val) return false;
+
+            queueP.offer(curP.left);
+            queueQ.offer(curQ.left);
+            queueP.offer(curP.right);
+            queueQ.offer(curQ.right);            
+        }
+        return true;
+    }
+}
