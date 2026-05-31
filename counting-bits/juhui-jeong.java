247d35c03448892c98cdb1c0a6fb9fa3b1e6038b
juhui <wngml0108@gmail.com>
2026-06-01 07:47:57 +0900

    WEEK 14: Solutions

--- counting-bits/juhui-jeong.java
+++ counting-bits/juhui-jeong.java
@@ -1,5 +1,6 @@
 
-
+/*
+이전 풀이
 class Solution {
     public int[] countBits(int n) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -20,5 +21,34 @@ class Solution {
             ans[i] = list.get(i);
         }
         return ans;
+    }
+}
+*/
+/*
+시간 복잡도: O(n log n)
+공간 복잡도: O(n)
+ */
+class Solution {
+    public int[] countBits(int n) {
+        // 0부터 n까지의 이진수를 만든다.
+        // 이진수로 변환된 수에서 1의 갯수를 카운팅한다.
+        // 0부터 n까지 1의 갯수를 카운팅한 것을 배열로 만들어 반환한다. 이때 배열의 길이는 n+1.
+        int[] result = new int[n+1];
+        
+        for (int i = 0; i <= n; i++) {
+            int num = divideTwo(i, 0);
+            result[i] = num;
+        }
+        return result;
+    }
+
+    private int divideTwo(int n, int remainderCnt) {
+        int divideNum = n/2;
+        int remainder = n%2;
+
+        int calcualteReminderNum = remainder + remainderCnt;
+        if (divideNum == 0) return calcualteReminderNum;
+
+        return divideTwo(divideNum, calcualteReminderNum);
     }
 }
