52aa51dd2f7872436da3b7f3b21c92d2c02fc655
reeseo3o <notyaeji@gmail.com>
2026-06-12 23:08:33 +0900

    style: fix lint in subtree-of-another-tree

--- subtree-of-another-tree/reeseo3o.js
+++ subtree-of-another-tree/reeseo3o.js
@@ -2,18 +2,17 @@
 // Space Complexity: O(n+m)
 
 const isSubtree = (root, subRoot) => {
-    const isSame = (a, b) => {
-      if (!a && !b) return true;
-      if (!a || !b) return false;
-      if (a.val !== b.val) return false;
-      return isSame(a.left, b.left) && isSame(a.right, b.right);
-    };
-  
-    if (!root) return false;
-    return (
-      isSame(root, subRoot) ||
-      isSubtree(root.left, subRoot) ||
-      isSubtree(root.right, subRoot)
-    );
+  const isSame = (a, b) => {
+    if (!a && !b) return true;
+    if (!a || !b) return false;
+    if (a.val !== b.val) return false;
+    return isSame(a.left, b.left) && isSame(a.right, b.right);
   };
-  
+
+  if (!root) return false;
+  return (
+    isSame(root, subRoot) ||
+    isSubtree(root.left, subRoot) ||
+    isSubtree(root.right, subRoot)
+  );
+};
