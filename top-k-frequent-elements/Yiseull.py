82f452124c64ee63baf9eab1aedde12e903700d1
Yiseul Park <omjl5123@catholic.ac.kr>
2026-06-21 21:37:09 +0900

    feat: 347. Top K Frequent Elements solution

--- top-k-frequent-elements/Yiseull.py
+++ top-k-frequent-elements/Yiseull.py
@@ -2,4 +2,13 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [key for key, counter in Counter(nums).most_common(k)]
+        # return [key for key, counter in Counter(nums).most_common(k)]
+
+        counter = {}
+        for num in nums:
+            if num in counter:
+                counter[num] += 1
+                continue
+            counter[num] = 1
+
+        return [key for key, counter in sorted(counter.items(), key=lambda x: -x[1])[0:k]]
