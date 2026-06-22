04ff111f2b88f46d75e7c6c8feba917d7ecee05b
JAEKWANG97 <marmong@naver.com>
2026-06-22 18:08:44 +0900

    week 01 solutions

---
+++ contains-duplicate/jaekwang97.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
