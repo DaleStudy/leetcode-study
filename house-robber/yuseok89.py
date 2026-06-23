4e4e41226157b830cd23f7acfae3c40f21d169f6
Yuseok Jo <yuseok89@gmail.com>
2026-06-23 22:56:20 +0900

    house robber solution

---
+++ house-robber/yuseok89.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        two_before, one_before = 0, 0
+
+        for num in nums:
+            cur = max(two_before + num, one_before)
+            two_before = one_before
+            one_before = cur
+
+        return max(one_before, two_before)
