06c94430f47309be6bfd08018b66ceebcb467c91
ICE0208 <doky3307@naver.com>
2026-06-26 01:31:21 +0900

    longest-consecutive-sequence

---
+++ longest-consecutive-sequence/ICE0208.py
@@ -0,0 +1,53 @@
+# 2차 풀이
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        ss = set(nums)
+
+        max_cnt = 0
+        for n in ss:
+            # 연속점의 시작점이 아닐 경우 패스
+            if n-1 in ss:
+                continue
+            
+            # 연속점의 시작점일 때만 실행되는 로직이므로
+            # 숫자를 하나씩 증가시켜보면서 체크
+            cnt = 1
+            i = n+1
+            while i in ss:
+                i += 1
+                cnt += 1
+            
+            max_cnt = max(max_cnt, cnt)
+        
+        return max_cnt
+
+
+# # 1차 풀이
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         ss = set(nums)
+#         visit = set()
+
+#         max_cnt = 0
+#         for n in ss:
+#             if n in visit:
+#                 continue
+#             visit.add(n)
+
+#             cnt = 1
+            
+#             i = n+1
+#             while i in ss:
+#                 visit.add(i)
+#                 i += 1
+#                 cnt += 1
+
+#             i = n-1
+#             while i in ss:
+#                 visit.add(i)
+#                 cnt += 1
+#                 i -= 1
+            
+#             max_cnt = max(max_cnt, cnt)
+        
+#         return max_cnt
