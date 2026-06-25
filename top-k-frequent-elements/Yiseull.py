9f3f0b1d6dd07d91b540cc5749035923278b8aad
Yiseul Park <omjl5123@catholic.ac.kr>
2026-06-25 22:19:47 +0900

    feat: 347. Top K Frequent Elements solution

--- top-k-frequent-elements/Yiseull.py
+++ top-k-frequent-elements/Yiseull.py
@@ -4,6 +4,15 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # return [key for key, counter in Counter(nums).most_common(k)]
 
+        # counter = {}
+        # for num in nums:
+        #     if num in counter:
+        #         counter[num] += 1
+        #         continue
+        #     counter[num] = 1
+        #
+        # return [key for key, counter in sorted(counter.items(), key=lambda x: -x[1])[0:k]]
+
         counter = {}
         for num in nums:
             if num in counter:
@@ -11,4 +20,11 @@ class Solution:
                 continue
             counter[num] = 1
 
-        return [key for key, counter in sorted(counter.items(), key=lambda x: -x[1])[0:k]]
+        heap = [(-v, k) for k, v in counter.items()]
+        heapify(heap)
+
+        answer = []
+        for _ in range(k):
+            answer.append(heappop(heap)[1])
+
+        return answer
