9e5326588ed1fff68f172757534852a0bca14e26
kim heoung doon <63146721+chapse57@users.noreply.github.com>
2026-06-26 17:05:47 +0900

    [chapse57] Week 1

---
+++ top-k-frequent-elements/chapse57.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = {}
+        for n in nums:
+            if n in count:
+                count[n] +=1
+            else:
+                count[n] =1      
+                # count.items()를 횟수 큰 순으로 정렬
+        freq = sorted(count.items(), key=lambda x: x[1], reverse=True)
+
+        result = []
+        for x in freq[:k]:
+            result.append(x[0])
+        return result
