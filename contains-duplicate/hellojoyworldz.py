a0fa13c77ce08579e8558fb256c0e7cffaa8e7bb
hellojoyworldz <hellojoyworldz@gmail.com>
2026-06-26 21:49:41 +0900

    1주차 파일 생성

---
+++ contains-duplicate/hellojoyworldz.py
@@ -0,0 +1,12 @@
+# - 문제: https://leetcode.com/problems/contains-duplicate/
+# - 해설: https://www.algodale.com/problems/contains-duplicate/
+
+# 217. Contains Duplicate
+# - Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        return len(set(nums)) != len(nums)
