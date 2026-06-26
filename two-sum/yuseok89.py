e81950be1c9acfb437c9381c1309e461e952d7e3
Yuseok Jo <yuseok89@gmail.com>
2026-06-27 00:55:32 +0900

    [yuseok89] WEEK 01 solutions (#2651)
    * contains duplicate solution
    
    * two sum solution
    
    * top k frequent elements solution
    
    * longest consecutive sequence solution
    
    * house robber solution
    
    * add newline
    
    * 리뷰 반영
    
    * 리뷰 반영
    
    * 개선 - AI assist

---
+++ two-sum/yuseok89.py
@@ -0,0 +1,16 @@
+// TC: O(N)
+// SC: O(N)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx_map = {}
+
+        for idx, num in enumerate(nums):
+            need = target - num
+
+            if need in idx_map and idx != idx_map[need]:
+                return [idx, idx_map[need]]
+
+            idx_map[num] = idx
+
+        return []
+
