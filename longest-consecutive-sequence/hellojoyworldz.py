435576e39e56b6160b5b4d5457e27ec57286a28f
Sunah Gwak <hellojoyworldz@gmail.com>
2026-06-27 19:55:26 +0900

    week01 문제풀기

--- longest-consecutive-sequence/hellojoyworldz.py
+++ longest-consecutive-sequence/hellojoyworldz.py
@@ -1,3 +1,26 @@
 # - 문제: https://leetcode.com/problems/longest-consecutive-sequence/
 # - 풀이: https://www.algodale.com/problems/longest-consecutive-sequence/
 
+# Current complexity:O(NlogN)
+# Suggested complexity:O(N)
+
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        sorted_nums = sorted(set(nums))
+
+        max_sequence = 1
+        current_sequence = 1
+
+        for i in range(1, len(sorted_nums)):
+            if sorted_nums[i] - sorted_nums[i - 1] == 1:
+                current_sequence += 1
+                max_sequence = max(max_sequence, current_sequence)
+            else:
+                current_sequence = 1
+
+        return max(max_sequence, current_sequence)
+
--- top-k-frequent-elements/hellojoyworldz.py
+++ top-k-frequent-elements/hellojoyworldz.py
@@ -1,3 +1,12 @@
 # - 문제: https://leetcode.com/problems/top-k-frequent-elements/
 # - 풀이: https://www.algodale.com/problems/top-k-frequent-elements/
 
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        return [num for num, count in counter.most_common(k)]
+
