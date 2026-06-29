a7c2098206acef7f99d1aa93a5d3e8f661db8f3a
woo <ilov1112@gmail.com>
2026-06-29 16:04:49 +0900

    fix: 좀 더 간결하게 수정

--- product-of-array-except-self/alphaorderly.py
+++ product-of-array-except-self/alphaorderly.py
@@ -5,22 +5,9 @@ class Solution:
         """
         N = len(nums)
 
-        zeroes = nums.count(0)
-
-        if zeroes > 1:
-            return [0] * N
-
         ans = [0] * N
-        acc = 1
 
-        # This could make logic complex, so treat it separately
-        if zeroes == 1:
-            place = nums.index(0)
-            for i in range(N):
-                if i != place:
-                    acc *= nums[i]
-            ans[place] = acc
-            return ans
+        acc = 1
 
         for i in range(N):
             ans[i] = acc
