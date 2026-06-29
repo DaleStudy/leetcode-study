8bd5a3234eddb92d76d5b516e4cf7a1128fa8d80
Yuseok Jo <yuseok89@gmail.com>
2026-06-29 22:50:12 +0900

    valid anagram solution

---
+++ valid-anagram/yuseok89.py
@@ -0,0 +1,13 @@
+## TC: O(N)
+## SC: O(1)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        cnt_s = Counter(s)
+        cnt_t = Counter(t)
+
+        return cnt_s == cnt_t
+
