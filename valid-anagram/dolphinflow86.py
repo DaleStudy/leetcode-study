d5a06a52c457f3cf0099bfcf89f60aca72f3863b
dolphinflow86 <dolphinflow86@gmail.com>
2026-06-29 22:02:49 +0900

    valid anagram optimized solution

--- valid-anagram/dolphinflow86.py
+++ valid-anagram/dolphinflow86.py
@@ -4,3 +4,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+# 2) Using dict to store count of the first string and decrease back to see if there's negative count.
+# TC: O(N + M) where N is the length of s and M is the length of t.
+# SC: O(N + M) where N is the length of s and M is the length of t.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        char_map: Dict[str, int] = {}
+        for ch in s:
+            char_map[ch] = char_map.get(ch, 0) + 1
+
+        for ch in t:
+            if ch not in char_map or char_map[ch] == 0:
+                return False
+
+            char_map[ch] -= 1
+
+        return True
