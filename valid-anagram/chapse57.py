afde7ad9a74f5bcc4037f6c8faf0e7275dc5e5d8
kim heoung doon <63146721+chapse57@users.noreply.github.com>
2026-06-30 16:32:42 +0900

    [chapse57] Week 2

---
+++ valid-anagram/chapse57.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+         
+        
+        
