db3afb5c6047b4f59e97337d6d414864f43aef06
Yiseul Park <omjl5123@catholic.ac.kr>
2026-07-04 23:17:48 +0900

    feat: 70. Climbing Stairs solution2

--- climbing-stairs/Yiseull.py
+++ climbing-stairs/Yiseull.py
@@ -1,11 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
+        if n <= 2: return n
 
-        dp = [0 for _ in range(n + 1)]
-        dp[0], dp[1] = 1, 1
+        p, q = 1, 2
+        for i in range(3, n + 1):
+            p, q = q, p + q
 
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-
-        return dp[n]
+        return q
