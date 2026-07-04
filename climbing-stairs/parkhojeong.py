a2e3989a237177583a67664c52af3122d85282e0
Hojeong Park <parkhj062@gmail.com>
2026-07-04 14:02:43 +0900

    update climbing-stairs

--- climbing-stairs/parkhojeong.py
+++ climbing-stairs/parkhojeong.py
@@ -4,10 +4,10 @@ class Solution:
             return 1
 
         arr = [0] * n
-        arr[0] = 1
-        arr[1] = 2
+        arr[0], arr[1] = 1, 2
+
         for i in range(2, n):
             arr[i] = arr[i - 1] + arr[i - 2]
 
-        return arr[n - 1]
+        return arr[-1]
 
