dd0e8ec068b4e9292aab4f4fe61f9482697c391c
Hoonji Choi <poi789047@gmail.com>
2026-07-05 16:00:21 -0700

    Feedback updated climbing-stairs/hoonjichoi1.java
    Co-authored-by: Hojeong Park <parkhj062@gmail.com>

--- climbing-stairs/hoonjichoi1.java
+++ climbing-stairs/hoonjichoi1.java
@@ -13,14 +13,12 @@ class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int prev = 1;
-        int result = 2;
-        int temp = 0;
+        int prev = 1, cur = 2;
         for (int i = 2; i < n ; i++) {
-            temp = result;
-            result += prev;
+            int temp = cur;
+            cur += prev;
             prev = temp;
         }     
-        return result;
+        return cur;
     }
 }
