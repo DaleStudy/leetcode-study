6b35d72820d3f2530d397eae7e023e8a6c3d03c1
JAEKWANG97 <marmong@naver.com>
2026-07-10 23:27:36 +0900

    restore positive-input loop condition

--- number-of-1-bits/jaekwang97.java
+++ number-of-1-bits/jaekwang97.java
@@ -2,7 +2,7 @@ class Solution {
     public int hammingWeight(int n) {
         int count = 0;
 
-        while (n != 0) {
+        while (n > 0) {
             n = n & (n - 1);
             count++;
         }
