5306dd31d4ec05ace41ded065b7df944e2031b30
JAEKWANG97 <marmong@naver.com>
2026-07-10 23:18:35 +0900

    number of 1 bits solution

---
+++ number-of-1-bits/jaekwang97.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+}
