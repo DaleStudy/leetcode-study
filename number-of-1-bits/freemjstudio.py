b33b7b1f7479ddc758bea25091f578fe6c540fd4
freemjstudio <mjwoo0@naver.com>
2026-07-11 12:54:17 +0900

    freemjstudio/number-of-1-bits second solution

--- number-of-1-bits/freemjstudio.py
+++ number-of-1-bits/freemjstudio.py
@@ -2,3 +2,13 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         binary = bin(n)
         return binary.count("1")
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0 
+
+        while n > 0:
+            count += n % 2
+            n //=2
+        
+        return count
