f42b34d31bc9f9855269dfee251e5cb99ec7f41c
Yiseul Park <omjl5123@catholic.ac.kr>
2026-07-11 18:48:36 +0900

    feat: 191. Number of 1 Bits solution2

--- number-of-1-bits/Yiseull.java
+++ number-of-1-bits/Yiseull.java
@@ -1,5 +1,12 @@
 class Solution {
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        // return Integer.bitCount(n);
+
+        int answer = 0;
+        while (n != 0) {
+            n &= n-1;
+            answer++;
+        }
+        return answer;
     }
 }
