7b8764b4e32d631f8d610842006ef2a23d237a84
hanseo <yds06206@gmail.com>
2026-07-11 21:02:20 +0900

    refactor: optimize max subarray calculation to use O(1) space

--- maximum-subarray/seueooo.js
+++ maximum-subarray/seueooo.js
@@ -1,17 +1,18 @@
 /**
  * 구간 내 최대 합을 dp에 저장
  * 시간복잡도 O(n)
- * 공간복잡도 O(n)
+ * 공간복잡도 O(1)
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let dp = new Array(nums.length).fill(0);
+  let prev = nums[0];
+  let max = nums[0];
 
-  dp[0] = nums[0];
   for (let i = 1; i < nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    prev = Math.max(prev + nums[i], nums[i]);
+    max = Math.max(max, prev);
   }
 
-  return Math.max(...dp);
+  return max;
 };
