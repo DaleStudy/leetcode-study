de03f8ed480d6ea19e8bd5d6010dacc987966766
xeulbn <gsbtiger0215@gmail.com>
2026-07-14 10:32:17 +0900

    fix: unnecessary variable delete

--- maximum-subarray/xeulbn.java
+++ maximum-subarray/xeulbn.java
@@ -5,26 +5,17 @@ class Solution {
         int currentSum = nums[0];
         int maxSum = nums[0];
 
-        int currentStart = 0;
-        int maxStart = 0;
-        int maxEnd = 0;
-
         for (int i=1; i<nums.length; i++) {
             if (nums[i] > currentSum + nums[i]) {
                 currentSum = nums[i];
-                currentStart = i;
             } else {
                 currentSum += nums[i];
             }
 
             if (currentSum > maxSum) {
                 maxSum = currentSum;
-                maxStart = currentStart;
-                maxEnd = i;
             }
         }
-
         return maxSum;
-
     }
 }
