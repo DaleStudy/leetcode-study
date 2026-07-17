869ca89ef9a3f3dadf71616197df04078634d930
seongmin36 <whtjdals3625@gmail.com>
2026-07-18 04:43:23 +0900

    fix tc: O(n), sc: O(1)

--- merge-two-sorted-lists/seongmin36.js
+++ merge-two-sorted-lists/seongmin36.js
@@ -1,16 +1,10 @@
 /**
-ListNode는 파라미터로 current value와 next value를 지닌다.
-배열 메서드도 사용할 수 없다.
+list1, list2는 시작 노드 객체
+핵심은 '가위바위보 기찻길 룰'
+cur.next가 list1, list2 비교 결과를 배치해주는 역할
 
-나는 원초적인 방법으로 해결해보았다.
-result 배열에 모든 수를 다 넣고, sort()하는 것이다.
-여기서 중요한 점은 '어떻게 배열 → ListNode로 변경하느냐'이다.
-[1, 2, 3] 배열을 1 → 2 → 3 리스트 노드를 만들기 위해서는 컴퓨터 구조상 역방향인 3부터 가져와야한다.
-이때 사용한 메서드는 'reduceRight()'다.
-reduceRight() 메서드는 reduce() 작업순서의 역방향이다.
-
-TC : O(nLogN) → sort()
-SC : O(n)
+TC : O(n)
+SC : O(1)
  */
 /**
  * Definition for singly-linked list.
@@ -25,21 +19,20 @@ SC : O(n)
  * @return {ListNode}
  */
 function mergeTwoLists(list1, list2) {
-  let result = [];
-  let node1 = list1;
-  let node2 = list2;
+  let dummy = new ListNode();
+  let cur = dummy;
 
-  while (node1 !== null) {
-    result.push(node1.val);
-    node1 = node1.next;
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      cur.next = list2;
+      list2 = list2.next;
+    } else {
+      cur.next = list1;
+      list1 = list1.next;
+    }
+    cur = cur.next;
   }
+  cur.next = list1 || list2; // 남아있는 원소 붙이기
 
-  while (node2 !== null) {
-    result.push(node2.val);
-    node2 = node2.next;
-  }
-
-  return result
-    .sort((a, b) => a - b)
-    .reduceRight((next, val) => new ListNode(val, next), null);
+  return dummy.next;
 }
