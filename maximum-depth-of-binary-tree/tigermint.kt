f8f2c312713798322bb48848082de5115a5063db
Seungyeol (Reo) Lee <lsy12210@gmail.com>
2026-07-18 23:46:34 +0900

    [tigermint] WEEK 04 Solutions (#2748)
    * feat: WEEK 04 solution (merge-two-sorted-lists)
    
    * feat: WEEK 04 solution (maximum-depth-of-binary-tree)
    
    * feat: WEEK 04 solution (find-minimum-in-rotated-sorted-array)
    
    * feat: WEEK 04 solution (word-search)
    
    * feat: WEEK 04 solution (coin-change)
    
    * refactor: WEEK 04 remove unnecessary non-null assertions (!!)

---
+++ maximum-depth-of-binary-tree/tigermint.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        var maxDepth = 0
+
+        fun dfs(node: TreeNode, depth: Int) {
+            //leaf node
+            if (node.left == null && node.right == null) maxDepth = maxOf(maxDepth, depth)
+
+            if (node.left != null) dfs(node.left!!, depth + 1)
+            if (node.right != null) dfs(node.right!!, depth + 1)
+        }
+
+        dfs(root, 1)
+
+        return maxDepth
+    }
+
+}
