e65811c991fc7c87af8f08050c9fe12823046734
KyungJin Jung <okyungjin@gmail.com>
2026-07-18 20:26:14 +0900

    79. Word Search (공간복잡도 최적화)

--- word-search/okyungjin.py
+++ word-search/okyungjin.py
@@ -3,8 +3,11 @@
 # 단, 셀은 한번만 사용 가능하다
 
 # dfs를 통해 백트랙킹으로 정답을 찾도록 구현했다.
-# TODO: 시간/공간 복잡도 계산
-class Solution:
+
+# L: word 의 길이
+# 시간 복잡도: O(m * n * 4^L)
+# 공간 복잡도: O(m * n + L)
+class SolutionA:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row_size = len(board)
         col_size = len(board[0])
@@ -13,29 +16,26 @@ class Solution:
         visited = [[False] * col_size for _ in range(row_size)]
 
         def dfs(row, col, str_idx) -> bool:
+            if str_idx == len(word):
+                return True
+            
             # 종료조건1: 보드의 좌표를 넘어가는 경우
             if row < 0 or row >= row_size or col < 0 or col >= col_size:
                 return False
             
-            # 종료조건2: 이미 방문한 경우
-            if visited[row][col]:
+            # 종료조건 2: 이미 방문함
+            # 종료조건 3: 문자열 불일치
+            if visited[row][col] or board[row][col] != word[str_idx]:
                 return False
-
-            # 종료조건3: 문자열 불일치
-            if board[row][col] != word[str_idx]:
-                return False
-
-            # 정답 발견 시 종료
-            if str_idx + 1 == len(word):
-                return True
-
+            
+        
             visited[row][col] = True
 
             # 차례대로 상, 하, 좌, 우의 좌표를 탐색
-            found = dfs(row - 1, col, str_idx + 1) or \
+            found = (dfs(row - 1, col, str_idx + 1) or \
                     dfs(row + 1, col, str_idx + 1) or \
                     dfs(row, col - 1, str_idx + 1) or \
-                    dfs(row, col + 1, str_idx + 1)
+                    dfs(row, col + 1, str_idx + 1))
 
             visited[row][col] = False # 백트래킹
 
@@ -45,7 +45,55 @@ class Solution:
         # 시작점을 찾는다
         for r in range(row_size):
             for c in range(col_size):
-                # board[r][c] == word[0] 인 시작점만 호출하도록 해서 최적화
+                if board[r][c] == word[0] and dfs(r, c, 0):
+                    return True
+
+        return False
+
+# SolutionA 의 공간복잡도 최적화 버전, visited 제거
+# L: word 의 길이
+# 시간 복잡도: O(m * n * 4^L)
+# 공간 복잡도: O(L)
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        VISITED_MARK = '#'
+
+        row_size = len(board)
+        col_size = len(board[0])
+
+        def dfs(row, col, str_idx) -> bool:
+            if str_idx == len(word):
+                return True
+            
+            # 종료조건1: 보드의 좌표를 넘어가는 경우
+            if row < 0 or row >= row_size or col < 0 or col >= col_size:
+                return False
+            
+            # 종료조건 2: 이미 방문함
+            # 종료조건 3: 문자열 불일치
+            if board[row][col] == VISITED_MARK or board[row][col] != word[str_idx]:
+                return False
+            
+
+            # VISITED_MARK로 교체하여 방문을 기록
+            origin_char = board[row][col]
+            board[row][col] = VISITED_MARK
+
+            # 차례대로 상, 하, 좌, 우의 좌표를 탐색
+            found = (dfs(row - 1, col, str_idx + 1) or \
+                    dfs(row + 1, col, str_idx + 1) or \
+                    dfs(row, col - 1, str_idx + 1) or \
+                    dfs(row, col + 1, str_idx + 1))
+
+            # 원본 문자열로 복원
+            board[row][col] = origin_char
+
+            return found
+
+
+        # 시작점을 찾는다
+        for r in range(row_size):
+            for c in range(col_size):
                 if board[r][c] == word[0] and dfs(r, c, 0):
                     return True
 
