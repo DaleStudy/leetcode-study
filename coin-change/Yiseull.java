9386a4d17ad098067e750f4409d9554b8f613f7c
Yiseul Park <omjl5123@catholic.ac.kr>
2026-07-18 23:57:44 +0900

    feat: 322. Coin Change solution2

--- coin-change/Yiseull.java
+++ coin-change/Yiseull.java
@@ -3,8 +3,10 @@ class Solution {
     private int answer = Integer.MAX_VALUE;
 
     public int coinChange(int[] coins, int amount) {
+        final int IMPOSSIBLE = amount + 1;
+
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        Arrays.fill(dp, IMPOSSIBLE);
         dp[0] = 0;
 
         for (int i = 1; i < amount + 1; i++) {
@@ -15,6 +17,6 @@ class Solution {
             }
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == IMPOSSIBLE ? -1 : dp[amount];
     }
 }
