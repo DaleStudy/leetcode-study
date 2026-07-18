03671de68311cee575c06fb5b490398cb6d189ec
Yiseul Park <omjl5123@catholic.ac.kr>
2026-07-18 22:47:52 +0900

    feat: 322. Coin Change solution

---
+++ coin-change/Yiseull.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    private int answer = Integer.MAX_VALUE;
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
