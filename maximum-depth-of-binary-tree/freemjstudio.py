4136b956f8828b12f32803a82e55336a23d4515d
freemjstudio <mjwoo0@naver.com>
2026-07-18 16:00:10 +0900

    maximum-depth-of-binary-tree/freemjstudio.py

---
+++ maximum-depth-of-binary-tree/freemjstudio.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
