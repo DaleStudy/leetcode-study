0a619fcd2f7c0dd1b3be83a07eb7c26886de8bfb
Yiseul Park <omjl5123@catholic.ac.kr>
2026-07-18 23:39:12 +0900

    feat: 153. Find Minimum in Rotated Sorted Array solution2

--- find-minimum-in-rotated-sorted-array/Yiseull.java
+++ find-minimum-in-rotated-sorted-array/Yiseull.java
@@ -6,21 +6,16 @@ class Solution {
     // 공간복잡도: O(1), 시간복잡도: O(logn)
     private int binarySearch(int[] nums) {
         int left = 0, right = nums.length - 1;
-        int answer = 5000;
 
-        while (left <= right) {
+        while (left < right) {
+            if (nums[left] <= nums[right]) return nums[left];
+
             int mid = (left + right) / 2;
 
-            if (nums[right] <= nums[left] && nums[left] <= nums[mid]) {
-                answer = Math.min(answer, nums[mid]);
-                left = mid + 1;
-            }
-            else {
-                answer = Math.min(answer, nums[mid]);
-                right = mid - 1;
-            }
+            if (nums[right] < nums[mid]) left = mid + 1;
+            else right = mid;
         }
 
-        return answer;
+        return nums[left];
     }
 }
