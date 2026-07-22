6dd411d8a947f8b8d38221d6bac1ad5fce0e5805
Hojeong Park <parkhj062@gmail.com>
2026-07-23 01:30:29 +0900

    implement trie prefix tree solution

---
+++ implement-trie-prefix-tree/parkhojeong.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, s: str):
+        self.s: str = s
+        self.isLast: bool = False
+        self.childs: dict[str, Node] = {}
+
+    def addChild(self, s: str):
+        if self.getChild(s) is not None:
+            return
+
+        node = Node(s)
+        self.childs[s] = node
+
+    def getChild(self, s: str) -> Node | None:
+        if s not in self.childs:
+            return None
+
+        return self.childs[s]
+
+    def setIsLast(self, isLast: bool):
+        self.isLast = isLast
+
+    def getIsLast(self) -> bool:
+        return self.isLast
+
+
+class Trie:
+    root: Node
+
+    def __init__(self):
+        self.root = Node("")
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for i in range(len(word)):
+            ch = word[i]
+            if node.getChild(ch) is None:
+                node.addChild(ch)
+
+            node = node.getChild(ch)
+            if i == len(word) - 1:
+                node.setIsLast(True)
+
+    def findNode(self, word) -> Node | None:
+        node = self.root
+        for ch in word:
+            node = node.getChild(ch)
+            if node is None:
+                return None
+
+        return node
+
+    def search(self, word: str) -> bool:
+        node = self.findNode(word)
+        if node is None or not node.getIsLast():
+            return False
+
+        return True
+
+    def startsWith(self, prefix: str) -> bool:
+        return self.findNode(prefix) is not None
+
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
