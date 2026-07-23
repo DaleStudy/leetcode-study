8facb2ecafa6f7580e260f37308a2e5c95d4b332
Hojeong Park <parkhj062@gmail.com>
2026-07-23 12:04:15 +0900

    improve word-break solution

--- word-break/parkhojeong.py
+++ word-break/parkhojeong.py
@@ -1,38 +1,25 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        first_letter_dict = {}
-        for word in wordDict:
-            first_letter_dict.setdefault(word[0], []).append(word)
+        word_len_set = set(len(word) for word in wordDict)
+        word_set = set(word for word in wordDict)
+        visited = set()
 
-        last_letter_dict = {}
-        for word in wordDict:
-            last_letter_dict.setdefault(word[-1], []).append(word)
-
-        for word in wordDict:
-            isSuccess = True
-            for small_s in s.split(word):
-                if small_s == "":
-                    continue
-                if not self.dfs(small_s, 0, first_letter_dict, last_letter_dict):
-                    isSuccess = False
-
-            if isSuccess:
+        def dfs(s: str):
+            if len(s) == 0:
                 return True
 
-        return False
+            if s in visited:
+                return False
 
-    def dfs(self, s: str, i, first_letter_dict, last_letter_dict):
-        if i == len(s):
-            return True
+            visited.add(s)
 
-        if s[i] not in first_letter_dict or s[-1] not in last_letter_dict:
+            for word_len in word_len_set:
+                if s[:word_len] not in word_set:
+                    continue
+
+                if dfs(s[word_len:]):
+                    return True
+
             return False
 
-        for word in first_letter_dict[s[i]]:
-            if not s[i:].startswith(word):
-                continue
-
-            if self.dfs(s, i + len(word), first_letter_dict, last_letter_dict):
-                return True
-
-        return False
+        return dfs(s)
