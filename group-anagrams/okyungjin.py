84fbf28b29551187db6d7cc4890d7e02fe1db5ec
KyungJin Jung <okyungjin@gmail.com>
2026-07-25 20:12:39 +0900

    49. Group Anagrams - 시간 복잡도 최적화

--- group-anagrams/okyungjin.py
+++ group-anagrams/okyungjin.py
@@ -18,3 +18,27 @@ class Solution:
                 anagram_map[key] = [s]
 
         return list(anagram_map.values())
+
+
+'''
+solution2: 문자열의 빈도수 배열을 해시의 키로 사용
+
+복잡도:
+    n: strs의 길이, k: 각 문자열의 최대 길이
+    시간 복잡도: O(n * k)
+    공간 복잡도: O(n * k)
+'''
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map: dict[tuple, List[str]] = defaultdict(list)
+
+        for s in strs:
+            counts = [0] * 26
+            for char in s:
+                counts[ord(char) - ord('a')] += 1
+
+            anagram_map[tuple(counts)].append(s)
+
+        return list(anagram_map.values())
