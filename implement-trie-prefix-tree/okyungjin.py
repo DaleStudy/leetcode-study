52ec55332b7d69c7c8d02a091f728faf3e30eaaf
KyungJin Jung <okyungjin@gmail.com>
2026-07-25 19:08:10 +0900

    208. Implement Trie (Prefix Tree) - TrieNode 자료구조 활용

--- implement-trie-prefix-tree/okyungjin.py
+++ implement-trie-prefix-tree/okyungjin.py
@@ -23,3 +23,58 @@ class Trie:
 
     def startsWith(self, prefix: str) -> bool:
         return prefix in self.prefix_set
+
+
+'''
+solution2: TrieNode 자료구조 활용
+'''
+class TrieNode:
+    def __init__(self):
+        self.children: dict[str, 'TrieNode'] = {}
+        self.is_leaf = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    '''
+    시간 복잡도: O(L), L: 단어의 길이
+    공간 복잡도: O(L), 겹치는 문자열이 없을 때 TrideNode N개 생성
+    '''
+    def insert(self, word: str) -> None:
+        node = self.root
+        
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        
+        node.is_leaf = True
+
+    '''
+    시간 복잡도: O(L), L: 단어의 길이
+    공간 복잡도: O(1) 
+    '''
+    def search(self, word: str) -> bool:
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        
+        return node.is_leaf
+
+    '''
+    시간 복잡도: O(L), L: 단어의 길이
+    공간 복잡도: O(1) 
+    '''
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        
+        return True
