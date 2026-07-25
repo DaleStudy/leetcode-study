bfc447f3e0fc609214e4d0b9bc54a17d16b1fe8a
KyungJin Jung <okyungjin@gmail.com>
2026-07-25 19:26:32 +0900

    139. Word Break - Trie 자료구조 활용

--- word-break/okyungjin.py
+++ word-break/okyungjin.py
@@ -97,3 +97,60 @@ class Solution:
                     break
                     
         return dp[-1]
+
+'''
+solution5: trie 자료구조 활용
+
+Trie 자료구조로 노드를 만들어서 탐색한다
+Examples3: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+
+[Root]
+  ├── 'c' ── 'a' ── 't'(leaf) ── 's'(leaf)
+  ├── 'd' ── 'o' ── 'g'(leaf)
+  ├── 's' ── 'a' ── 'n' ── 'd'(leaf)
+  └── 'a' ── 'n' ── 'd'(leaf)
+'''
+class TrieNode:
+    def __init__(self):
+        self.children: dict[int, 'TrieNode'] = {}
+        self.is_leaf = False
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        root = self._build_trie(wordDict)
+        
+        size = len(s)
+        dp = [False] * (size + 1)
+        dp[0] = True
+        
+        for start in range(size):
+            if not dp[start]:
+                continue
+            
+            node = root
+            for end in range(start + 1, size + 1):
+                char = s[end - 1]
+                # trie에 없으면 바로 종료
+                if char not in node.children:
+                    break
+                node = node.children[char]
+                
+                if node.is_leaf:
+                    dp[end] = True
+                    
+        return dp[-1]
+
+    # wordDict로 trie 자료구조 생성
+    def _build_trie(self, wordDict: list[str]) -> TrieNode:
+        root = TrieNode()
+
+        for word in wordDict:
+            node = root
+
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.is_leaf = True
+
+        return root
