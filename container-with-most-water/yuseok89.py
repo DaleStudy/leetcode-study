e5e9478c2acd12fe261d2437426a86378942f5e1
Yuseok Jo <yuseok89@gmail.com>
2026-07-31 22:41:15 +0900

    container with most water solution

---
+++ container-with-most-water/yuseok89.py
@@ -0,0 +1,22 @@
+# TC: O(N)
+# SC: O(1)
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        max_height = max(height)
+        ans = 0
+
+        while l < r:
+            ans = max(ans, min(height[l], height[r]) * (r - l))
+
+            if ans > max_height * (r - l):
+                return ans
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return ans;
+
