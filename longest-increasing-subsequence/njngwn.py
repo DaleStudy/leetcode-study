be440df00eaaf6126e72211b45d4fd3037338a49
Jeongwon Na <jeongwon.na@tum.de>
2026-07-31 20:03:58 +0200

    solve: longest increasing subsequence

---
+++ longest-increasing-subsequence/njngwn.py
@@ -0,0 +1,23 @@
+class Solution:
+    # Time Complexity: O(n*log(n)), n: len(nums)
+    # Space Complexity: O(1)
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        seq = []
+
+        for num in nums:
+            if not seq or (seq[-1] < num):
+                seq.append(num)
+                continue
+
+            # insert with binary search
+            left, right = 0, len(seq)
+            while left < right:
+                mid = (left + right) // 2
+                if seq[mid] < num:
+                    left = mid + 1
+                else:
+                    right = mid
+
+            seq[left] = num
+
+        return len(seq)
