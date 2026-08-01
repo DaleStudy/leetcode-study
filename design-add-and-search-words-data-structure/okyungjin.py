6663c8267639effa73f5bdf405751484f6594387
KyungJin Jung <okyungjin@gmail.com>
2026-08-01 15:47:30 +0900

    211. Design Add and Search Words Data Structure - 버킷 방식으로 수정했으나 TLE

--- design-add-and-search-words-data-structure/okyungjin.py
+++ design-add-and-search-words-data-structure/okyungjin.py
@@ -1,13 +1,12 @@
 """
-solution1: 직관적인 풀이
-통과는 하지만 아주 느리다
+solution1: 직관적인 풀이 (PASS인데 엄청 느리다)
 
 Runetime: 5438ms, Beats 5.00%
 Memory: 26.74MB, Beats 99.60%
 
 접근법:
     addWord: `word_set`에 단어를 삽입한다.
-    search: `.`은 a-z로 변환해서 데카르트 곱을 통해 가능한 모든 문자열의 조합인 `candidates`를 만든다. `candidates`를 순회하며 `word_set`에 단어가 있는지 확인한다.
+    search: 와일드카드(`.`)은 a-z로 변환해서 데카르트 곱을 통해 가능한 모든 문자열의 조합인 `candidates`를 만든다. `candidates`를 순회하며 `word_set`에 단어가 있는지 확인한다.
 """
 import string
 import itertools
@@ -15,7 +14,6 @@ import itertools
 class WordDictionary:
     def __init__(self):
         self.word_set = set()
-        print(string.ascii_lowercase)
         
     """
     L: `word`의 길이
@@ -36,7 +34,7 @@ class WordDictionary:
     def search(self, word: str) -> bool:
         candidates = []
 
-        """예시
+        """Example:
         word: "bad", candidates: ['b', 'a', 'd']
         word: ".ad", candidates: ['abcdefghijklmnopqrstuvwxyz', 'a', 'd']
         word: "b..", candidates: ['b', 'abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz']
@@ -53,3 +51,57 @@ class WordDictionary:
                 return True
 
         return False
+
+"""
+Soultion2: `word`와 길이가 같은 단어들만 탐색하는 방법
+"""
+from collections import defaultdict
+
+
+class WordDictionary:
+    def __init__(self):
+        self.buckets: defaultdict[int, set[str]] = defaultdict(set)
+
+    """
+    L: `word`의 길이
+
+    Time: O(L)
+    Space: O(L)
+    """
+    def addWord(self, word: str) -> None:
+        bucket = self.buckets[len(word)]
+
+        if word not in bucket:
+            bucket.add(word)
+
+    """
+    L: `word`의 길이
+    N: 같은 길이를 가진 저장된 단어의 개수
+
+    Time: O(L) - 와일드카드 없을 때 / O(N*L) - 와일드카드 있을 때
+    Space: O(1)
+    """
+    def search(self, word: str) -> bool:
+        # 글자수가 동일한 단어들만 탐색
+        bucket: set[str] = self.buckets[len(word)]
+
+        # 와일드카드 없을 때
+        if '.' not in word:
+            return word in bucket
+
+        for candidate in bucket:
+            if self._matches(word, candidate):
+                return True
+
+        return False
+
+    def _matches(self, word: str, candidate: str) -> bool:
+        for i, c in enumerate(word):
+            if c != '.' and c != candidate[i]:
+                return False
+
+        return True
+
+# wordDictionary = WordDictionary()
+# wordDictionary.addWord("bad")
+# wordDictionary.search("..d")
