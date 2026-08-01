b8faa506ed8abff3cb8ffce59174dc10d930b8fc
ICE0208 <doky3307@naver.com>
2026-08-01 21:32:35 +0900

    spiral matrix 공간 복잡도 줄이기

--- spiral-matrix/ICE0208.py
+++ spiral-matrix/ICE0208.py
@@ -1,11 +1,15 @@
-# 이렇게 푸는게 맞나..? 싶은데
-# 일단 1차 제출하고 리팩토링 해보겠습니다. 🫠
-
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        VISIT_NUM = 10000
+
         total = sum(map(lambda k: len(k), matrix))
         answer = []
-        visit = [[False] * len(matrix[0]) for _ in range(len(matrix))]
+
+        def isVisit(i, j):
+            return matrix[i][j] == VISIT_NUM
+
+        def setVisit(i, j):
+            matrix[i][j] = VISIT_NUM
 
         def inRange(i, j):
             if not (0<=i<len(matrix)):
@@ -16,26 +20,26 @@ class Solution:
 
         i, j = 0, 0
         answer.append(matrix[0][0])
-        visit[0][0] = True
+        setVisit(0, 0)
         while len(answer) < total:
-            while inRange(i, j+1) and not visit[i][j+1]:
-                visit[i][j+1] = True
+            while inRange(i, j+1) and not isVisit(i, j+1):
                 answer.append(matrix[i][j+1])
+                setVisit(i, j+1)
                 j += 1
             
-            while inRange(i+1, j) and not visit[i+1][j]:
-                visit[i+1][j] = True
+            while inRange(i+1, j) and not isVisit(i+1, j):
                 answer.append(matrix[i+1][j])
+                setVisit(i+1, j)
                 i += 1
 
-            while inRange(i, j-1) and not visit[i][j-1]:
-                visit[i][j-1] = True
+            while inRange(i, j-1) and not isVisit(i, j-1):
                 answer.append(matrix[i][j-1])
+                setVisit(i, j-1)
                 j -= 1
             
-            while inRange(i-1, j) and not visit[i-1][j]:
-                visit[i-1][j] = True
+            while inRange(i-1, j) and not isVisit(i-1, j):
                 answer.append(matrix[i-1][j])
+                setVisit(i-1, j)
                 i -= 1
 
         return answer
