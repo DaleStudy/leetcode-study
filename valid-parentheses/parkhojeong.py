ce2ca44b19d0078c8f5acc77cc58c7d91d1f46ff
Hojeong Park <parkhj062@gmail.com>
2026-08-02 14:59:34 +0900

    refactor valid-parentheses

--- valid-parentheses/parkhojeong.py
+++ valid-parentheses/parkhojeong.py
@@ -2,22 +2,21 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
 
-        open_parenthesis_set = set({"{", "[", "("})
         close_parenthesis_map = {
-            "]": "[",
-            ")": "(",
-            "}": "{"
+            "[": "]",
+            "(": ")",
+            "{": "}"
         }
 
         for ch in s:
-            if ch in open_parenthesis_set:
+            if ch in close_parenthesis_map:
                 stack.append(ch)
             else:
                 if len(stack) == 0:
                     return False
 
                 top = stack.pop()
-                if close_parenthesis_map[ch] != top:
+                if close_parenthesis_map[top] != ch:
                     return False
 
         if len(stack) > 0:
