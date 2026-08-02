9aa4021c0f016962f95c1086c7a62a3a61496325
sangbeenmoon <jjabi22@naver.com>
2026-08-02 16:07:27 +0900

    [sangbeenmoon] WEEK 06 Solutions (#2790)
    * week06.
    
    * apply review.
    
    ---------
    
    Co-authored-by: sangbeenmoon <moon.sb@navercorp.com>

--- longest-increasing-subsequence/sangbeenmoon.py
+++ longest-increasing-subsequence/sangbeenmoon.py
@@ -25,3 +25,56 @@ class Solution:
                 seq[i] = num
     
         return len(seq)
+
+
+
+
+
+
+
+# ------------
+
+# TC : O(n^2)
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        dp = [1] * len(nums)
+        for i, num in enumerate(nums):
+
+            max_val = 1
+
+            for j in range(0, i):
+                if nums[j] < num:
+                    max_val = max(max_val, dp[j] + 1)
+            
+            dp[i] = max_val
+        
+        return max(dp)
+    
+
+# -----------
+
+# TC : O(nlogn)
+    
+from bisect import bisect_left
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        seq = []
+
+        for i, num in enumerate(nums):
+            if i == 0:
+                seq.append(num)
+                continue
+
+            last = seq[len(seq) - 1] 
+            if last < num:
+                seq.append(num)
+            elif last > num:
+                target_idx = bisect_left(seq, num)
+                seq[target_idx] = num
+        
+        return len(seq)
+    
