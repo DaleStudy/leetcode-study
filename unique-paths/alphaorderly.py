1d3d1e7cec1744e41a1824df25bb919c0dfbeb74
woo <ilov1112@gmail.com>
2026-08-03 11:49:49 +0900

    fix: 범위 수정

--- unique-paths/alphaorderly.py
+++ unique-paths/alphaorderly.py
@@ -33,7 +33,7 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [1] * n
 
-        for _ in range(1, m):
+        for _ in range(m - 1):
             for c in range(1, n):
                 dp[c] += dp[c - 1]
 
