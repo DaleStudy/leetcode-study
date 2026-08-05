fe7b603126cd8208bf5b25dbe8aba6485c4c9324
dolphinflow86 <dolphinflow86@gmail.com>
2026-08-05 22:19:58 +0900

    number of islands solution

---
+++ number-of-islands/dolphinflow86.py
@@ -0,0 +1,36 @@
+# R is the number of rows, and C is the number of columns.
+# TC: O(R * C) - visits each cell at most once
+# SC: O(R * C) - uses the recursion stack in the worst case
+class Solution:
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(row, col):
+            if (
+                row < 0
+                or row >= rows
+                or col < 0
+                or col >= cols
+                or grid[row][col] != "1"
+            ):
+                return
+
+            grid[row][col] = "0"
+
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+
+        islands = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
+                    islands += 1
+                    dfs(row, col)
+
+        return islands
+
