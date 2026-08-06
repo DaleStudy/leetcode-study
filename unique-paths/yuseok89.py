b9ec41daa5443f0c33ebdf8744d74a3a2001bf6e
Yuseok Jo <yuseok89@gmail.com>
2026-08-07 00:22:45 +0900

    unique paths solution

---
+++ unique-paths/yuseok89.py
@@ -0,0 +1,14 @@
+# TC: O(NM)
+# SC: O(N)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        cnt = [0] * n
+        cnt[0] = 1
+
+        for i in range(0, m):
+            for j in range(1, n):
+                cnt[j] += cnt[j - 1]
+
+        return cnt[n - 1];
+
