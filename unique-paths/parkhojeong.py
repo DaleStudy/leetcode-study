f4fdb9b8bf1e5538fe40f8d088492fee9c1601b7
Hojeong Park <parkhj062@gmail.com>
2026-08-07 02:32:49 +0900

    unique-paths: optimize space

--- unique-paths/parkhojeong.py
+++ unique-paths/parkhojeong.py
@@ -1,27 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        row = [0] * m
-        grid = [row] * n
-        grid[0][0] = 1
+        column = [1] * m
 
-        row_len = len(grid)
-        col_len = len(row)
+        for col in range(n - 1):
 
-        def get_unique_path(row, col):
-            if row == 0 and col == 0:
-                return grid[0][0]
+            for row in range(1, m):
+                column[row] = column[row] + column[row - 1]
 
-            if row == 0:
-                return grid[row][col - 1]
-
-            if col == 0:
-                return grid[row - 1][col]
-
-            return grid[row - 1][col] + grid[row][col - 1]
-
-
-        for row in range(row_len):
-            for col in range(col_len):
-                grid[row][col] = get_unique_path(row, col)
-
-        return grid[-1][-1]
+        return column[-1]
