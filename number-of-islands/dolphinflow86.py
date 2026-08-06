7d34a68e234482c844a7dddd9bda2a358b19f208
dolphinflow86 <dolphinflow86@gmail.com>
2026-08-06 20:53:25 +0900

    update variable naming

--- number-of-islands/dolphinflow86.py
+++ number-of-islands/dolphinflow86.py
@@ -4,15 +4,15 @@
 class Solution:
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+        row_count = len(grid)
+        column_count = len(grid[0])
 
         def dfs(row, col):
             if (
                 row < 0
-                or row >= rows
+                or row >= row_count
                 or col < 0
-                or col >= cols
+                or col >= column_count
                 or grid[row][col] != "1"
             ):
                 return
@@ -24,13 +24,13 @@ class Solution:
             dfs(row, col - 1)
             dfs(row, col + 1)
 
-        islands = 0
+        island_count = 0
 
-        for row in range(rows):
-            for col in range(cols):
+        for row in range(row_count):
+            for col in range(column_count):
                 if grid[row][col] == "1":
-                    islands += 1
+                    island_count += 1
                     dfs(row, col)
 
-        return islands
+        return island_count
 
