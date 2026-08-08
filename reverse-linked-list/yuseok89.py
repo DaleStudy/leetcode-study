9f34535f29cc48c86cb72938f864a1247efcd10a
Yuseok Jo <yuseok89@gmail.com>
2026-08-09 02:20:22 +0900

    [yuseok89] WEEK 07 Solutions (#2800)
    * reverse linked list solution
    
    * longest substring without repeating characters solution
    
    * number of islands solution
    
    * unique paths solution
    
    * set matrix zeroes solution
    
    * 리뷰 반영

---
+++ reverse-linked-list/yuseok89.py
@@ -0,0 +1,21 @@
+# TC: O(N)
+# SC: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev = None
+        cur = head
+
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        return prev
+
