8089eb30877c1aa50366a9b8f4dee76c6e715a81
sangbeenmoon <moon.sb@navercorp.com>
2026-08-08 15:15:20 +0900

    wwek7.

--- number-of-islands/sangbeenmoon.py
+++ number-of-islands/sangbeenmoon.py
@@ -27,3 +27,53 @@ class Solution:
                     answer = answer + 1
                     dfs(x,y)
         return answer
+
+# ------
+
+
+
+
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        x_len , y_len = len(grid[0]), len(grid)
+
+        visited = [[False] * x_len for _ in range(y_len)]
+
+        dx = [0,0,-1,1]
+        dy = [-1,1,0,0]
+
+        def dfs(xx:int, yy:int):
+
+            for d in range(4):
+                nx = xx + dx[d]
+                ny = yy + dy[d]
+
+                if 0 <= nx and nx < x_len and 0 <= ny and ny < y_len and grid[ny][nx] == "1":
+                    if not visited[ny][nx]:
+                        visited[ny][nx] = True
+                        dfs(nx,ny)
+
+            return
+
+        answer = 0
+
+        for xx in range(x_len):
+            for yy in range(y_len):
+                if grid[yy][xx] == "1" and not visited[yy][xx]:
+                    answer += 1
+                    visited[yy][xx] = True
+                    dfs(xx,yy)
+
+        return answer
+
+
+                    
+
+            
+
+
+
+            
--- unique-paths/sangbeenmoon.py
+++ unique-paths/sangbeenmoon.py
@@ -11,3 +11,17 @@ class Solution:
                 dp[r][c] = dp[r-1][c] + dp[r][c-1]
         
         return dp[m-1][n-1]
+
+
+
+# ------------
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n for _ in range(m)]
+
+        for r in range(1,m):
+            for c in range(1,n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+
+        return dp[m-1][n-1]
