375d1fe0b2ae14faed9a5367bd6f2a1bc15458a1
woo <ilov1112@gmail.com>
2026-08-11 16:09:34 +0900

    fix: 이상한 코드 수정

--- longest-common-subsequence/alphaorderly.py
+++ longest-common-subsequence/alphaorderly.py
@@ -15,7 +15,7 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         if len(text1) > len(text2):
-            text1, text2 = text1, text2
+            text1, text2 = text2, text1
 
         T1, T2 = len(text1), len(text2)
         dp = [0] * (T1 + 1)
