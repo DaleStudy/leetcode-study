e30386bee958a9946507d8ca8137c2a76910308a
ICE0208 <doky3307@naver.com>
2026-08-15 20:16:09 +0900

    reverse-bits 비트 조작 계산

--- reverse-bits/ICE0208.java
+++ reverse-bits/ICE0208.java
@@ -1,3 +1,20 @@
+class Solution2 {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int lastBit = n & 1; // 가장 오른쪽 비트 추출
+
+            result <<= 1;        // 새 비트를 넣을 자리 확보
+            result |= lastBit;   // 추출한 비트를 오른쪽 끝에 추가
+
+            n >>>= 1;            // 처리한 비트를 버림.
+        }
+
+        return result;
+    }
+}
+
 class Solution {
     public int reverseBits(int n) {
         String binary = String.format("%32s", Integer.toBinaryString(n))
