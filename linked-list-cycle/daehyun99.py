7e892cd800c16f42a9d5760bbd3dada59c5fc480
daehyun99 <daehyun99@kakao.com>
2026-08-16 15:02:57 +0900

    Solve: hasCycle

---
+++ linked-list-cycle/daehyun99.py
@@ -0,0 +1,22 @@
+# Time: O(N)
+# Space: O(N)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+
+        curr = head
+
+        while curr is not None:
+            if curr.next in seen:
+                return True
+            seen.add(curr.next)
+            curr = curr.next
+
+        return False
+        
