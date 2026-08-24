325643e54c932a5ff07e8ca2a5a94258b3fa4fcd
daehyun99 <daehyun99@kakao.com>
2026-08-24 13:39:59 +0900

    Solve: invertTree

---
+++ invert-binary-tree/daehyun99.py
@@ -0,0 +1,17 @@
+# Time: O(N)
+# Space: O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def bfs(node):
+            if node is not None:
+                node.left, node.right = bfs(node.right), bfs(node.left)
+            return node
+        return bfs(root)                
+
+        
