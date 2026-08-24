03fa77dd7055bf7d5161f7899839d34aa1f466fb
JeonJe <43032391+JeonJe@users.noreply.github.com>
2026-08-24 20:53:52 +0900

    invert binary tree solution

---
+++ invert-binary-tree/JeonJe.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// TC: O(n)
+// SC: O(n)
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
