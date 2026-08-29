46c39de082f396653739cd878c059d6f5888457a
ICE0208 <doky3307@naver.com>
2026-08-29 21:24:41 +0900

    search in rotated sorted array

---
+++ search-in-rotated-sorted-array/ICE0208.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // 왼쪽 절반이 정렬되어 있는 경우
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            // 오른쪽 절반이 정렬되어 있는 경우
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
