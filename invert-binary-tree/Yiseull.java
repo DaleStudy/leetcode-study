891eb2b918f6bfbf2a12d017e02d78028de7af4f
Yiseul Park <omjl5123@catholic.ac.kr>
2026-08-29 14:25:06 +0900

    feat: 226. Invert Binary Tree solution

---
+++ invert-binary-tree/Yiseull.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        return new TreeNode(root.val, invertTree(root.right), invertTree(root.left));
+    }
+}
