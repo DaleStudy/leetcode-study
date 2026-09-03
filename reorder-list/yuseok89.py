d35f81d85bc087a2d03b482d78a4ec436ea753b1
Yuseok Jo <yuseok89@gmail.com>
2026-09-03 23:08:34 +0900

    reorder list solution

---
+++ reorder-list/yuseok89.py
@@ -0,0 +1,37 @@
+# TC: O(N)
+# SC: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        if not head or not head.next:
+            return
+
+        def rec(right: Optional[ListNode]) -> Optional[ListNode]:
+            if not right:
+                return head
+
+            left = rec(right.next)
+
+            if not left:
+                return None
+
+            if left == right or left.next == right:
+                right.next = None
+                return None
+
+            nxt_left = left.next
+            left.next = right
+            right.next = nxt_left
+
+            return nxt_left
+
+        rec(head)
+
