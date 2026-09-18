46c37b9945d997bf8db3bdfbdb8727b35edbb388
Yuseok Jo <yuseok89@gmail.com>
2026-09-19 01:28:52 +0900

    kth smallest element in a bst solution

---
+++ kth-smallest-element-in-a-bst/yuseok89.py
@@ -0,0 +1,32 @@
+# TC: O(N)
+# SC: O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode | None, k: int) -> int:
+
+        ans = 0
+
+        def rec(node: TreeNode | None) -> None:
+            nonlocal k, ans
+
+            if node is None or k <= 0:
+                return
+
+            rec(node.left)
+
+            k -= 1
+            if k == 0:
+                ans = node.val
+                return
+
+            rec(node.right)
+
+        rec(root)
+
+        return ans
+
