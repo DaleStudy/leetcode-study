5528633d4287f9b3df646810b6a7b3611a36c84b
dolphinflow86 <dolphinflow86@gmail.com>
2026-09-19 20:16:06 +0900

    find median from data stream solution

---
+++ find-median-from-data-stream/dolphinflow86.py
@@ -0,0 +1,32 @@
+# N is the number of elements added to the data stream.
+# TC: O(log N) for addNum, O(1) for findMedian
+# SC: O(N) - stores elements split into two heaps
+
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []  # max-heap (store negative values)
+        self.large = []  # min-heap
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -num)
+
+        if self.small and self.large and (-self.small[0] > self.large[0]):
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        if len(self.small) > len(self.large) + 1:
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        if len(self.large) > len(self.small):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return float(-self.small[0])
+        return (-self.small[0] + self.large[0]) / 2.0
